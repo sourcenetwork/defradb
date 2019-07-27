1c1ea00c09e2522c4e8c29bc4f71403400113c4e
John-Alan Simmons <simmons.johnalan@gmail.com>
2019-07-26 20:16:59 -0400

    core: Updated MerkleClock interface to support DAG operations directly

--- core/clock.go
+++ core/clock.go
@@ -1,4 +1,10 @@
 package core
 
+import (
+	"github.com/ipfs/go-cid"
+)
+
 type MerkleClock interface {
+	AddDAGNode(delta core.Delta) (cid.Cid, error)
+	ProcessNode(NodeGetter, cid.Cid, uint64, Delta, ipld.Node) ([]cid.Cid, error)
 }
