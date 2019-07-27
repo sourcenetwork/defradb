0484d886a5b9c1a197f1b99894798d8633385b74
John-Alan Simmons <simmons.johnalan@gmail.com>
2019-07-27 00:42:43 -0400

    Wrote first MerkleClock implementation, refactored baseMerkleCRDT, and added ReplicatedData as a param to MerkleCRDT. Lets see if we can get this to all compile

--- merkle/clock/clock.go
+++ merkle/clock/clock.go
@@ -1,5 +1,136 @@
 package clock
 
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/sourcenetwork/defradb/core"
+
+	cid "github.com/ipfs/go-cid"
+	dshelp "github.com/ipfs/go-ipfs-ds-help"
+	ipld "github.com/ipfs/go-ipld-format"
+)
+
 type merkleClock struct {
-	heads *heads
+	store          ds.Datstore
+	heads          *heads
+	crdt           core.ReplicatedData
+	extractDeltaFn func(ipld.Node) core.Delta
+}
+
+// NewMerkleClock returns a new merkle clock to read/write events (deltas) to
+// the clock
+func NewMerkleClock(store ds.Datastore, ns ds.Key, deltaFn func(ipld.Node) core.Delta) core.MerkleClock {
+	return nil
+}
+
+func (mc *merkleClock) putBlock(heads []cid.Cid, height uint64, delta core.Delta) (ipld.Node, error) {
+	if delta != nil {
+		delta.SetPriority(height)
+	}
+
+	node, err := makeNode(delta, heads)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating block")
+	}
+
+	ctx := context.Background()
+	err = mc.store.dagSyncer.Add(ctx, node)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error writing new block %s", node.Cid())
+	}
+
+	return node, nil
+}
+
+func (mc *merkleClock) AddDAGNode(delta core.Delta) (cid.Cid, error) {
+	heads, height, err := mc.Heads.List()
+	if err != nil {
+		return cid.Undef, errors.Wrap(err, "error getting heads")
+	}
+	height = height + 1
+
+	delta.SetPriority(height)
+
+	// write the delta and heads to a new block
+	nd, err := mc.putBlock(heads, height, delta)
+	if err != nil {
+		return cid.Undef
+	}
+
+	// apply the new node and merge the delta with state
+	_, err = mc.ProcessNode(
+		&NodeGetter{mc.store.dagSyncer, mc.extractDeltaFn},
+		nd.Cid(),
+		height,
+		delta,
+		nd,
+	)
+
+	if err != nil {
+		return cid.Undef, errors.Wrap(err, "error processing new block")
+	}
+	return nd.Cid(), nil
+}
+
+// ProcessNode processes an already merged delta into a crdt
+// by
+func (mc *merkleclock) ProcessNode(ng core.NodeGetter, root cid.Cid, rootPrio uint64, delta core.Delta, node ipld.Node) ([]cid.Cid, error) {
+	current := node.Cid()
+	err := mc.crdt.Merge(delta, dshelp.CidToDsKey(current).String())
+	if err != nil {
+		return nil, errors.Wrapf(err, "error merging delta from %s", current)
+	}
+
+	// if prio := delta.GetPriority(); prio%10 == 0 {
+	// 	store.logger.Infof("merged delta from %s (priority: %d)", current, prio)
+	// } else {
+	// 	store.logger.Debugf("merged delta from %s (priority: %d)", current, prio)
+	// }
+
+	links := node.Links()
+	if len(links) == 0 { // reached the bottom, at a leaf
+		err := store.heads.Add(root, rootPrio)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error adding head %s", root)
+		}
+		return nil, nil
+	}
+
+	children := []cid.Cid{}
+
+	for _, l := range links {
+		child := l.Cid
+		isHead, _, err := mc.headsIsHead(child)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error checking if %s is head", child)
+		}
+
+		if isHead {
+			// reached one of the current heads, replace it with the tip
+			// of current branch
+			err := mc.heads.Replace(child, root, rootPrio)
+			if err != nil {
+				return nil, errors.Wrapf(err, "error replacing head: %s->%s", child, root)
+			}
+
+			continue
+		}
+
+		known, err := mc.store.dagSyncer.HasBlock(child)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error checking for know block %s", child)
+		}
+		if known {
+			// we reached a non-head node in the known tree.
+			// This means our root block is a new head
+			mc.heads.Add(root, rootPrio)
+			continue
+		}
+
+		children = append(children, child)
+	}
+
+	return children, nil
 }
--- merkle/crdt/merklecrdt.go
+++ merkle/crdt/merklecrdt.go
@@ -1,21 +1,19 @@
 package crdt
 
 import (
-	"github.com/pkg/errors"
 	"github.com/sourcenetwork/defradb/core"
 	"github.com/sourcenetwork/defradb/merkle/clock"
 
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
-	dshelp "github.com/ipfs/go-ipfs-ds-help"
 )
 
 type MerkleCRDT interface {
 	core.ReplicatedData
-	core.MerkleClock
-	WithStore(ds.Datastore)
-	WithNS(ds.Key)
-	ProcessNode(ng core.NodeGetter, root cid.Cid, rootPrio uint64, delta core.Delta, node ipld.Node) ([]cid.Cid, error)
+	// core.MerkleClock
+	// WithStore(ds.Datastore)
+	// WithNS(ds.Key)
+	// ProcessNode(ng core.NodeGetter, root cid.Cid, rootPrio uint64, delta core.Delta, node ipld.Node) ([]cid.Cid, error)
 	// NewObject() error
 }
 
@@ -32,6 +30,10 @@ var (
 	defaultMerkleCRDTs = make(map[Type]MerkleCRDTFactory)
 )
 
+// The baseMerkleCRDT handles the merkle crdt overhead functions
+// that aren't CRDT specific like the mutations and state retrieval
+// functions. It handles creating and publishing the crdt DAG with
+// the help of the MerkleClock
 type baseMerkleCRDT struct {
 	clock clock.MerkleClock
 	crdt  core.ReplicatedData
@@ -41,47 +43,18 @@ func (base *baseMerkleCRDT) Merge(other core.Delta, id string) error {
 	return base.crdt.Merge(other, id)
 }
 
-func (base *baseMerkleCRDT) ProcessNode(ng core.NodeGetter, root cid.Cid, rootPrio uint64, delta core.Delta, node ipld.Node) ([]cid.Cid, error) {
-	current := node.Cid()
-	err := base.Merge(delta, dshelp.CidToDsKey(current).String())
-	if err != nil {
-		return nil, errors.Wrapf(eff, "error merging delta from %s", current)
-	}
+// func (base *baseMerkleCRDT) ProcessNode(ng core.NodeGetter, root cid.Cid, rootPrio uint64, delta core.Delta, node ipld.Node) ([]cid.Cid, error) {
+// 	current := node.Cid()
+// 	err := base.Merge(delta, dshelp.CidToDsKey(current).String())
+// 	if err != nil {
+// 		return nil, errors.Wrapf(eff, "error merging delta from %s", current)
+// 	}
 
-	return base.clock.ProcessNode(ng, root, rootPrio, delta, node)
-}
+// 	return base.clock.ProcessNode(ng, root, rootPrio, delta, node)
+// }
 
 // Publishes the delta to state
 func (base *baseMerkleCRDT) Publish(delta core.Delta) (cid.Cid, error) {
-	return base.addDAGNode(delta)
-}
-
-func (base *baseMerkleCRDT) addDAGNode(delta core.Delta) (cid.Cid, error) {
-	heads, height, err := base.clock.Heads.List()
-	if err != nil {
-		return cid.Undef, errors.Wrap(err, "error getting heads")
-	}
-	height = height + 1
-
-	delta.SetPriority(height)
-
-	// write the delta and heads to a new block
-	nd, err := base.putBlock(heads, height, delta)
-	if err != nil {
-		return cid.Undef
-	}
-
-	// apply the new node and merge the delta with state
-	_, err = base.ProcessNode(
-		&merkleclock.NodeGetter{base.store.dagSynce},
-		nd.Cid(),
-		height,
-		delta,
-		nd,
-	)
-
-	if err != nil {
-		return cid.Undef, errors.Wrap(err, "error processing new block")
-	}
-	return nd.Cid(), nil
+	return base.clock.addDAGNode(delta)
+	// and broadcast
 }
