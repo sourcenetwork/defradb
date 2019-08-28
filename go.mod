da4618ebdc6ed37db7ebbdcb8c68c49787c40c8f
John-Alan Simmons <simmons.johnalan@gmail.com>
2019-08-28 18:26:18 -0400

    New tests for base MerkleCRDT started

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/multiformats/go-multihash v0.0.6
 	github.com/pkg/errors v0.8.1
 	github.com/ugorji/go/codec v1.1.7
+	github.com/whyrusleeping/go-logging v0.0.0-20170515211332-0457bb6b88fc
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e // indirect
---
+++ merkle/crdt/merklecrdt_test.go
@@ -0,0 +1,51 @@
+package crdt
+
+import (
+	"testing"
+
+	corecrdt "github.com/sourcenetwork/defradb/core/crdt"
+	"github.com/sourcenetwork/defradb/merkle/clock"
+	dagstore "github.com/sourcenetwork/defradb/store"
+
+	"github.com/ipfs/go-cid"
+	ds "github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore/namespace"
+	logging "github.com/ipfs/go-log"
+	"github.com/sourcenetwork/defradb/core/crdt"
+)
+
+var (
+	log = logging.Logger("defrabd.tests.merklecrdt")
+)
+
+func newDS() ds.Datastore {
+	return ds.NewMapDatastore()
+}
+
+func newTestBaseMerkleCRDT() *baseMerkleCRDT {
+	store := newDS()
+	batchStore := namespace.Wrap(store, ds.NewKey("blockstore"))
+	dagstore := dagstore.NewDAGStore(batchStore)
+	ns := ds.NewKey("/test/db")
+	reg := corecrdt.NewLWWRegister(store, ns, "mydockey")
+	clk := clock.NewMerkleClock(store, dagstore, ns, reg, crdt.LWWRegDeltaExtractorFn, log)
+	return &baseMerkleCRDT{clk, reg}
+}
+
+func TestMerkleCRDTPublish(t *testing.T) {
+	bCRDT := newTestBaseMerkleCRDT()
+	delta := &corecrdt.LWWRegDelta{
+		Data: []byte("test"),
+	}
+
+	c, err := bCRDT.Publish(delta)
+	if err != nil {
+		t.Error("Failed to publish delta to MerkleCRDT:", err)
+		return
+	}
+
+	if c == cid.Undef {
+		t.Error("Published returned invalid CID Undef:", c)
+		return
+	}
+}
