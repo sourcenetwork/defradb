7be091f4d790d98b8418f81d9bb9fc38d73c4f22
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-03-18 19:25:50 -0400

    Created Factory system for MerkleCRDTs to reduce per call overhead

---
+++ core/store.go
@@ -0,0 +1,17 @@
+package core
+
+import (
+	"github.com/sourcenetwork/defradb/store"
+
+	ds "github.com/ipfs/go-datastore"
+	logging "github.com/ipfs/go-log"
+)
+
+// MultiStore is an interface wrapper around the 3 main types of stores needed for
+// MerkleCRDTs
+type MultiStore interface {
+	Data() ds.Datastore
+	Head() ds.Datastore
+	Dag() *store.DAGStore
+	Log() logging.StandardLogger
+}
