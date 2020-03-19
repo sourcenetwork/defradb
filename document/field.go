0961ccbbbc2fe0ae2bacdc24d49baf50fb698bda
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-03-19 17:18:19 -0400

    Working towards initial document functionality, updated field/value system

--- document/field.go
+++ document/field.go
@@ -1,16 +1,30 @@
 package document
 
-import "github.com/sourcenetwork/defradb/merkle/crdt"
+import (
+	"github.com/sourcenetwork/defradb/merkle/crdt"
+
+	ds "github.com/ipfs/go-datastore"
+)
 
 // Field is an interface to interact with Fields inside a document
 type Field interface {
+	Key() ds.Key
 	Name() string
 	Type() crdt.Type //TODO Abstract into a Field Type interface
 }
 
 type simpleField struct {
 	name     string
+	key      ds.Key
 	crdtType crdt.Type
+}
+
+func (doc *Document) newField(t crdt.Type, name string) Field {
+	return simpleField{
+		name:     name,
+		key:      doc.Key().ChildString(name),
+		crdtType: t,
+	}
 }
 
 func (field simpleField) Name() string {
@@ -21,9 +35,6 @@ func (field simpleField) Type() crdt.Type {
 	return field.crdtType
 }
 
-func newField(key string, t crdt.Type) Field {
-	return simpleField{
-		name:     key,
-		crdtType: t,
-	}
+func (field simpleField) Key() ds.Key {
+	return field.key
 }
--- document/value.go
+++ document/value.go
@@ -1,22 +1,25 @@
 package document
 
-import (
-	"github.com/sourcenetwork/defradb/merkle/crdt"
-)
+import "github.com/sourcenetwork/defradb/merkle/crdt"
 
 // Value is an interface that points to a concrete Value implementation
 // May collapse this down without an interface
 type Value interface {
 	Value() interface{}
+	Type() crdt.Type
 	IsDocument() bool
 }
 
 type simpleValue struct {
+	t     crdt.Type
 	value interface{}
 }
 
-func newValue(val interface{}) simpleValue {
-	return simpleValue{val}
+func newValue(t crdt.Type, val interface{}) simpleValue {
+	return simpleValue{
+		t:     t,
+		value: val,
+	}
 }
 
 // func (val simpleValue) Set(val interface{})
@@ -25,11 +28,21 @@ func (val simpleValue) Value() interface{} {
 	return val.value
 }
 
+func (val simpleValue) Type() crdt.Type {
+	return val.t
+}
+
 func (val simpleValue) IsDocument() bool {
 	_, ok := val.value.(*Document)
 	return ok
 }
 
-type merkleCRDTValue struct {
-	crdt crdt.MerkleCRDT
-}
+// type merkleCRDTValue struct {
+// 	crdt crdt.MerkleCRDT
+// }
+
+// func newMerkleCRDTValue(dt crdt.MerkleCRDT) *merkleCRDTValue {
+// 	return &merkleCRDTValue{
+// 		crdt: dt
+// 	}
+// }
