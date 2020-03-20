4c649a5434c5e2cec45753de8abf2236049968c6
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-03-20 18:40:14 -0400

    Added CRDT Getter/Setter for db usage later

--- document/value.go
+++ document/value.go
@@ -6,17 +6,20 @@ import "github.com/sourcenetwork/defradb/merkle/crdt"
 // May collapse this down without an interface
 type Value interface {
 	Value() interface{}
-	Type() crdt.Type
 	IsDocument() bool
+	Type() crdt.Type
+	GetCRDT() crdt.MerkleCRDT
+	SetCRDT(crdt.MerkleCRDT) error
 }
 
 type simpleValue struct {
 	t     crdt.Type
 	value interface{}
+	crdt  crdt.MerkleCRDT
 }
 
-func newValue(t crdt.Type, val interface{}) simpleValue {
-	return simpleValue{
+func newValue(t crdt.Type, val interface{}) *simpleValue {
+	return &simpleValue{
 		t:     t,
 		value: val,
 	}
@@ -35,6 +38,20 @@ func (val simpleValue) Type() crdt.Type {
 func (val simpleValue) IsDocument() bool {
 	_, ok := val.value.(*Document)
 	return ok
+}
+
+func (val simpleValue) GetCRDT() crdt.MerkleCRDT {
+	return val.crdt
+}
+
+func (val *simpleValue) SetCRDT(crdt crdt.MerkleCRDT) error {
+	// if val.Type() != crdt.Type() {
+
+	// } else {
+
+	// }
+	val.crdt = crdt
+	return nil
 }
 
 // type merkleCRDTValue struct {
