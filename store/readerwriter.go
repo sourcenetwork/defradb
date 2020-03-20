d863e0f4980aa72a11e05b261eb593af3550e9cf
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-03-20 18:41:01 -0400

    New DSReaderWriter interface to simplify ds.Datastore interface over the most of the defra package

---
+++ store/readerwriter.go
@@ -0,0 +1,17 @@
+package store
+
+import (
+	ds "github.com/ipfs/go-datastore"
+)
+
+// DSReaderWriter simplifies the interface that is exposed by a
+// ds.Datastore into its subcomponents Reader and Writer.
+// Using this simplified interface means that both ds.Datastore
+// and ds.Txn satisy the interface. Due to go-datastore#113 and
+// go-datastore#114 ds.Txn no longer implements ds.Datastore
+// Which means we can't swap between the two for Datastores that
+// support TxnDatastore.
+type DSReaderWriter interface {
+	ds.Read
+	ds.Write
+}
