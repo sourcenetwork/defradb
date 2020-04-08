ab8538d7676fdb64a75820d49e54c8730628b19f
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-04-08 18:33:45 -0400

    Migrated DAGStore from using ipld.DAGService to Blockstore interface

--- core/dag.go
+++ core/dag.go
@@ -2,11 +2,9 @@ package core
 
 import (
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
-	ipld "github.com/ipfs/go-ipld-format"
 )
 
 // DAGStore proxies the ipld.DAGService under the /core namespace for future-proofing
 type DAGStore interface {
-	ipld.DAGService
-	Blockstore() blockstore.Blockstore
+	blockstore.Blockstore
 }
--- store/dag.go
+++ store/dag.go
@@ -1,17 +1,10 @@
 package store
 
 import (
-	"context"
-
 	"github.com/sourcenetwork/defradb/core"
 
-	blockservice "github.com/ipfs/go-blockservice"
-	ds "github.com/ipfs/go-datastore"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
-	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	ipld "github.com/ipfs/go-ipld-format"
 	logging "github.com/ipfs/go-log"
-	dag "github.com/ipfs/go-merkledag"
 )
 
 var (
@@ -20,59 +13,54 @@ var (
 
 // DAGStore is the interface to the underlying BlockStore and BlockService
 type dagStore struct {
-	ipld.DAGService // become a DAG service
-	ctx             context.Context
-	store           ds.Batching
-	bstore          blockstore.Blockstore
-	bserv           blockservice.BlockService
+	blockstore.Blockstore // become a Blockstore
+	store                 core.DSReaderWriter
+	// bstore          blockstore.Blockstore
+	// bserv           blockservice.BlockService
 }
 
 // NewDAGStore creates a new DAGStore with the supplied
 // Batching datastore
-func NewDAGStore(batcher ds.Batching) core.DAGStore {
+func NewDAGStore(store core.DSReaderWriter) core.DAGStore {
 	dstore := &dagStore{
-		ctx:   context.Background(), // @todo Do we need to properly pass through context chain on DAGStore?
-		store: batcher,
+		Blockstore: blockstore.NewBlockstore(store),
+		store:      store,
 	}
-
-	dstore.setupBlockstore()
-	dstore.setupBlockService()
-	dstore.setupDAGService()
 
 	return dstore
 }
 
-func (d *dagStore) setupBlockstore() error {
-	bs := blockstore.NewBlockstore(d.store)
-	bs = blockstore.NewIdStore(bs)
-	cachedbs, err := blockstore.CachedBlockstore(d.ctx, bs, blockstore.DefaultCacheOpts())
-	if err != nil {
-		return err
-	}
-	d.bstore = cachedbs
-	return nil
-}
+// func (d *dagStore) setupBlockstore() error {
+// 	bs := blockstore.NewBlockstore(d.store)
+// 	// bs = blockstore.NewIdStore(bs)
+// 	// cachedbs, err := blockstore.CachedBlockstore(d.ctx, bs, blockstore.DefaultCacheOpts())
+// 	// if err != nil {
+// 	// 	return err
+// 	// }
+// 	d.bstore = bs
+// 	return nil
+// }
 
-func (d *dagStore) setupBlockService() error {
-	// if d.cfg.Offline {
-	// 	d.bserv = blockservice.New(d.bstore, offline.Exchange(p.bstore))
-	// 	return nil
-	// }
+// func (d *dagStore) setupBlockService() error {
+// 	// if d.cfg.Offline {
+// 	// 	d.bserv = blockservice.New(d.bstore, offline.Exchange(p.bstore))
+// 	// 	return nil
+// 	// }
 
-	// bswapnet := network.NewFromIpfsHost(p.host, p.dht)
-	// bswap := bitswap.New(p.ctx, bswapnet, p.bstore)
-	// p.bserv = blockservice.New(p.bstore, bswap)
+// 	// bswapnet := network.NewFromIpfsHost(p.host, p.dht)
+// 	// bswap := bitswap.New(p.ctx, bswapnet, p.bstore)
+// 	// p.bserv = blockservice.New(p.bstore, bswap)
 
-	// @todo Investigate if we need an Exchanger or if it can stay as nil
-	d.bserv = blockservice.New(d.bstore, offline.Exchange(d.bstore))
-	return nil
-}
+// 	// @todo Investigate if we need an Exchanger or if it can stay as nil
+// 	d.bserv = blockservice.New(d.bstore, offline.Exchange(d.bstore))
+// 	return nil
+// }
 
-func (d *dagStore) setupDAGService() error {
-	d.DAGService = dag.NewDAGService(d.bserv)
-	return nil
-}
+// func (d *dagStore) setupDAGService() error {
+// 	d.DAGService = dag.NewDAGService(d.bserv)
+// 	return nil
+// }
 
-func (d *dagStore) Blockstore() blockstore.Blockstore {
-	return d.bstore
-}
+// func (d *dagStore) Blockstore() blockstore.Blockstore {
+// 	return d.bstore
+// }
