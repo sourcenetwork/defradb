b4114315f7e9607cc6a7d0fc5c44602a757b169d
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-09-05 08:54:09 -0400

    Converted field/type generation to Thunkers to support dynamic/circular/self-refencing type definitions. Added Schema Generator type as the main generator entry point. Refined SchemaManager

--- query/schema/graphql/manager.go
+++ query/schema/graphql/manager.go
@@ -7,38 +7,43 @@ import (
 // SchemaManager creates an instanced management point
 // for schema intake/outtake, and updates.
 type SchemaManager struct {
-	schema  gql.Schema
-	typeMap gql.TypeMap
-	types   []*gql.Object // base defined types
+	schema       gql.Schema
+	definedTypes []*gql.Object // base defined types
 }
 
 // NewSchemaManager returns a new instance of a SchemaManager
 // with a new default type map
-func NewSchemaManager() *SchemaManager {
-	return &SchemaManager{
-		typeMap: defaultTypeMap(),
+func NewSchemaManager() (*SchemaManager, error) {
+	sm := &SchemaManager{}
+	schema, err := gql.NewSchema(gql.SchemaConfig{
+		Types: defaultTypes(),
+	})
+	if err != nil {
+		return sm, err
 	}
+	sm.schema = schema
+	return sm, nil
 }
 
 // default type map includes all the native scalar types
-func defaultTypeMap() gql.TypeMap {
-	return gql.TypeMap{
-		"Boolean":  gql.Boolean,
-		"DateTime": gql.DateTime,
-		"Float":    gql.Float,
-		"ID":       gql.ID,
-		"Int":      gql.Int,
-		"String":   gql.String,
+func defaultTypes() []gql.Type {
+	return []gql.Type{
+		gql.Boolean,
+		gql.DateTime,
+		gql.Float,
+		gql.ID,
+		gql.Int,
+		gql.String,
 
 		// Root Query Schema types
 		// Sort/Order enum
-		"Ordering": OrderingEnum,
+		OrderingEnum,
 
 		// filter scalar blocks
-		"BooleanOperatorBlock":  BooleanOperatorBlock,
-		"DateTimeOperatorBlock": DateTimeOperatorBlock,
-		"FloatOperatorBlock":    FloatOperatorBlock,
-		"IntOperatorBlock":      IntOperatorBlock,
-		"StringOperatorBlock":   StringOperatorBlock,
+		BooleanOperatorBlock,
+		DateTimeOperatorBlock,
+		FloatOperatorBlock,
+		IntOperatorBlock,
+		StringOperatorBlock,
 	}
 }
