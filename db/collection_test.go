281872259d45e77a592afc56cbddd1ee1e0c5646
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-10-29 02:10:29 -0400

    Correctly set default primary index on Collection creation

--- db/collection_test.go
+++ db/collection_test.go
@@ -1,1 +1,30 @@
 package db
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sourcenetwork/defradb/db/base"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCollection(t *testing.T) {
+	db, err := newMemoryDB()
+	assert.NoError(t, err)
+
+	_, err = newTestCollection(db)
+	assert.NoError(t, err)
+}
+
+func TestCollectionProperties(t *testing.T) {
+	db, err := newMemoryDB()
+	assert.NoError(t, err)
+
+	col, err := newTestCollection(db)
+	assert.NoError(t, err)
+
+	assert.Equal(t, col.Name(), "test")
+	assert.Equal(t, col.ID(), uint32(1))
+	assert.True(t, reflect.DeepEqual(col.Schema(), base.SchemaDescription{}))
+	assert.Equal(t, len(col.Indexes()), 1)
+}
