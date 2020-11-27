257af04a69432189a6bde70d5641ea97ef653d79
John-Alan Simmons <simmons.johnalan@gmail.com>
2020-11-26 20:02:34 -0500

    sourceInfo defintion for future dataSource objects

---
+++ query/graphql/planner/datasource.go
@@ -0,0 +1,7 @@
+package planner
+
+// sourceInfo stores info about the data source
+type sourceInfo struct{}
+
+// datasource is a set of utilities for constructing scan/index/join nodes
+// from a given query statement
