ea49800cc2ab79082d8c3721094974608ddc2226
John-Alan Simmons <simmons.johnalan@gmail.com>
2021-03-02 15:21:16 -0500

    Updated API definitions for Commit queries.

--- query/graphql/schema/api.go
+++ query/graphql/schema/api.go
@@ -9,7 +9,7 @@ var queryAllCommits = &gql.Field{
 	Name: "allCommits",
 	Type: gql.NewList(types.Commit),
 	Args: gql.FieldConfigArgument{
-		"dockey": newArgConfig(gql.ID),
+		"dockey": newArgConfig(gql.NewNonNull(gql.ID)),
 		"field":  newArgConfig(gql.String),
 	},
 }
@@ -18,7 +18,7 @@ var queryLatestCommits = &gql.Field{
 	Name: "latestCommits",
 	Type: gql.NewList(types.Commit),
 	Args: gql.FieldConfigArgument{
-		"dockey": newArgConfig(gql.ID),
+		"dockey": newArgConfig(gql.NewNonNull(gql.ID)),
 		"field":  newArgConfig(gql.String),
 	},
 }
