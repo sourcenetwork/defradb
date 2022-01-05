266faf0d21afd42401f0dd811c0fd83d975b9e63
John-Alan Simmons <simmons.johnalan@gmail.com>
2022-01-05 11:06:35 -0500

    implemented NewPeer with grpc serve/listen over libp2p

--- net/peer.go
+++ net/peer.go
@@ -2,11 +2,17 @@ package net
 
 import (
 	"context"
+	"errors"
 
 	format "github.com/ipfs/go-ipld-format"
 	"github.com/libp2p/go-libp2p-core/host"
+	gostream "github.com/libp2p/go-libp2p-gostream"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"google.golang.org/grpc"
+
+	"github.com/sourcenetwork/defradb/client"
+	corenet "github.com/sourcenetwork/defradb/core/net"
+	pb "github.com/sourcenetwork/defradb/net/pb"
 )
 
 // Peer is a DefraDB Peer node which exposes all the LibP2P host/peer functionality
@@ -14,12 +20,42 @@ import (
 type peer struct {
 	//config??
 
-	format.DAGService
 	host host.Host
 	ps   *pubsub.PubSub
 
 	rpc    *grpc.Server
 	server *server
 
-	ctx context.Context
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+// NewPeer creates a new instance of the DefraDB server as a peer-to-peer node.
+func NewPeer(ctx context.Context, db client.DB, h host.Host, ps *pubsub.PubSub, ds format.DAGService, serverOptions []grpc.ServerOption, dialOptions []grpc.DialOption) (*peer, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	p := &peer{
+		host:   h,
+		ps:     ps,
+		rpc:    grpc.NewServer(serverOptions...),
+		ctx:    ctx,
+		cancel: cancel,
+	}
+	var err error
+	p.server, err = newServer(p, db, dialOptions...)
+	if err != nil {
+		return nil, err
+	}
+
+	listener, err := gostream.Listen(h, corenet.Protocol)
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		pb.RegisterServiceServer(p.rpc, p.server)
+		if err := p.rpc.Serve(listener); err != nil && !errors.Is(err, grpc.ErrServerStopped) {
+			// @todo: Log fatal
+		}
+	}()
+	return p, nil
 }
