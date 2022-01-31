c466629a0c56eaefb30bfa38bc254eaace583dc8
John-Alan Simmons <simmons.johnalan@gmail.com>
2022-01-31 18:28:18 -0500

    Added offset/limit + sort benchmarks. Removed old untracked deleted benchmark files

---
+++ bench/query/simple/with_limit_offset_test.go
@@ -0,0 +1,54 @@
+package query
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sourcenetwork/defradb/bench/fixtures"
+)
+
+var (
+	userSimpleWithLimitOffsetQuery = `
+	query {
+		User(limit: 10, offset: 5) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+)
+
+func Benchmark_Query_UserSimple_Query_WithLimitOffset_Sync_1(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 1, userSimpleWithLimitOffsetQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query_WithLimitOffset_Sync_10(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 10, userSimpleWithLimitOffsetQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query_WithLimitOffset_Sync_100(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 100, userSimpleWithLimitOffsetQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query_WithLimitOffset_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 1000, userSimpleWithLimitOffsetQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
---
+++ bench/query/simple/with_sort_test.go
@@ -0,0 +1,54 @@
+package query
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sourcenetwork/defradb/bench/fixtures"
+)
+
+var (
+	userSimpleWithSortQuery = `
+	query {
+		User(order: {Age: ASC}) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+)
+
+func Benchmark_Query_UserSimple_Query_WithSort_Sync_1(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 1, userSimpleWithSortQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query_WithSort_Sync_10(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 10, userSimpleWithSortQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query_WithSort_Sync_100(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 100, userSimpleWithSortQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query_WithSort_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, fixtures.WithSchema(ctx, "user_simple"), 1000, userSimpleWithSortQuery, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
