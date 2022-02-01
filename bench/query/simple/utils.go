931155f43f0eb31aad75eece16927e237d531b62
John-Alan Simmons <simmons.johnalan@gmail.com>
2022-01-31 22:39:48 -0500

    Added query formatting with dockey replacement from inserted set

--- bench/query/simple/utils.go
+++ bench/query/simple/utils.go
@@ -3,6 +3,8 @@ package query
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"strings"
 	"testing"
 
 	benchutils "github.com/sourcenetwork/defradb/bench"
@@ -41,6 +43,9 @@ func runQueryBenchGetSync(
 	query string,
 ) error {
 	// fmt.Printf("Query:\n%s\n", query)
+
+	// any preprocessing?
+	query = formatQuery(b, query, dockeys)
 	b.StartTimer()
 
 	for i := 0; i < b.N; i++ {
@@ -56,4 +61,36 @@ func runQueryBenchGetSync(
 		// fmt.Println("--------------------")
 	}
 	return nil
+}
+
+func formatQuery(b *testing.B, query string, dockeys [][]key.DocKey) string {
+	numPlaceholders := strings.Count(query, "{{dockey}}")
+	if numPlaceholders == 0 {
+		return query
+	}
+	// create a copy of dockeys since we'll be mutating it
+	dockeysCopy := dockeys[:]
+
+	// b.Logf("formatting query, replacing %v instances", numPlaceholders)
+	// b.Logf("Query before: %s", query)
+
+	if len(dockeysCopy) < numPlaceholders {
+		b.Fatalf("Invalid number of query placeholders, max is %v requested is %v", len(dockeys), numPlaceholders)
+	}
+
+	for i := 0; i < numPlaceholders; i++ {
+		// pick a random dockey, needs to be unique accross all
+		// calls, so remove the selected one between calls
+		rIndex := rand.Intn(len(dockeysCopy))
+		key := dockeysCopy[rIndex][0]
+
+		// remove selected key
+		dockeysCopy = append(dockeysCopy[:rIndex], dockeysCopy[rIndex+1:]...)
+
+		// replace
+		query = strings.Replace(query, "{{dockey}}", key.String(), 1)
+	}
+
+	// b.Logf("Query After: %s", query)
+	return query
 }
