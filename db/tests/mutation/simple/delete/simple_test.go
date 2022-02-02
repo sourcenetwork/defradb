99257df30109540be072c76a5a5306c450373f17
Shahzad Lone <shahzadlone@gmail.com>
2022-02-01 23:34:31 -0500

    Add more tests

--- db/tests/mutation/simple/delete/simple_test.go
+++ db/tests/mutation/simple/delete/simple_test.go
@@ -31,6 +31,63 @@ func TestMutationDeleteDocumentUsingSingleKey(t *testing.T) {
 		},
 
 		{
+			Description: "Deletion of a document that doesn't exist in non-empty collection.",
+			Query: `mutation {
+						delete_user(id: "bae-8ca944fd-260e-5a44-b88f-326d9faca811") {
+							_key
+						}
+					}`,
+			Docs: map[int][]string{
+				0: {
+					(`{
+						"name": "Shahzad",
+						"age":  26,
+						"points": 48.5,
+						"verified": true
+					}`)},
+			},
+			Results:       nil,
+			ExpectedError: "No document for the given key exists",
+		},
+
+		{
+			Description: "Deletion of a document without sub selection, should give error.",
+			Query: `mutation {
+						delete_user(id: "bae-8ca944fd-260e-5a44-b88f-326d9faca810")
+					}`,
+			Docs: map[int][]string{
+				0: {
+					(`{
+						"name": "Shahzad",
+						"age":  26,
+						"points": 48.5,
+						"verified": true
+					}`)},
+			},
+			Results:       nil,
+			ExpectedError: "[Field \"delete_user\" of type \"[user]\" must have a sub selection.]",
+		},
+
+		{
+			Description: "Deletion of a document without _key sub-selection.",
+			Query: `mutation {
+						delete_user(id: "bae-8ca944fd-260e-5a44-b88f-326d9faca810") {
+						}
+					}`,
+			Docs: map[int][]string{
+				0: {
+					(`{
+						"name": "Shahzad",
+						"age":  26,
+						"points": 48.5,
+						"verified": true
+					}`)},
+			},
+			Results:       nil,
+			ExpectedError: "Syntax Error GraphQL request (2:67) Unexpected empty IN {}\n\n1: mutation {\n2: \\u0009\\u0009\\u0009\\u0009\\u0009\\u0009delete_user(id: \"bae-8ca944fd-260e-5a44-b88f-326d9faca810\") {\n                                                                     ^\n3: \\u0009\\u0009\\u0009\\u0009\\u0009\\u0009}\n",
+		},
+
+		{
 			Description: "Simple delete mutation where one element exists.",
 			Query: `mutation {
 						delete_user(id: "bae-8ca944fd-260e-5a44-b88f-326d9faca810") {
@@ -51,6 +108,7 @@ func TestMutationDeleteDocumentUsingSingleKey(t *testing.T) {
 					"_key": "bae-8ca944fd-260e-5a44-b88f-326d9faca810",
 				},
 			},
+			ExpectedError: "",
 		},
 	}
 
