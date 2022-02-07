034258005ac0345ac0857c8eb406944a404323cb
John-Alan Simmons <simmons.johnalan@gmail.com>
2022-02-07 17:09:52 -0500

    chore: Update License 0.2.0 (#181)
    * Updated license(s) 0.2.0
    
    * Updated license copyright header in code files
    
    Co-authored-by: Orpheus Lummis <o@orpheuslummis.info>

--- db/tests/query/simple/with_group_sum_test.go
+++ db/tests/query/simple/with_group_sum_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Source Inc.
+// Copyright 2022 Democratized Data Foundation
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -7,6 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
+
 package simple
 
 // TODO!!!!! once scalar are merged, this should be capable of summing int/float arrays - likely needs some tweaks in generator.go and query.go
