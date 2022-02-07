a650358619975b9fad664b1742e86802201cddbb
John-Alan Simmons <simmons.johnalan@gmail.com>
2022-02-07 17:09:52 -0500

    chore: Update License 0.2.0 (#181)
    * Updated license(s) 0.2.0
    
    * Updated license copyright header in code files
    
    Co-authored-by: Orpheus Lummis <o@orpheuslummis.info>

--- db/tests/utils.go
+++ db/tests/utils.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Source Inc.
+// Copyright 2022 Democratized Data Foundation
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -7,6 +7,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
+
 package tests
 
 import (
