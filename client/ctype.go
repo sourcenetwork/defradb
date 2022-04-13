6b66960390c4e4699586a688aaeca39b3befad93
Andrew Sisley <a.j.sisley@hotmail.com>
2022-04-13 16:48:19 -0400

    Document CType consts

--- client/ctype.go
+++ client/ctype.go
@@ -13,6 +13,10 @@ package client
 // CType indicates CRDT type
 type CType byte
 
+// Available CRDT types.
+//
+// If a CRDT type is not declared here, we do not support it!  CRDT types here
+// may not be valid in all contexts.
 const (
 	NONE_CRDT = CType(iota) // reserved none type
 	LWW_REGISTER
