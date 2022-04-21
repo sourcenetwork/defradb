c75327890247d737a1ae56a12839282659aa4937
John-Alan Simmons <simmons.johnalan@gmail.com>
2022-04-21 11:14:48 -0400

    added more benchmarks

--- bench/query/simple/with_filter_test.go
+++ bench/query/simple/with_filter_test.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	userSimpleWithFilterQuery = `
+	userSimpleWithFilterQuery1 = `
 	query {
 		User(filter: {Age: {_gt: 10}}) {
 			_key
@@ -29,35 +29,282 @@ var (
 		}
 	}
 	`
+
+	userSimpleWithFilterQuery2 = `
+	query {
+		User(filter: {Age: {_gt: 50}}) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery3 = `
+	query {
+		User(filter: {Age: {_gt: 90}}) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery4 = `
+	query {
+		User(filter: {Points: {_gt: 10000}}) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery5 = `
+	query {
+		User(filter: {Verified: {_eq: true}}) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery6 = `
+	query {
+		User(filter: {Age: {_gt: 10}}) {
+			_key
+			Name
+			Age
+			Points
+			Verified
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery7 = `
+	query {
+		User(filter: {Age: {_gt: 50}}) {
+			_key
+			Age
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery8 = `
+	query {
+		User(filter: {Age: {_gt: 90}}) {
+			_key
+			Age
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery9 = `
+	query {
+		User(filter: {Points: {_gt: 10000}}) {
+			_key
+			Name
+		}
+	}
+	`
+
+	userSimpleWithFilterQuery10 = `
+	query {
+		User(filter: {Verified: {_eq: true}}) {
+			_key
+		}
+	}
+	`
 )
 
-func Benchmark_Query_UserSimple_Query_WithFilter_Sync_1(b *testing.B) {
+func Benchmark_Query_UserSimple_Query1_WithFilter_Sync_1(b *testing.B) {
 	ctx := context.Background()
-	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1, userSimpleWithFilterQuery, false)
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1, userSimpleWithFilterQuery1, false)
 	if err != nil {
 		b.Fatal(err)
 	}
 }
 
-func Benchmark_Query_UserSimple_Query_WithFilter_Sync_10(b *testing.B) {
+func Benchmark_Query_UserSimple_Query1_WithFilter_Sync_10(b *testing.B) {
 	ctx := context.Background()
-	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10, userSimpleWithFilterQuery, false)
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10, userSimpleWithFilterQuery1, false)
 	if err != nil {
 		b.Fatal(err)
 	}
 }
 
-func Benchmark_Query_UserSimple_Query_WithFilter_Sync_100(b *testing.B) {
+func Benchmark_Query_UserSimple_Query1_WithFilter_Sync_100(b *testing.B) {
 	ctx := context.Background()
-	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 100, userSimpleWithFilterQuery, false)
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 100, userSimpleWithFilterQuery1, false)
 	if err != nil {
 		b.Fatal(err)
 	}
 }
 
-func Benchmark_Query_UserSimple_Query_WithFilter_Sync_1000(b *testing.B) {
+func Benchmark_Query_UserSimple_Query1_WithFilter_Sync_1000(b *testing.B) {
 	ctx := context.Background()
-	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery, false)
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery1, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query1_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery1, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query2_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery2, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query2_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery2, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query3_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery3, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query3_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery3, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query4_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery4, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query4_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery4, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query5_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery5, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query5_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery5, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query6_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery6, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query6_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery6, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query7_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery7, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query7_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery7, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query8_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery8, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query8_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery8, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query9_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery9, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query9_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery9, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query10_WithFilter_Sync_1000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 1000, userSimpleWithFilterQuery10, false)
+	if err != nil {
+		b.Fatal(err)
+	}
+}
+
+func Benchmark_Query_UserSimple_Query10_WithFilter_Sync_10000(b *testing.B) {
+	ctx := context.Background()
+	err := runQueryBenchGet(b, ctx, fixtures.ForSchema(ctx, "user_simple"), 10000, userSimpleWithFilterQuery10, false)
 	if err != nil {
 		b.Fatal(err)
 	}
