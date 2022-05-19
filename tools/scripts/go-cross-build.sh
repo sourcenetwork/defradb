1dede96b89a4e9afadefe99afc85d0e6591a4c0a
Orpheus Lummis <o@orpheuslummis.info>
2022-05-19 16:21:02 -0400

    tools: Cross-build script working and located

--- tools/scripts/go-cross-build.sh
+++ tools/scripts/go-cross-build.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-package=$1
-if [[ -z "$package" ]]; then
-  echo "usage: $0 <package-name>"
-  exit 1
-fi
-package_split=(${package//\// })
-package_name=${package_split[-1]}
-
-platforms=("windows/amd64" "windows/386" "linux/amd64" "darwin/amd64")
+command="cli/defradb/main.go"
+build_dir="./build/"
+platforms=("windows/amd64" "windows/386" "linux/amd64" "darwin/amd64" "darwin/arm64")
 
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=$package_name'-'$GOOS'-'$GOARCH
+    output_name=$build_dir'defradb-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $command
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
