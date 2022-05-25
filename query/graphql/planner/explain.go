eea17bac6039ef8786e44e64258c13a62931f7e8
Shahzad Lone <shahzadlone@gmail.com>
2022-05-25 04:46:46 -0400

    wip: Add ability to explain `typeIndexJoin` nodes. todo: Still have to implement their children.

--- query/graphql/planner/explain.go
+++ query/graphql/planner/explain.go
@@ -29,6 +29,7 @@ var (
 	_ explainablePlanNode = (*scanNode)(nil)
 	_ explainablePlanNode = (*selectNode)(nil)
 	_ explainablePlanNode = (*selectTopNode)(nil)
+	_ explainablePlanNode = (*typeIndexJoin)(nil)
 
 	// Nodes to implement in the next explain request PRs.
 	// _ explainablePlanNode = (*averageNode)(nil)
@@ -38,7 +39,6 @@ var (
 	// _ explainablePlanNode = (*renderNode)(nil)
 	// _ explainablePlanNode = (*sortNode)(nil)
 	// _ explainablePlanNode = (*sumNode)(nil)
-	// _ explainablePlanNode = (*typeIndexJoin)(nil)
 	// _ explainablePlanNode = (*updateNode)(nil)
 
 	// Internal Nodes that we don't want to expose / explain.
@@ -85,8 +85,6 @@ var (
 // }
 func buildExplainGraph(source planNode) (map[string]interface{}, error) {
 
-	// fmt.Println("======================= source      : ", source.Kind())
-
 	explainGraph := map[string]interface{}{}
 
 	if source == nil {
@@ -95,40 +93,47 @@ func buildExplainGraph(source planNode) (map[string]interface{}, error) {
 
 	switch node := source.(type) {
 
-	// Walk the multiple children if it is a MultiNode (MultiNode itself is non-explainable).
+	// Walk the multiple children if it is a MultiNode.
+	// Note: MultiNode nodes are not explainable but we use them to wrap the children under them.
 	case MultiNode:
-		childrenSources := node.Children()
-		for _, childSource := range childrenSources {
-			var err error
-			explainGraph, err = buildExplainGraph(childSource.Source())
+		// List to store all explain graphs of explainable children of MultiNode.
+		multiChildExplainGraph := []map[string]interface{}{}
+		for _, childSource := range node.Children() {
+			childExplainGraph, err := buildExplainGraph(childSource)
 			if err != nil {
 				return nil, err
 			}
+			// Add the child's explain graph to the list with all explainable children of MultiNode.
+			multiChildExplainGraph = append(multiChildExplainGraph, childExplainGraph)
 		}
+		// Add the list of explainable children graphs we built above under the current MultiNode.
+		explainNodeLabelTitle := strcase.ToLowerCamel(node.Kind())
+		explainGraph[explainNodeLabelTitle] = multiChildExplainGraph
 
-	// Only explain the node if it is explainable.
+	// If this node has subscribed to the optable-interface that makes a node explainable.
 	case explainablePlanNode:
+		// Start building the explain graph.
 		explainGraphBuilder, err := node.Explain()
 		if err != nil {
 			return nil, err
 		}
-
 		// If not the last child then keep walking the graph to find more explainable nodes.
 		if node.Source() != nil {
-			childExplainGraph, err := buildExplainGraph(node.Source())
+			nextExplainGraph, err := buildExplainGraph(node.Source())
 			if err != nil {
 				return nil, err
 			}
-			for key, value := range childExplainGraph {
+			// Add the key-value pairs from the next nested explain graph into the builder.
+			for key, value := range nextExplainGraph {
 				explainGraphBuilder[key] = value
 			}
 		}
-
+		// Add the explainable graph of the next node under current explainable node.
 		explainNodeLabelTitle := strcase.ToLowerCamel(node.Kind())
 		explainGraph[explainNodeLabelTitle] = explainGraphBuilder
 
 	default:
-		// Node is neither a MultiNode or any explainable node. Skip over it but walk it's children.
+		// Node is neither a MultiNode nor an "explainable" node. Skip over it but walk it's child(ren).
 		var err error
 		explainGraph, err = buildExplainGraph(source.Source())
 		if err != nil {
@@ -182,6 +187,16 @@ func (n *scanNode) Explain() (map[string]interface{}, error) {
 	// @todo: Add the spans attribute (couldn't find an example to test).
 	// spansAttribute := styleAttribute("Spans")
 	// explainerMap[spansAttribute] = n.spans
+
+	return explainerMap, nil
+}
+
+func (n *typeIndexJoin) Explain() (map[string]interface{}, error) {
+	explainerMap := map[string]interface{}{}
+
+	// Add the type attribute.
+	// Add the relation attribute.
+	// Add the relation attribute.
 
 	return explainerMap, nil
 }
---
+++ tests/integration/query/explain/with_count_test.go
@@ -0,0 +1,221 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package one_to_many_multiple
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestExplainQueryOneToManyWithACount(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Explain one one-to-many relation query with count",
+		Query: `query @explain {
+				author {
+					name
+					numberOfBooks: _count(books: {})
+				}
+			}`,
+
+		Docs: map[int][]string{
+			//articles
+			0: {
+				(`{
+					"name": "After Guantánamo, Another Injustice",
+					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
+				}`),
+				(`{
+					"name": "To my dear readers",
+					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
+					}`),
+				(`{
+					"name": "Twinklestar's Favourite Xmas Cookie",
+					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
+				}`),
+			},
+			//books
+			1: {
+				(`{
+					"name": "Painted House",
+					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
+				}`),
+				(`{
+					"name": "A Time for Mercy",
+					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
+					}`),
+				(`{
+					"name": "Theif Lord",
+					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
+				}`),
+			},
+			//authors
+			2: {
+				// bae-41598f0c-19bc-5da6-813b-e80f14a10df3
+				(`{
+					"name": "John Grisham",
+					"age": 65,
+					"verified": true
+				}`),
+				// bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04
+				(`{
+					"name": "Cornelia Funke",
+					"age": 62,
+					"verified": false
+				}`),
+			},
+		},
+
+		// ----> selectTopNode            (explainable but no-attributes)
+		//     ----> selectNode           (explainable)
+		//         ----> typeIndexJoin    (explainable)
+		//             ----> typeJoinMany (non-explainable)
+		//                 ----> scanNode (explainable)
+		Results: []map[string]interface{}{
+			{
+				"explain": map[string]interface{}{
+					"selectTopNode": map[string]interface{}{
+						"selectNode": map[string]interface{}{
+							"filter": nil,
+							"typeIndexJoin": map[string]interface{}{
+								"scanNode": map[string]interface{}{
+									"collectionID":   "3",
+									"collectionName": "author",
+									"filter":         nil,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestExplainQueryOneToManyMultipleWithCounts(t *testing.T) {
+	test := testUtils.QueryTestCase{
+
+		Description: "Explain two one-to-many relation query with count",
+
+		Query: `query @explain {
+				author {
+					name
+					numberOfBooks: _count(books: {})
+					numberOfArticles: _count(articles: {})
+				}
+			}`,
+
+		Docs: map[int][]string{
+			//articles
+			0: {
+				(`{
+					"name": "After Guantánamo, Another Injustice",
+					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
+				}`),
+				(`{
+					"name": "To my dear readers",
+					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
+					}`),
+				(`{
+					"name": "Twinklestar's Favourite Xmas Cookie",
+					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
+				}`),
+			},
+			//books
+			1: {
+				(`{
+					"name": "Painted House",
+					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
+				}`),
+				(`{
+					"name": "A Time for Mercy",
+					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
+					}`),
+				(`{
+					"name": "Theif Lord",
+					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
+				}`),
+			},
+			//authors
+			2: {
+				// bae-41598f0c-19bc-5da6-813b-e80f14a10df3
+				(`{
+					"name": "John Grisham",
+					"age": 65,
+					"verified": true
+				}`),
+				// bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04
+				(`{
+					"name": "Cornelia Funke",
+					"age": 62,
+					"verified": false
+				}`),
+			},
+		},
+
+		// ----> selectTopNode                     (explainable but no attributes)
+		//     ----> selectNode                    (explainable)
+		//         ----> parallelNode              (non-explainable but wraps children)
+		//             ----> typeIndexJoin         (explainable)
+		//                 ----> typeJoinMany      (non-explainable)
+		//                     ----> scanNode      (explainable)
+		//                         ----> scanNode  (explainable)
+		//             ----> typeIndexJoin         (explainable)
+		//                 ----> typeJoinMany      (non-explainable)
+		//                     ----> scanNode      (explainable)
+		//                         ----> scanNode  (explainable)
+		Results: []map[string]interface{}{
+			{
+				"explain": map[string]interface{}{
+					"selectTopNode": map[string]interface{}{
+						"selectNode": map[string]interface{}{
+							"parallelNode": []map[string]interface{}{
+								{
+									"typeIndexJoin": map[string]interface{}{
+										"scanNode": map[string]interface{}{
+											"collectionID":   "3",
+											"collectionName": "author",
+											"filter":         nil,
+											"scanNode": map[string]interface{}{
+												"collectionID":   "3",
+												"collectionName": "author",
+												"filter":         nil,
+											},
+										},
+									},
+								},
+								{
+									"typeIndexJoin": map[string]interface{}{
+										"scanNode": map[string]interface{}{
+											"collectionID":   "3",
+											"collectionName": "author",
+											"filter":         nil,
+											"scanNode": map[string]interface{}{
+												"collectionID":   "3",
+												"collectionName": "author",
+												"filter":         nil,
+											},
+										},
+									},
+								},
+							},
+							"filter": nil,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
