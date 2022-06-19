bb08df0f4501b5a40861f1ef9f4193f712f5e819
Orpheus Lummis <o@orpheuslummis.info>
2022-06-19 05:51:13 -0500

    chore: Add utility for generation of man pages (#493)

---
+++ cmd/genmanpages/main.go
@@ -0,0 +1,51 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+/*
+genmanpages handles the generation of man pages. It is meant to be used as part of packaging scripts, as man pages
+installation is packaging and system dependent.
+*/
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+
+	"github.com/sourcenetwork/defradb/cli"
+	"github.com/sourcenetwork/defradb/logging"
+	"github.com/spf13/cobra/doc"
+)
+
+const defaultPerm os.FileMode = 0o777
+
+var log = logging.MustNewLogger("defra.genmanpages")
+
+func main() {
+	dirFlag := flag.String("o", "build/man", "Directory in which to generate DefraDB man pages")
+	flag.Parse()
+	genRootManPages(*dirFlag)
+}
+
+func genRootManPages(dir string) {
+	ctx := context.Background()
+	header := &doc.GenManHeader{
+		Title:   "defradb - Peer-to-Peer Edge Database",
+		Section: "1",
+	}
+	err := os.MkdirAll(dir, defaultPerm)
+	if err != nil {
+		log.FatalE(ctx, "Failed to create directory", err, logging.NewKV("dir", dir))
+	}
+	err = doc.GenManTree(cli.RootCmd, header, dir)
+	if err != nil {
+		log.FatalE(ctx, "Failed generation of man pages", err)
+	}
+}
