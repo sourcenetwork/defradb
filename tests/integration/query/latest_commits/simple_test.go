88d36b3f343abf3c79a9630ddb608b5ed952fe80
Andrew Sisley <a.j.sisley@hotmail.com>
2022-06-20 16:15:09 -0400

    Expand latest commits tests

---
+++ tests/integration/query/latest_commits/simple_test.go
@@ -0,0 +1,45 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package latest_commits
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+// This test is for documentation reasons only. This is not
+// desired behaviour (should return all latest commits).
+func TestQueryLatestCommits(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple latest commits query",
+		Query: `query {
+					latestCommits {
+						cid
+						links {
+							cid
+							name
+						}
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John",
+					"Age": 21
+				}`,
+			},
+		},
+		ExpectedError: "Field \"latestCommits\" argument \"dockey\" of type \"ID!\" is required but not provided.",
+	}
+
+	executeTestCase(t, test)
+}
