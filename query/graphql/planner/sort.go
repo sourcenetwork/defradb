9c4cbd463bd09110abda40e862bebf0c2d6c8a9a
Shahzad Lone <shahzadlone@gmail.com>
2022-06-24 02:45:00 -0400

    wip: Add implementation of explaining sortNode attribute(s).

--- query/graphql/planner/sort.go
+++ query/graphql/planner/sort.go
@@ -54,6 +54,7 @@ type sortNode struct {
 	// that sorts, then provides the values
 	// sorted
 	sortStrategy sortingStrategy
+
 	// indicates if our underlying sortStrategy is still
 	// consuming and sorting data.
 	needSort bool
@@ -97,7 +98,20 @@ func (n *sortNode) Value() core.Doc {
 // Explain method returns a map containing all attributes of this node that
 // are to be explained, subscribes / opts-in this node to be an explainablePlanNode.
 func (n *sortNode) Explain() (map[string]interface{}, error) {
-	return map[string]interface{}{}, nil
+	orderings := []map[string]interface{}{}
+
+	for _, element := range n.ordering {
+		orderings = append(orderings,
+			map[string]interface{}{
+				"field":     element.Field,
+				"direction": string(element.Direction),
+			},
+		)
+	}
+
+	return map[string]interface{}{
+		"orderings": orderings,
+	}, nil
 }
 
 func (n *sortNode) Next() (bool, error) {
@@ -124,9 +138,6 @@ func (n *sortNode) Next() (bool, error) {
 		if err := n.sortStrategy.Add(n.plan.Value()); err != nil {
 			return false, err
 		}
-
-		// finalize, assign valueIter = sortStrategy
-		// break
 	}
 
 	next, err := n.valueIter.Next()
