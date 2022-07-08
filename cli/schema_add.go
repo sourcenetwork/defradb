e6b41d7ee02f0c11a1b03174685c8abb0ba220c3
Orpheus Lummis <o@orpheuslummis.info>
2022-07-08 17:32:10 -0400

    feat: Add support for stdin as input in CLI (#608)

--- cli/schema_add.go
+++ cli/schema_add.go
@@ -11,74 +11,114 @@
 package cli
 
 import (
-	"bytes"
-	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
 
-	httpapi "github.com/sourcenetwork/defradb/api/http"
-	"github.com/sourcenetwork/defradb/logging"
 	"github.com/spf13/cobra"
+
+	httpapi "github.com/sourcenetwork/defradb/api/http"
 )
 
-var (
-	schemaFile string
-)
+var schemaFile string
 
-// addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new schema type to a DefraDB instance",
-	Long: `Example Usage:
-> defradb client schema add -f user.sdl`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := context.Background()
+	Use:   "add [schema]",
+	Short: "Add a new schema type to DefraDB",
+	Long: `Add a new schema type to DefraDB.
 
-		var schema []byte
-		if len(args) > 0 {
-			schema = []byte(strings.Join(args, "\n"))
-		} else if schemaFile != "" {
+Example: add as an argument string:
+  defradb client schema add 'type Foo { ... }'
+
+Example: add from file:
+  defradb client schema add -f schema.graphql
+
+Example: add from stdin:
+  cat schema.graphql | defradb client schema add -
+
+To learn more about the DefraDB GraphQL Schema Language, refer to https://docs.source.network.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var schema string
+		inputIsPipe, err := isStdinPipe()
+		if err != nil {
+			return err
+		}
+
+		if len(args) > 1 {
+			return fmt.Errorf("too many arguments")
+		}
+
+		if schemaFile != "" {
 			buf, err := os.ReadFile(schemaFile)
-			cobra.CheckErr(err)
-			schema = buf
+			if err != nil {
+				return fmt.Errorf("failed to read schema file: %w", err)
+			}
+			schema = string(buf)
+		} else if inputIsPipe && (len(args) == 0 || args[0] != "-") {
+			log.FeedbackInfo(
+				cmd.Context(),
+				"Run 'defradb client schema add -' to read from stdin."+
+					" Example: 'cat schema.graphql | defradb client schema add -').",
+			)
+			return nil
+		} else if len(args) == 0 {
+			err := cmd.Help()
+			if err != nil {
+				return fmt.Errorf("failed to print help: %w", err)
+			}
+			return nil
+		} else if args[0] == "-" {
+			stdin, err := readStdin()
+			if err != nil {
+				return fmt.Errorf("failed to read stdin: %w", err)
+			}
+			if len(stdin) == 0 {
+				return fmt.Errorf("no schema in stdin provided")
+			} else {
+				schema = stdin
+			}
 		} else {
-			log.Fatal(ctx, "Missing schema")
+			schema = args[0]
+		}
+
+		if schema == "" {
+			return fmt.Errorf("empty schema provided")
 		}
 
 		endpoint, err := httpapi.JoinPaths(cfg.API.AddressToURL(), httpapi.SchemaLoadPath)
 		if err != nil {
-			log.ErrorE(ctx, "Join paths failed", err)
-			return
+			return fmt.Errorf("join paths failed: %w", err)
 		}
 
-		res, err := http.Post(endpoint.String(), "text", bytes.NewBuffer(schema))
-		cobra.CheckErr(err)
+		res, err := http.Post(endpoint.String(), "text", strings.NewReader(schema))
+		if err != nil {
+			return fmt.Errorf("failed to post schema: %w", err)
+		}
 
 		defer func() {
 			err = res.Body.Close()
 			if err != nil {
-				log.ErrorE(ctx, "Response body closing failed", err)
+				log.ErrorE(cmd.Context(), "response body closing failed", err)
 			}
 		}()
 
 		result, err := io.ReadAll(res.Body)
-		cobra.CheckErr(err)
-		log.Info(ctx, "", logging.NewKV("Result", string(result)))
+		if err != nil {
+			return fmt.Errorf("failed to read response body: %w", err)
+		}
+
+		indentedResult, err := indentJSON(result)
+		if err != nil {
+			return fmt.Errorf("failed to pretty print result: %w", err)
+		}
+		log.FeedbackInfo(cmd.Context(), indentedResult)
+		return nil
 	},
 }
 
 func init() {
 	schemaCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	addCmd.Flags().StringVarP(&schemaFile, "file", "f", "", "File to load a schema from")
+	addCmd.Flags().StringVarP(&schemaFile, "file", "f", "", "file to load a schema from")
 }
