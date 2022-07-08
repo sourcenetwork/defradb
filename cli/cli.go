9b5ac9c5a4618d2b4f50f320c86140e7a4d6bdfe
Orpheus Lummis <o@orpheuslummis.info>
2022-07-08 03:10:58 +0000

    feat: Add stdin support in CLI

---
+++ cli/cli.go
@@ -0,0 +1,43 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package cli
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func stdinIsPipe() bool {
+	fileInfo, _ := os.Stdin.Stat()
+	return fileInfo.Mode()&os.ModeCharDevice == 0
+}
+
+func readStdin() (string, error) {
+	var s strings.Builder
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s.Write(scanner.Bytes())
+	}
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("reading standard input: %w", err)
+	}
+	return s.String(), nil
+}
+
+func indentJSON(b []byte) (string, error) {
+	var indentedJSON bytes.Buffer
+	err := json.Indent(&indentedJSON, b, "", "  ")
+	return indentedJSON.String(), err
+}
