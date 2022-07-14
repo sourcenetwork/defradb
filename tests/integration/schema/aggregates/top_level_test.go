b52fd08d0af3036de7a47856ee052d31d2fa5a5e
Andrew Sisley <a.j.sisley@hotmail.com>
2022-07-14 15:03:03 -0400

    Add top-level aggregate schema tests

---
+++ tests/integration/schema/aggregates/top_level_test.go
@@ -0,0 +1,221 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package aggregates
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration/schema"
+)
+
+func TestSchemaAggregateTopLevelCreatesCountGivenSchema(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Schema: []string{
+			`
+				type users {}
+			`,
+		},
+		IntrospectionQuery: `
+			query IntrospectionQuery {
+				__schema {
+					queryType {
+						fields {
+							name
+							args {
+								name
+								type {
+									name
+									inputFields {
+										name
+										type {
+											name
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`,
+		ContainsData: map[string]interface{}{
+			"__schema": map[string]interface{}{
+				"queryType": map[string]interface{}{
+					"fields": []interface{}{
+						map[string]interface{}{
+							"name": "_count",
+							"args": []interface{}{
+								map[string]interface{}{
+									"name": "users",
+									"type": map[string]interface{}{
+										"name": "usersCountInputObj",
+										"inputFields": []interface{}{
+											map[string]interface{}{
+												"name": "_",
+												"type": map[string]interface{}{
+													"name": "Int",
+												},
+											},
+											map[string]interface{}{
+												"name": "filter",
+												"type": map[string]interface{}{
+													"name": "usersFilterArg",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteQueryTestCase(t, test)
+}
+
+func TestSchemaAggregateTopLevelCreatesSumGivenSchema(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Schema: []string{
+			`
+				type users {}
+			`,
+		},
+		IntrospectionQuery: `
+			query IntrospectionQuery {
+				__schema {
+					queryType {
+						fields {
+							name
+							args {
+								name
+								type {
+									name
+									inputFields {
+										name
+										type {
+											name
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`,
+		ContainsData: map[string]interface{}{
+			"__schema": map[string]interface{}{
+				"queryType": map[string]interface{}{
+					"fields": []interface{}{
+						map[string]interface{}{
+							"name": "_sum",
+							"args": []interface{}{
+								map[string]interface{}{
+									"name": "users",
+									"type": map[string]interface{}{
+										"name": "usersNumericAggregateBaseArg",
+										"inputFields": []interface{}{
+											map[string]interface{}{
+												"name": "field",
+												"type": map[string]interface{}{
+													"name": "usersNumericFieldsArg",
+												},
+											},
+											map[string]interface{}{
+												"name": "filter",
+												"type": map[string]interface{}{
+													"name": "usersFilterArg",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteQueryTestCase(t, test)
+}
+
+func TestSchemaAggregateTopLevelCreatesAverageGivenSchema(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Schema: []string{
+			`
+				type users {}
+			`,
+		},
+		IntrospectionQuery: `
+			query IntrospectionQuery {
+				__schema {
+					queryType {
+						fields {
+							name
+							args {
+								name
+								type {
+									name
+									inputFields {
+										name
+										type {
+											name
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`,
+		ContainsData: map[string]interface{}{
+			"__schema": map[string]interface{}{
+				"queryType": map[string]interface{}{
+					"fields": []interface{}{
+						map[string]interface{}{
+							"name": "_avg",
+							"args": []interface{}{
+								map[string]interface{}{
+									"name": "users",
+									"type": map[string]interface{}{
+										"name": "usersNumericAggregateBaseArg",
+										"inputFields": []interface{}{
+											map[string]interface{}{
+												"name": "field",
+												"type": map[string]interface{}{
+													"name": "usersNumericFieldsArg",
+												},
+											},
+											map[string]interface{}{
+												"name": "filter",
+												"type": map[string]interface{}{
+													"name": "usersFilterArg",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteQueryTestCase(t, test)
+}
