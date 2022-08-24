cd2d47cf8662726d16e12c67d90f978e214d8e6c
Andrew Sisley <a.j.sisley@hotmail.com>
2022-08-24 12:40:17 -0400

    Add option type
    Allows for safe(r) and efficient handling of nillable values.

---
+++ client/option.go
@@ -0,0 +1,34 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package client
+
+// Option represents an item that may or may not have a value.
+type Option[T any] struct {
+	// If HasValue is true, this Option contains a value, if
+	// it is false it contains no value.
+	HasValue bool
+
+	// The Value of this Option. Should be ignored if HasValue is false.
+	Value T
+}
+
+// Some returns an `Option` of type `T` with the given value.
+func Some[T any](value T) Option[T] {
+	return Option[T]{
+		HasValue: true,
+		Value:    value,
+	}
+}
+
+// Some returns an `Option` of type `T` with no value.
+func None[T any]() Option[T] {
+	return Option[T]{}
+}
