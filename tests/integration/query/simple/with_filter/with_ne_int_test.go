c12f4ea08d40bb016f752b45cb389bdb7603344a
Andrew Sisley <a.j.sisley@hotmail.com>
2022-09-14 12:27:42 -0400

    Add not equals filter tests

---
+++ tests/integration/query/simple/with_filter/with_ne_int_test.go
@@ -0,0 +1,83 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package simple
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestQuerySimpleWithIntNotEqualsFilterBlock(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple query with ne int filter",
+		Query: `query {
+					users(filter: {Age: {_ne: 21}}) {
+						Name
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John",
+					"Age": 21
+				}`,
+				`{
+					"Name": "Bob",
+					"Age": 32
+				}`,
+			},
+		},
+		Results: []map[string]interface{}{
+			{
+				"Name": "Bob",
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestQuerySimpleWithIntNotEqualsNilFilterBlock(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple query with ne int nil filter",
+		Query: `query {
+					users(filter: {Age: {_ne: null}}) {
+						Name
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John",
+					"Age": 21
+				}`,
+				`{
+					"Name": "Bob",
+					"Age": 32
+				}`,
+				`{
+					"Name": "Fred"
+				}`,
+			},
+		},
+		Results: []map[string]interface{}{
+			{
+				"Name": "Bob",
+			},
+			{
+				"Name": "John",
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
---
+++ tests/integration/query/simple/with_filter/with_ne_string_test.go
@@ -0,0 +1,83 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package simple
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestQuerySimpleWithStringNotEqualsFilterBlock(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple query with ne string filter",
+		Query: `query {
+					users(filter: {Name: {_ne: "John"}}) {
+						Age
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John",
+					"Age": 21
+				}`,
+				`{
+					"Name": "Bob",
+					"Age": 32
+				}`,
+			},
+		},
+		Results: []map[string]interface{}{
+			{
+				"Age": uint64(32),
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestQuerySimpleWithStringNotEqualsNilFilterBlock(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple query with ne string nil filter",
+		Query: `query {
+					users(filter: {Name: {_ne: null}}) {
+						Age
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John",
+					"Age": 21
+				}`,
+				`{
+					"Name": "Bob",
+					"Age": 32
+				}`,
+				`{
+					"Age": 36
+				}`,
+			},
+		},
+		Results: []map[string]interface{}{
+			{
+				"Age": uint64(32),
+			},
+			{
+				"Age": uint64(21),
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
