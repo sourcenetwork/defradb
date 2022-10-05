f3d5d1427d8d423b8ab0cfbb95b9bd0f7bc89648
Andrew Sisley <a.j.sisley@hotmail.com>
2022-10-05 16:37:21 -0400

    Add __typename support
    As per GQL spec

---
+++ tests/integration/query/simple/with_typename_test.go
@@ -0,0 +1,73 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package simple
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestQuerySimpleWithTypeName(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple query with typename",
+		Query: `query {
+					users {
+						Name
+						__typename
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John"
+				}`,
+			},
+		},
+		Results: []map[string]any{
+			{
+				"Name":       "John",
+				"__typename": "users",
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestQuerySimpleWithAliasedTypeName(t *testing.T) {
+	test := testUtils.QueryTestCase{
+		Description: "Simple query with aliased typename",
+		Query: `query {
+					users {
+						Name
+						__typename
+						t1: __typename
+					}
+				}`,
+		Docs: map[int][]string{
+			0: {
+				`{
+					"Name": "John"
+				}`,
+			},
+		},
+		Results: []map[string]any{
+			{
+				"Name":       "John",
+				"__typename": "users",
+				"t1":         "users",
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
