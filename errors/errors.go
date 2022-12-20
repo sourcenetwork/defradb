dae42906e883613e5c9c9bee1c63bb25e013775c
Andrew Sisley <a.j.sisley@hotmail.com>
2022-12-20 12:57:22 -0500

    PR FIXUP - Rename depthToSkip

--- errors/errors.go
+++ errors/errors.go
@@ -70,20 +70,20 @@ func WithStack(err error, keyvals ...KV) error {
 // This function will not be inlined by the compiler as it will spoil any stacktrace
 // generated.
 //go:noinline
-func newError(message string, additionalStackDepthToSkip int, keyvals ...KV) *defraError {
-	return withStackTrace(message, additionalStackDepthToSkip+1, keyvals...)
+func newError(message string, depthToSkip int, keyvals ...KV) *defraError {
+	return withStackTrace(message, depthToSkip+1, keyvals...)
 }
 
 // This function will not be inlined by the compiler as it will spoil any stacktrace
 // generated.
 //go:noinline
-func withStackTrace(message string, additionalStackDepthToSkip int, keyvals ...KV) *defraError {
+func withStackTrace(message string, depthToSkip int, keyvals ...KV) *defraError {
 	stackBuffer := make([]uintptr, MaxStackDepth)
 
 	// Skip the first X frames as they are part of this library (and dependencies) and are
 	// best hidden, also account for any parent calls within this library.
 	const depthFromHereToSkip int = 2
-	length := runtime.Callers(depthFromHereToSkip+additionalStackDepthToSkip, stackBuffer[:])
+	length := runtime.Callers(depthFromHereToSkip+depthToSkip, stackBuffer[:])
 	stack := stackBuffer[:length]
 	stackText := toString(stack)
 
