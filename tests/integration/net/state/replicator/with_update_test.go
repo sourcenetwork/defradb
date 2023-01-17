0cd9b062bf6f5b7314d9ec86c52fc2500726dca7
Andrew Sisley <a.j.sisley@hotmail.com>
2023-01-17 15:04:19 -0500

    PR FIXUP - Add inverted replicator update test

--- tests/integration/net/state/replicator/with_update_test.go
+++ tests/integration/net/state/replicator/with_update_test.go
@@ -61,3 +61,49 @@ func TestP2POneToOneReplicatorUpdatesDocCreatedBeforeReplicatorConfig(t *testing
 
 	testUtils.ExecuteTestCase(t, test)
 }
+
+func TestP2POneToOneReplicatorUpdatesDocCreatedBeforeReplicatorConfigWithNodesInversed(t *testing.T) {
+	test := testUtils.P2PTestCase{
+		NodeConfig: []*config.Config{
+			testUtils.RandomNetworkingConfig(),
+			testUtils.RandomNetworkingConfig(),
+		},
+		NodeReplicators: map[int][]int{
+			0: {
+				1,
+			},
+		},
+		SeedDocuments: map[int]string{
+			// This document is created in all nodes before the replicator is set up.
+			// Updates should be synced across nodes.
+			0: `{
+				"Name": "John",
+				"Age": 21
+			}`,
+		},
+		Updates: map[int]map[int][]string{
+			// Note: The update is applied to the target node (not source) specified in the config.
+			1: {
+				0: {
+					`{
+						"Age": 60
+					}`,
+				},
+			},
+		},
+		Results: map[int]map[int]map[string]any{
+			0: {
+				0: {
+					"Age": uint64(60),
+				},
+			},
+			1: {
+				0: {
+					"Age": uint64(60),
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
