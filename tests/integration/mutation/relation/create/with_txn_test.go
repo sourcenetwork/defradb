a84d38a8c0ff61c1cf921ad9671db376ff196c4d
Andrew Sisley <a.j.sisley@hotmail.com>
2023-02-13 15:34:35 -0500

    Migrate TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward

--- tests/integration/mutation/relation/create/with_txn_test.go
+++ tests/integration/mutation/relation/create/with_txn_test.go
@@ -171,64 +171,55 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsForward(t *testing.
 }
 
 func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Create relational documents, and check the links in backward direction.",
-
-		Docs: map[int][]string{
-			// publishers
-			2: {
+		Actions: []any{
+			testUtils.CreateDoc{
+				CollectionID: 2,
 				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
+				Doc: `{
 					"name": "Website",
 					"address": "Manning Publications"
 				}`,
-
+			},
+			testUtils.CreateDoc{
+				CollectionID: 2,
 				// "_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				`{
+				Doc: `{
 					"name": "Online",
 					"address": "Manning Early Access Program (MEAP)"
 				}`,
 			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
 			// Create books related to publishers, and ensure they are correctly linked (in and out of transactions).
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 0,
 				Request: `mutation {
 					create_book(data: "{\"name\": \"Book By Website\",\"rating\": 4.0, \"publisher_id\": \"bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4\"}") {
 						_key
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
 					},
 				},
 			},
-
-			{
-				TransactionId: 1,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 1,
 				Request: `mutation {
 					create_book(data: "{\"name\": \"Book By Online\",\"rating\": 4.0, \"publisher_id\": \"bae-8a381044-9206-51e7-8bc8-dc683d5f2523\"}") {
 						_key
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
 					},
 				},
 			},
-
 			// Assert publisher -> books direction within transaction 0.
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 0,
 				Request: `query {
 					book {
 						_key
@@ -239,7 +230,6 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 						}
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
@@ -251,11 +241,9 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 					},
 				},
 			},
-
 			// Assert publisher -> books direction within transaction 1.
-			{
-				TransactionId: 1,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 1,
 				Request: `query {
 					book {
 						_key
@@ -266,7 +254,6 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 						}
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
@@ -278,40 +265,47 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 					},
 				},
 			},
-		},
-
-		// Assert publishers -> books direction outside the transactions.
-		Request: `query {
-			publisher {
-				_key
-				name
-				published {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{
-			{
-				"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				"name": "Website",
-				"published": map[string]any{
-					"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-					"name": "Book By Website",
-				},
+			// Commit the transactions before querying the end result
+			testUtils.TransactionCommit{
+				TransactionID: 0,
 			},
+			testUtils.TransactionCommit{
+				TransactionID: 1,
+			},
+			testUtils.Request{
+				// Assert publishers -> books direction outside the transactions.
+				Request: `query {
+					publisher {
+						_key
+						name
+						published {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+						"name": "Website",
+						"published": map[string]any{
+							"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+							"name": "Book By Website",
+						},
+					},
 
-			{
-				"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				"name": "Online",
-				"published": map[string]any{
-					"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-					"name": "Book By Online",
+					{
+						"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
+						"name": "Online",
+						"published": map[string]any{
+							"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
+							"name": "Book By Online",
+						},
+					},
 				},
 			},
 		},
 	}
 
-	relationTests.ExecuteTestCase(t, test)
+	relationTests.Execute(t, test)
 }
