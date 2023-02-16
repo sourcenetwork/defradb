b92b6bc45c0aa597796be26ba2271c086d3d7852
AndrewSisley <a.j.sisley@hotmail.com>
2023-02-16 12:46:09 -0500

    refactor: Migrate mutation-relation tests to new framework (#1109)
    * Add mutation/relation wrapper
    
    * Migrate TestTransactionalCreationAndLinkingOfRelationalDocumentsForward
    
    * Migrate TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward
    
    * Migrate TestRelationalDeletionOfADocumentUsingSingleKey_Success
    
    * Migrate TestTxnDeletionOfRelatedDocFromPrimarySideForwardDirection
    
    * Migrate TestTxnDeletionOfRelatedDocFromPrimarySideBackwardDirection
    
    * Migrate TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnForwardDirection
    
    * Migrate TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnBackwardDirection
    
    * Migrate TestTxnDeletionOfRelatedDocFromNonPrimarySideForwardDirection
    
    * Migrate TestTxnDeletionOfRelatedDocFromNonPrimarySideBackwardDirection
    
    * Remove legacy framework wrapper

--- tests/integration/mutation/relation/create/with_txn_test.go
+++ tests/integration/mutation/relation/create/with_txn_test.go
@@ -19,64 +19,55 @@ import (
 )
 
 func TestTransactionalCreationAndLinkingOfRelationalDocumentsForward(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Create relational documents, and check the links in forward direction.",
-
-		Docs: map[int][]string{
-			// publishers
-			2: {
+		Actions: []any{
+			testUtils.CreateDoc{
+				CollectionID: 2,
 				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
+				Doc: `{
 					"name": "Website",
 					"address": "Manning Publications"
 				}`,
-
+			},
+			testUtils.CreateDoc{
+				CollectionID: 2,
 				// "_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				`{
+				Doc: `{
 					"name": "Online",
 					"address": "Manning Early Access Program (MEAP)"
 				}`,
 			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
 			// Create books related to publishers, and ensure they are correctly linked (in and out of transactions).
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 0,
 				Request: `mutation {
 					create_book(data: "{\"name\": \"Book By Website\",\"rating\": 4.0, \"publisher_id\": \"bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4\"}") {
 						_key
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
 					},
 				},
 			},
-
-			{
-				TransactionId: 1,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 1,
 				Request: `mutation {
 					create_book(data: "{\"name\": \"Book By Online\",\"rating\": 4.0, \"publisher_id\": \"bae-8a381044-9206-51e7-8bc8-dc683d5f2523\"}") {
 						_key
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
 					},
 				},
 			},
-
 			// Assert publisher -> books direction within transaction 0.
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 0,
 				Request: `query {
 					publisher {
 						_key
@@ -87,7 +78,6 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsForward(t *testing.
 						}
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
@@ -105,11 +95,9 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsForward(t *testing.
 					},
 				},
 			},
-
 			// Assert publisher -> books direction within transaction 1.
-			{
-				TransactionId: 1,
-
+			testUtils.TransactionRequest2{
+				TransactionID: 1,
 				Request: `query {
 					publisher {
 						_key
@@ -120,7 +108,6 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsForward(t *testing.
 						}
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key":      "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
@@ -138,103 +125,15 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsForward(t *testing.
 					},
 				},
 			},
-		},
-
-		// Assert books -> publisher direction outside the transactions.
-		Request: `query {
-			book {
-				_key
-				name
-				publisher {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{
-			{
-				"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-				"name": "Book By Website",
-				"publisher": map[string]any{
-					"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-					"name": "Website",
-				},
+			// Commit the transactions before querying the end result
+			testUtils.TransactionCommit{
+				TransactionID: 0,
 			},
-
-			{
-				"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-				"name": "Book By Online",
-				"publisher": map[string]any{
-					"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-					"name": "Online",
-				},
+			testUtils.TransactionCommit{
+				TransactionID: 1,
 			},
-		},
-	}
-
-	relationTests.ExecuteTestCase(t, test)
-}
-
-func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Create relational documents, and check the links in backward direction.",
-
-		Docs: map[int][]string{
-			// publishers
-			2: {
-				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
-					"name": "Website",
-					"address": "Manning Publications"
-				}`,
-
-				// "_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				`{
-					"name": "Online",
-					"address": "Manning Early Access Program (MEAP)"
-				}`,
-			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Create books related to publishers, and ensure they are correctly linked (in and out of transactions).
-			{
-				TransactionId: 0,
-
-				Request: `mutation {
-					create_book(data: "{\"name\": \"Book By Website\",\"rating\": 4.0, \"publisher_id\": \"bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4\"}") {
-						_key
-					}
-				}`,
-
-				Results: []map[string]any{
-					{
-						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-					},
-				},
-			},
-
-			{
-				TransactionId: 1,
-
-				Request: `mutation {
-					create_book(data: "{\"name\": \"Book By Online\",\"rating\": 4.0, \"publisher_id\": \"bae-8a381044-9206-51e7-8bc8-dc683d5f2523\"}") {
-						_key
-					}
-				}`,
-
-				Results: []map[string]any{
-					{
-						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-					},
-				},
-			},
-
-			// Assert publisher -> books direction within transaction 0.
-			{
-				TransactionId: 0,
-
+			testUtils.Request{
+				// Assert books -> publisher direction outside the transactions.
 				Request: `query {
 					book {
 						_key
@@ -245,7 +144,6 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 						}
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
@@ -255,25 +153,7 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 							"name": "Website",
 						},
 					},
-				},
-			},
 
-			// Assert publisher -> books direction within transaction 1.
-			{
-				TransactionId: 1,
-
-				Request: `query {
-					book {
-						_key
-						name
-						publisher {
-							_key
-							name
-						}
-					}
-				}`,
-
-				Results: []map[string]any{
 					{
 						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
 						"name": "Book By Online",
@@ -285,39 +165,147 @@ func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing
 				},
 			},
 		},
+	}
 
-		// Assert publishers -> books direction outside the transactions.
-		Request: `query {
-			publisher {
-				_key
-				name
-				published {
-					_key
-					name
-				}
-			}
-		}`,
+	relationTests.Execute(t, test)
+}
 
-		Results: []map[string]any{
-			{
-				"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				"name": "Website",
-				"published": map[string]any{
-					"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-					"name": "Book By Website",
+func TestTransactionalCreationAndLinkingOfRelationalDocumentsBackward(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Create relational documents, and check the links in backward direction.",
+		Actions: []any{
+			testUtils.CreateDoc{
+				CollectionID: 2,
+				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+				Doc: `{
+					"name": "Website",
+					"address": "Manning Publications"
+				}`,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 2,
+				// "_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
+				Doc: `{
+					"name": "Online",
+					"address": "Manning Early Access Program (MEAP)"
+				}`,
+			},
+			// Create books related to publishers, and ensure they are correctly linked (in and out of transactions).
+			testUtils.TransactionRequest2{
+				TransactionID: 0,
+				Request: `mutation {
+					create_book(data: "{\"name\": \"Book By Website\",\"rating\": 4.0, \"publisher_id\": \"bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4\"}") {
+						_key
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+					},
 				},
 			},
+			testUtils.TransactionRequest2{
+				TransactionID: 1,
+				Request: `mutation {
+					create_book(data: "{\"name\": \"Book By Online\",\"rating\": 4.0, \"publisher_id\": \"bae-8a381044-9206-51e7-8bc8-dc683d5f2523\"}") {
+						_key
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
+					},
+				},
+			},
+			// Assert publisher -> books direction within transaction 0.
+			testUtils.TransactionRequest2{
+				TransactionID: 0,
+				Request: `query {
+					book {
+						_key
+						name
+						publisher {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+						"name": "Book By Website",
+						"publisher": map[string]any{
+							"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+							"name": "Website",
+						},
+					},
+				},
+			},
+			// Assert publisher -> books direction within transaction 1.
+			testUtils.TransactionRequest2{
+				TransactionID: 1,
+				Request: `query {
+					book {
+						_key
+						name
+						publisher {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
+						"name": "Book By Online",
+						"publisher": map[string]any{
+							"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
+							"name": "Online",
+						},
+					},
+				},
+			},
+			// Commit the transactions before querying the end result
+			testUtils.TransactionCommit{
+				TransactionID: 0,
+			},
+			testUtils.TransactionCommit{
+				TransactionID: 1,
+			},
+			testUtils.Request{
+				// Assert publishers -> books direction outside the transactions.
+				Request: `query {
+					publisher {
+						_key
+						name
+						published {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+						"name": "Website",
+						"published": map[string]any{
+							"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+							"name": "Book By Website",
+						},
+					},
 
-			{
-				"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				"name": "Online",
-				"published": map[string]any{
-					"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-					"name": "Book By Online",
+					{
+						"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
+						"name": "Online",
+						"published": map[string]any{
+							"_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
+							"name": "Book By Online",
+						},
+					},
 				},
 			},
 		},
 	}
 
-	relationTests.ExecuteTestCase(t, test)
+	relationTests.Execute(t, test)
 }
--- tests/integration/mutation/relation/delete/with_txn_test.go
+++ tests/integration/mutation/relation/delete/with_txn_test.go
@@ -19,182 +19,47 @@ import (
 )
 
 func TestTxnDeletionOfRelatedDocFromPrimarySideForwardDirection(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Delete related doc with transaction from primary side (forward).",
-
-		Docs: map[int][]string{
-			// books
-			0: {
+		Actions: []any{
+			testUtils.CreateDoc{
+				// books
+				CollectionID: 0,
 				// "_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-				`{
+				Doc: `{
 					"name": "Book By Website",
 					"rating": 4.0,
 					"publisher_id": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4"
 				}`,
 			},
-
-			// publishers
-			2: {
+			testUtils.CreateDoc{
+				// publishers
+				CollectionID: 2,
 				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
+				Doc: `{
 					"name": "Website",
 					"address": "Manning Publications"
 				}`,
 			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Delete a liniked book that exists.
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				// Delete a linked book that exists.
+				TransactionID: 0,
 				Request: `mutation {
 			        delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
 			            _key
 			        }
 			    }`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
 					},
 				},
 			},
-		},
-
-		// Assert after transaction(s) have been commited, to ensure the book was deleted.
-		Request: `query {
-			publisher {
-				_key
-				name
-				published {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{
-			{
-				"_key":      "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				"name":      "Website",
-				"published": nil,
+			testUtils.TransactionCommit{
+				TransactionID: 0,
 			},
-		},
-	}
-
-	relationTests.ExecuteTestCase(t, test)
-}
-
-func TestTxnDeletionOfRelatedDocFromPrimarySideBackwardDirection(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Delete related doc with transaction from primary side (backward).",
-
-		Docs: map[int][]string{
-			// books
-			0: {
-				// "_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-				`{
-					"name": "Book By Website",
-					"rating": 4.0,
-					"publisher_id": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4"
-				}`,
-			},
-
-			// publishers
-			2: {
-				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
-					"name": "Website",
-					"address": "Manning Publications"
-				}`,
-			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Delete a liniked book that exists.
-			{
-				TransactionId: 0,
-
-				Request: `mutation {
-			        delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
-			            _key
-			        }
-			    }`,
-
-				Results: []map[string]any{
-					{
-						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-					},
-				},
-			},
-		},
-
-		// Assert after transaction(s) have been commited, to ensure the book was deleted.
-		Request: `query {
-			book {
-				_key
-				name
-				publisher {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{},
-	}
-
-	relationTests.ExecuteTestCase(t, test)
-}
-
-func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnForwardDirection(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Transaction can read a record that was deleted in a non-commited transaction (forward).",
-
-		Docs: map[int][]string{
-			// books
-			0: {
-				// "_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-				`{
-					"name": "Book By Website",
-					"rating": 4.0,
-					"publisher_id": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4"
-				}`,
-			},
-
-			// publishers
-			2: {
-				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
-					"name": "Website",
-					"address": "Manning Publications"
-				}`,
-			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Delete a liniked book that exists in transaction 0.
-			{
-				TransactionId: 0,
-
-				Request: `mutation {
-			        delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
-			            _key
-			        }
-			    }`,
-
-				Results: []map[string]any{
-					{
-						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-					},
-				},
-			},
-
-			// Read the book (forward) that was deleted (in the non-commited transaction) in another transaction.
-			{
-				TransactionId: 1,
-
+			testUtils.Request{
+				// Assert after transaction(s) have been commited, to ensure the book was deleted.
 				Request: `query {
 					publisher {
 						_key
@@ -205,7 +70,130 @@ func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnForwardDirection(t *tes
 						}
 					}
 				}`,
+				Results: []map[string]any{
+					{
+						"_key":      "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+						"name":      "Website",
+						"published": nil,
+					},
+				},
+			},
+		},
+	}
 
+	relationTests.Execute(t, test)
+}
+
+func TestTxnDeletionOfRelatedDocFromPrimarySideBackwardDirection(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Delete related doc with transaction from primary side (backward).",
+		Actions: []any{
+			testUtils.CreateDoc{
+				// books
+				CollectionID: 0,
+				// "_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+				Doc: `{
+					"name": "Book By Website",
+					"rating": 4.0,
+					"publisher_id": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4"
+				}`,
+			},
+			testUtils.CreateDoc{
+				// publishers
+				CollectionID: 2,
+				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+				Doc: `{
+					"name": "Website",
+					"address": "Manning Publications"
+				}`,
+			},
+			testUtils.TransactionRequest2{
+				// Delete a linked book that exists.
+				TransactionID: 0,
+				Request: `mutation {
+			        delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
+			            _key
+			        }
+			    }`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+					},
+				},
+			},
+			testUtils.TransactionCommit{
+				TransactionID: 0,
+			},
+			testUtils.Request{
+				// Assert after transaction(s) have been commited, to ensure the book was deleted.
+				Request: `query {
+					book {
+						_key
+						name
+						publisher {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{},
+			},
+		},
+	}
+
+	relationTests.Execute(t, test)
+}
+
+func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnForwardDirection(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Transaction can read a record that was deleted in a non-commited transaction (forward).",
+		Actions: []any{
+			testUtils.CreateDoc{
+				// books
+				CollectionID: 0,
+				// "_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+				Doc: `{
+					"name": "Book By Website",
+					"rating": 4.0,
+					"publisher_id": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4"
+				}`,
+			},
+			testUtils.CreateDoc{
+				// publishers
+				CollectionID: 2,
+				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+				Doc: `{
+					"name": "Website",
+					"address": "Manning Publications"
+				}`,
+			},
+			testUtils.TransactionRequest2{
+				// Delete a linked book that exists.
+				TransactionID: 0,
+				Request: `mutation {
+			        delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
+			            _key
+			        }
+			    }`,
+				Results: []map[string]any{
+					{
+						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
+					},
+				},
+			},
+			testUtils.TransactionRequest2{
+				// Read the book (forward) that was deleted (in the non-commited transaction) in another transaction.
+				TransactionID: 1,
+				Request: `query {
+					publisher {
+						_key
+						name
+						published {
+							_key
+							name
+						}
+					}
+				}`,
 				Results: []map[string]any{
 					{
 						"_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
@@ -217,79 +205,75 @@ func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnForwardDirection(t *tes
 					},
 				},
 			},
-		},
-
-		// Assert after transaction(s) have been commited, to ensure the book was deleted.
-		Request: `query {
-			publisher {
-				_key
-				name
-				published {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{
-			{
-				"_key":      "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				"name":      "Website",
-				"published": nil,
+			testUtils.TransactionCommit{
+				TransactionID: 0,
+			},
+			testUtils.Request{
+				// Assert after transaction(s) have been commited, to ensure the book was deleted.
+				Request: `query {
+					publisher {
+						_key
+						name
+						published {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key":      "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
+						"name":      "Website",
+						"published": nil,
+					},
+				},
 			},
 		},
 	}
 
-	relationTests.ExecuteTestCase(t, test)
+	relationTests.Execute(t, test)
 }
 
 func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnBackwardDirection(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Transaction can read a record that was deleted in a non-commited transaction (backward).",
-
-		Docs: map[int][]string{
-			// books
-			0: {
+		Actions: []any{
+			testUtils.CreateDoc{
+				// books
+				CollectionID: 0,
 				// "_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
-				`{
+				Doc: `{
 					"name": "Book By Website",
 					"rating": 4.0,
 					"publisher_id": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4"
 				}`,
 			},
-
-			// publishers
-			2: {
+			testUtils.CreateDoc{
+				// publishers
+				CollectionID: 2,
 				// "_key": "bae-0e7c3bb5-4917-5d98-9fcf-b9db369ea6e4",
-				`{
+				Doc: `{
 					"name": "Website",
 					"address": "Manning Publications"
 				}`,
 			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Delete a liniked book that exists in transaction 0.
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				// Delete a linked book that exists in transaction 0.
+				TransactionID: 0,
 				Request: `mutation {
-					delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
-						_key
-					}
+			        delete_book(id: "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722") {
+			            _key
+			        }
 			    }`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
 					},
 				},
 			},
-
-			// Read the book (backwards) that was deleted (in the non-commited transaction) in another transaction.
-			{
-				TransactionId: 1,
-
+			testUtils.TransactionRequest2{
+				// Read the book (backwards) that was deleted (in the non-commited transaction) in another transaction.
+				TransactionID: 1,
 				Request: `query {
 					book {
 						_key
@@ -300,7 +284,6 @@ func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnBackwardDirection(t *te
 						}
 					}
 				}`,
-
 				Results: []map[string]any{
 					{
 						"_key": "bae-5b16ccd7-9cae-5145-a56c-03cfe7787722",
@@ -312,154 +295,153 @@ func TestATxnCanReadARecordThatIsDeletedInANonCommitedTxnBackwardDirection(t *te
 					},
 				},
 			},
+			testUtils.TransactionCommit{
+				TransactionID: 0,
+			},
+			testUtils.Request{
+				// Assert after transaction(s) have been commited, to ensure the book was deleted.
+				Request: `query {
+					book {
+						_key
+						name
+						publisher {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{},
+			},
 		},
-
-		// Assert after transaction(s) have been commited, to ensure the book was deleted.
-		Request: `query {
-			book {
-				_key
-				name
-				publisher {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{},
 	}
 
-	relationTests.ExecuteTestCase(t, test)
+	relationTests.Execute(t, test)
 }
 
 func TestTxnDeletionOfRelatedDocFromNonPrimarySideForwardDirection(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Delete related doc with transaction from non-primary side (forward).",
-
-		Docs: map[int][]string{
-			// books
-			0: {
+		Actions: []any{
+			testUtils.CreateDoc{
+				// books
+				CollectionID: 0,
 				// "_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-				`{
+				Doc: `{
 					"name": "Book By Online",
 					"rating": 4.0,
 					"publisher_id": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523"
 				}`,
 			},
-
-			// publishers
-			2: {
+			testUtils.CreateDoc{
+				// publishers
+				CollectionID: 2,
 				// "_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				`{
+				Doc: `{
 					"name": "Online",
 					"address": "Manning Early Access Program (MEAP)"
 				}`,
 			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Delete a publisher and outside the transaction ensure it's linked
-			// book gets correctly unlinked too.
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				// Delete a publisher and outside the transaction ensure it's linked
+				// book gets correctly unlinked too.
+				TransactionID: 0,
 				Request: `mutation {
-					delete_publisher(id: "bae-8a381044-9206-51e7-8bc8-dc683d5f2523") {
-						_key
-					}
-				}`,
-
+			        delete_publisher(id: "bae-8a381044-9206-51e7-8bc8-dc683d5f2523") {
+			            _key
+			        }
+			    }`,
 				Results: []map[string]any{
 					{
 						"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
 					},
 				},
 			},
+			testUtils.TransactionCommit{
+				TransactionID: 0,
+			},
+			testUtils.Request{
+				// Assert after transaction(s) have been commited.
+				Request: `query {
+					publisher {
+						_key
+						name
+						published {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{},
+			},
 		},
-
-		// Assert after transaction(s) have been commited.
-		Request: `query {
-			publisher {
-				_key
-				name
-				published {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{},
 	}
 
-	relationTests.ExecuteTestCase(t, test)
+	relationTests.Execute(t, test)
 }
 
 func TestTxnDeletionOfRelatedDocFromNonPrimarySideBackwardDirection(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Delete related doc with transaction from non-primary side (backward).",
-
-		Docs: map[int][]string{
-			// books
-			0: {
+		Actions: []any{
+			testUtils.CreateDoc{
+				// books
+				CollectionID: 0,
 				// "_key": "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-				`{
+				Doc: `{
 					"name": "Book By Online",
 					"rating": 4.0,
 					"publisher_id": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523"
 				}`,
 			},
-
-			// publishers
-			2: {
+			testUtils.CreateDoc{
+				// publishers
+				CollectionID: 2,
 				// "_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
-				`{
+				Doc: `{
 					"name": "Online",
 					"address": "Manning Early Access Program (MEAP)"
 				}`,
 			},
-		},
-
-		TransactionalRequests: []testUtils.TransactionRequest{
-			// Delete a publisher and outside the transaction ensure it's linked
-			// book gets correctly unlinked too.
-			{
-				TransactionId: 0,
-
+			testUtils.TransactionRequest2{
+				// Delete a publisher and outside the transaction ensure it's linked
+				// book gets correctly unlinked too.
+				TransactionID: 0,
 				Request: `mutation {
-					delete_publisher(id: "bae-8a381044-9206-51e7-8bc8-dc683d5f2523") {
-						_key
-					}
-				}`,
-
+			        delete_publisher(id: "bae-8a381044-9206-51e7-8bc8-dc683d5f2523") {
+			            _key
+			        }
+			    }`,
 				Results: []map[string]any{
 					{
 						"_key": "bae-8a381044-9206-51e7-8bc8-dc683d5f2523",
 					},
 				},
 			},
-		},
-
-		// Assert after transaction(s) have been commited.
-		Request: `query {
-			book {
-				_key
-				name
-				publisher {
-					_key
-					name
-				}
-			}
-		}`,
-
-		Results: []map[string]any{
-			{
-				"_key":      "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
-				"name":      "Book By Online",
-				"publisher": nil,
+			testUtils.TransactionCommit{
+				TransactionID: 0,
+			},
+			testUtils.Request{
+				// Assert after transaction(s) have been commited.
+				Request: `query {
+					book {
+						_key
+						name
+						publisher {
+							_key
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"_key":      "bae-edf7f0fc-f0fd-57e2-b695-569d87e1b251",
+						"name":      "Book By Online",
+						"publisher": nil,
+					},
+				},
 			},
 		},
 	}
 
-	relationTests.ExecuteTestCase(t, test)
+	relationTests.Execute(t, test)
 }
