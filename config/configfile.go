d5a545a337abe496775dfde16b1cc899edb3a3f7
Orpheus Lummis <o@orpheuslummis.info>
2023-02-20 09:59:04 -0500

    configfile template to a separate file (approximate commit)

--- config/configfile.go
+++ config/configfile.go
@@ -22,7 +22,13 @@ const (
 	defaultConfigFilePerm        = 0o644
 )
 
-func (cfg *Config) writeConfigFile(path string) error {
+// defaultConfigTemplate must reflect the Config struct in content and configuration.
+// All parameters must be represented here, to support Viper's automatic environment variable handling.
+// We embed the default config template for clarity and to avoid autoformatters from breaking it.
+//
+//go:embed configfile_yaml.gotmpl
+var defaultConfigTemplate string
+
 	buffer, err := cfg.toBytes()
 	if err != nil {
 		return err
@@ -39,71 +45,33 @@ func (cfg *Config) WriteConfigFileToRootDir(rootDir string) error {
 	return cfg.writeConfigFile(path)
 }
 
-// defaultConfigTemplate must reflect Config in content and configuration.
-// All parameters must be represented here, to support Viper's automatic environment variable handling.
-const defaultConfigTemplate = `# DefraDB configuration (YAML)
+func (cfg *Config) CreateRootDirAndConfigFile() error {
+	if err := os.MkdirAll(cfg.Rootdir, defaultDirPerm); err != nil {
+		return err
+	}
+	log.FeedbackInfo(context.Background(), fmt.Sprintf("Created DefraDB root directory at %v", cfg.Rootdir))
+	if err := cfg.WriteConfigFile(); err != nil {
+		return err
+	}
+	return nil
+}
 
-# NOTE: Paths below are relative to the DefraDB directory.
-# By default, the DefraDB directory is "$HOME/.defradb", but
-# can be changed via the $DEFRA_ROOT env variable or --rootdir CLI flag.
+func DefaultRootDir() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.FatalE(context.Background(), "error determining user directory", err)
+	}
+	return filepath.Join(home, ".defradb")
+}
 
-datastore:
-  # Store can be badger | memory
-    #   badger: fast pure Go key-value store optimized for SSDs (https://github.com/dgraph-io/badger)
-    #   memory: in-memory version of badger
-  store: {{ .Datastore.Store }}
-  badger:
-    path: {{ .Datastore.Badger.Path }}
-    # Maximum file size of the value log files. The in-memory file size will be 2*valuelogfilesize.
-    # Human friendly units can be used (ex: 500MB).
-    valuelogfilesize: {{ .Datastore.Badger.ValueLogFileSize }}
-  maxtxnretries: {{ .Datastore.MaxTxnRetries }}
-  # memory:
-  #    size: {{ .Datastore.Memory.Size }}
+func FileExists(filePath string) bool {
+	statInfo, err := os.Stat(filePath)
+	existsAsFile := (err == nil && !statInfo.IsDir())
+	return existsAsFile
+}
 
-api:
-  # Address of the HTTP API to listen on or connect to
-  address: {{ .API.Address }}
-  # Whether the API server should listen over HTTPS
-  tls: {{ .API.TLS }}
-  # The path to the public key file. Ignored if domains is set.
-  pubkeypath: {{ .API.PubKeyPath }}
-  # The path to the private key file. Ignored if domains is set.
-  privkeypath: {{ .API.PrivKeyPath }}
-  # Email address to let the CA (Let's Encrypt) send notifications via email when there are issues (optional).
-  # email: {{ .API.Email }}
-
-net:
-  # Whether the P2P is disabled
-  p2pdisabled: {{ .Net.P2PDisabled }}
-  # Listening address of the P2P network
-  p2paddress: {{ .Net.P2PAddress }}
-  # Listening address of the RPC endpoint
-  rpcaddress: {{ .Net.RPCAddress }}
-  # gRPC server address
-  tcpaddress: {{ .Net.TCPAddress }}
-  # Time duration after which a RPC connection to a peer times out
-  rpctimeout: {{ .Net.RPCTimeout }}
-  # Whether the node has pubsub enabled or not
-  pubsub: {{ .Net.PubSubEnabled }}
-  # Enable libp2p's Circuit relay transport protocol https://docs.libp2p.io/concepts/circuit-relay/
-  relay: {{ .Net.RelayEnabled }}
-  # List of peers to boostrap with, specified as multiaddresses (https://docs.libp2p.io/concepts/addressing/)
-  peers: {{ .Net.Peers }}
-  # Amount of time after which an idle RPC connection would be closed
-  RPCMaxConnectionIdle: {{ .Net.RPCMaxConnectionIdle }}
-
-log:
-  # Log level. Options are debug, info, error, fatal
-  level: {{ .Log.Level }}
-  # Include stacktrace in error and fatal logs
-  stacktrace: {{ .Log.Stacktrace }}
-  # Supported log formats are json, csv
-  format: {{ .Log.Format }}
-  # Where the log output is written to
-  output: {{ .Log.Output }}
-  # Disable colored log output
-  nocolor: {{ .Log.NoColor }}
-  # Caller location in log output
-  caller: {{ .Log.Caller }}
-`
+func FolderExists(folderPath string) bool {
+	statInfo, err := os.Stat(folderPath)
+	existsAsFolder := (err == nil && statInfo.IsDir())
+	return existsAsFolder
+}
