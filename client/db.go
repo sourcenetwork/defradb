6323e22fbb44b27054a0a8bd5547c13fec633808
Andrew Sisley <a.j.sisley@hotmail.com>
2023-03-07 12:32:55 -0500

    WIP BREAKING - Rework client interfaces to wrap transactions

--- client/db.go
+++ client/db.go
@@ -20,6 +20,23 @@ import (
 )
 
 type DB interface {
+	CreateCollection(context.Context, CollectionDescription) (Collection, error)
+
+	GetCollectionByName(context.Context, string) (Collection, error)
+	GetCollectionBySchemaID(context.Context, string) (Collection, error)
+	GetAllCollections(context.Context) ([]Collection, error)
+
+	ExecRequest(context.Context, string) *RequestResult
+}
+
+type DBImmediate interface { //name??
+	DB
+	// P2PImmediate holds the P2P related methods that must be implemented by the database.
+	P2PImmediate
+
+	WithTxn(context.Context, bool) (DBTxn, error)
+	WithConcurrentTxn(context.Context, bool) (DBTxn, error)
+
 	AddSchema(context.Context, string) error
 
 	// PatchSchema takes the given JSON patch string and applies it to the set of CollectionDescriptions
@@ -35,10 +52,27 @@ type DB interface {
 	// will be applied.
 	PatchSchema(context.Context, string) error
 
-	CreateCollection(context.Context, CollectionDescription) (Collection, error)
-	CreateCollectionTxn(context.Context, datastore.Txn, CollectionDescription) (Collection, error)
+	Root() datastore.RootStore
+	Blockstore() blockstore.Blockstore
+	Events() events.Events
+	MaxTxnRetries() int
 
-	// UpdateCollectionTxn updates the persisted collection description matching the name of the given
+	PrintDump(ctx context.Context) error
+
+	Close(context.Context)
+}
+
+type DefraTxn interface {
+	Commit(context.Context) error
+	Discard(context.Context)
+}
+
+type DBTxn interface {
+	DB
+	P2PTxn
+	DefraTxn
+
+	// UpdateCollection updates the persisted collection description matching the name of the given
 	// description, to the values in the given description.
 	//
 	// It will validate the given description using [ValidateUpdateCollectionTxn] before updating it.
@@ -46,38 +80,13 @@ type DB interface {
 	// The collection (including the schema version ID) will only be updated if any changes have actually
 	// been made, if the given description matches the current persisted description then no changes will be
 	// applied.
-	UpdateCollectionTxn(context.Context, datastore.Txn, CollectionDescription) (Collection, error)
+	UpdateCollection(context.Context, CollectionDescription) (Collection, error)
 
-	// ValidateUpdateCollectionTxn validates that the given collection description is a valid update.
+	// ValidateUpdateCollection validates that the given collection description is a valid update.
 	//
 	// Will return true if the given desctiption differs from the current persisted state of the
 	// collection. Will return an error if it fails validation.
-	ValidateUpdateCollectionTxn(context.Context, datastore.Txn, CollectionDescription) (bool, error)
-
-	GetCollectionByName(context.Context, string) (Collection, error)
-	GetCollectionByNameTxn(context.Context, datastore.Txn, string) (Collection, error)
-	GetCollectionBySchemaID(context.Context, string) (Collection, error)
-	GetCollectionBySchemaIDTxn(context.Context, datastore.Txn, string) (Collection, error)
-	GetAllCollections(context.Context) ([]Collection, error)
-	GetAllCollectionsTxn(context.Context, datastore.Txn) ([]Collection, error)
-
-	Root() datastore.RootStore
-	Blockstore() blockstore.Blockstore
-
-	NewTxn(context.Context, bool) (datastore.Txn, error)
-	NewConcurrentTxn(context.Context, bool) (datastore.Txn, error)
-	ExecRequest(context.Context, string) *RequestResult
-	ExecTransactionalRequest(context.Context, string, datastore.Txn) *RequestResult
-	Close(context.Context)
-
-	Events() events.Events
-
-	MaxTxnRetries() int
-
-	PrintDump(ctx context.Context) error
-
-	// P2P holds the P2P related methods that must be implemented by the database.
-	P2P
+	ValidateUpdateCollection(context.Context, CollectionDescription) (bool, error)
 }
 
 type GQLResult struct {
