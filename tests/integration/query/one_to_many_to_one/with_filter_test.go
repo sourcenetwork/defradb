e89d3d12b275d9ff5e5b2132c943cc1a0468c849
Islam Aliev <islam@source.network>
2023-03-14 17:39:21 +0100

    fix: Resolve aggregates' mapping with deep nested subtypes (#1175)
    * Fix recursive call to CloneWithoutRender
    * Fix deep orderby
    * Map properly aggregates with deep filter
    * Flatten test actions
      Update new test framework so that it allows to adding slices to the slice of actions
    * Switch to new testing framework
      Make all test in tests/integration/query/one_to_many_to_one use new testing framework

--- tests/integration/query/one_to_many_to_one/with_filter_test.go
+++ tests/integration/query/one_to_many_to_one/with_filter_test.go
@@ -17,88 +17,105 @@ import (
 )
 
 func TestQueryComplexWithDeepFilterOnRenderedChildren(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "One-to-many-to-one deep filter on rendered children.",
-		Request: `query {
-			Author (filter: {book: {publisher: {yearOpened: {_gt: 2021}}}}) {
-				name
-				book {
-					publisher {
-						yearOpened
-					}
-				}
-			}
-		}`,
-
-		Docs: map[int][]string{
+		Actions: []any{
+			gqlSchemaOneToManyToOne(),
 			// Authors
-			0: {
+			testUtils.CreateDoc{
+				CollectionID: 0,
 				// bae-41598f0c-19bc-5da6-813b-e80f14a10df3, Has written 5 books
-				`{
+				Doc: `{
 					"name": "John Grisham",
 					"age": 65,
 					"verified": true
 				}`,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 0,
 				// bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04, Has written 1 book
-				`{
+				Doc: `{
 					"name": "Cornelia Funke",
 					"age": 62,
 					"verified": false
 				}`,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 0,
 				// Has written no book
-				`{
+				Doc: `{
 					"name": "Not a Writer",
 					"age": 6,
 					"verified": false
 				}`,
 			},
-
 			// Books
-			1: {
+			testUtils.CreateDoc{
+				CollectionID: 1,
 				// "bae-b6c078f2-3427-5b99-bafd-97dcd7c2e935", Has 1 publisher
-				`{
+				Doc: `{
 					"name": "The Rooster Bar",
 					"rating": 4,
 					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
 				}`,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 1,
 				// "bae-b8091c4f-7594-5d7a-98e8-272aadcedfdf", Has 1 publisher
-				`{
+				Doc: `{
 					"name": "Theif Lord",
 					"rating": 4.8,
 					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
 				}`,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 1,
 				// "bae-4fb9e3e9-d1d3-5404-bf15-10e4c995d9ca", Has no publisher.
-				`{
+				Doc: `{
 					"name": "The Associate",
 					"rating": 4.2,
 					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
 				}`,
 			},
-
 			// Publishers
-			2: {
-				`{
+			testUtils.CreateDoc{
+				CollectionID: 2,
+				Doc: `{
 					"name": "Only Publisher of The Rooster Bar",
 					"address": "1 Rooster Ave., Waterloo, Ontario",
 					"yearOpened": 2022,
 					"book_id": "bae-b6c078f2-3427-5b99-bafd-97dcd7c2e935"
 			    }`,
-				`{
+			},
+			testUtils.CreateDoc{
+				CollectionID: 2,
+				Doc: `{
 					"name": "Only Publisher of Theif Lord",
 					"address": "1 Theif Lord, Waterloo, Ontario",
 					"yearOpened": 2020,
 					"book_id": "bae-b8091c4f-7594-5d7a-98e8-272aadcedfdf"
 			    }`,
 			},
-		},
-
-		Results: []map[string]any{
-			{
-				"name": "Cornelia Funke",
-				"book": []map[string]any{
+			testUtils.Request{
+				Request: `query {
+					Author (filter: {book: {publisher: {yearOpened: {_gt: 2021}}}}) {
+						name
+						book {
+							publisher {
+								yearOpened
+							}
+						}
+					}
+				}`,
+				Results: []map[string]any{
 					{
-						"publisher": map[string]any{
-							"yearOpened": uint64(2022),
+						"name": "Cornelia Funke",
+						"book": []map[string]any{
+							{
+								"publisher": map[string]any{
+									"yearOpened": uint64(2022),
+								},
+							},
 						},
 					},
 				},
@@ -106,5 +123,93 @@ func TestQueryComplexWithDeepFilterOnRenderedChildren(t *testing.T) {
 		},
 	}
 
-	executeTestCase(t, test)
+	testUtils.ExecuteTestCase(t, []string{"Author", "Book", "Publisher"}, test)
+}
+
+func TestOneToManyToOneWithSumOfDeepFilterSubTypeOfBothDescAndAsc(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "1-N-1 sums of deep filter subtypes of both descending and ascending.",
+		Actions: []any{
+			gqlSchemaOneToManyToOne(),
+			createDocsWith6BooksAnd5Publishers(),
+			testUtils.Request{
+				Request: `query {
+					Author {
+						name
+						s1: _sum(book: {field: rating, filter: {publisher: {yearOpened: {_eq: 2013}}}})
+						s2: _sum(book: {field: rating, filter: {publisher: {yearOpened: {_ge: 2020}}}})
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"name": "John Grisham",
+						// 'Theif Lord' (4.8 rating) 2020, then 'A Time for Mercy' 2013 (4.5 rating).
+						"s1": 4.5,
+						// 'The Associate' as it has no publisher (4.2 rating), then 'Painted House' 1995 (4.9 rating).
+						"s2": 4.8,
+					},
+					{
+						"name": "Not a Writer",
+						"s1":   0.0,
+						"s2":   0.0,
+					},
+					{
+						"name": "Cornelia Funke",
+						"s1":   0.0,
+						"s2":   4.0,
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, []string{"Author", "Book", "Publisher"}, test)
+}
+
+func TestOneToManyToOneWithSumOfDeepFilterSubTypeAndDeepOrderBySubtypeOppositeDirections(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "1-N-1 sum of deep filter subtypes and non-sum deep filter",
+		Actions: []any{
+			gqlSchemaOneToManyToOne(),
+			createDocsWith6BooksAnd5Publishers(),
+			testUtils.Request{
+				Request: `query {
+					Author {
+						name
+						s1: _sum(book: {field: rating, filter: {publisher: {yearOpened: {_eq: 2013}}}})
+						Publishers2020: book(filter: {publisher: {yearOpened: {_ge: 2020}}}) {
+							name
+						}
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"name": "John Grisham",
+						"s1":   4.5,
+						"Publishers2020": []map[string]any{
+							{
+								"name": "Theif Lord",
+							},
+						},
+					},
+					{
+						"name":           "Not a Writer",
+						"s1":             0.0,
+						"Publishers2020": []map[string]any{},
+					},
+					{
+						"name": "Cornelia Funke",
+						"s1":   0.0,
+						"Publishers2020": []map[string]any{
+							{
+								"name": "The Rooster Bar",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, []string{"Author", "Book", "Publisher"}, test)
 }
--- tests/integration/query/one_to_many_to_one/with_order_test.go
+++ tests/integration/query/one_to_many_to_one/with_order_test.go
@@ -17,9 +17,13 @@ import (
 )
 
 func TestMultipleOrderByWithDepthGreaterThanOne(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Multiple orderby with depth greater than 1.",
-		Request: `query {
+		Actions: []any{
+			gqlSchemaOneToManyToOne(),
+			createDocsWith6BooksAnd5Publishers(),
+			testUtils.Request{
+				Request: `query {
 			Book (order: {rating: ASC, publisher: {yearOpened: DESC}}) {
 				name
 				rating
@@ -29,160 +33,68 @@ func TestMultipleOrderByWithDepthGreaterThanOne(t *testing.T) {
 				}
 			}
 		}`,
-
-		Docs: map[int][]string{
-			// Authors
-			0: {
-				// bae-41598f0c-19bc-5da6-813b-e80f14a10df3, Has written 5 books
-				`{
-					"name": "John Grisham",
-					"age": 65,
-					"verified": true
-				}`,
-				// bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04, Has written 1 book
-				`{
-					"name": "Cornelia Funke",
-					"age": 62,
-					"verified": false
-				}`,
-				// Has written no book
-				`{
-					"name": "Not a Writer",
-					"age": 6,
-					"verified": false
-				}`,
-			},
-
-			// Books
-			1: {
-				// "bae-b6c078f2-3427-5b99-bafd-97dcd7c2e935", Has 1 publisher
-				`{
-					"name": "The Rooster Bar",
-					"rating": 4,
-					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
-				}`,
-				// "bae-b8091c4f-7594-5d7a-98e8-272aadcedfdf", Has 1 publisher
-				`{
-					"name": "Theif Lord",
-					"rating": 4.8,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-4fb9e3e9-d1d3-5404-bf15-10e4c995d9ca", Has no publisher.
-				`{
-					"name": "The Associate",
-					"rating": 4.2,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-b9b83269-1f28-5c3b-ae75-3fb4c00d559d", Has 1 publisher
-				`{
-					"name": "Painted House",
-					"rating": 4.9,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-c674e3b0-ebb6-5b89-bfa3-d1128288d21a", Has 1 publisher
-				`{
-					"name": "A Time for Mercy",
-					"rating": 4.5,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-7ba73251-c935-5f44-ac04-d2061149cc14", Has 1 Publisher
-				`{
-					"name": "Sooley",
-					"rating": 3.2,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-			},
-
-			// Publishers
-			2: {
-				`{
-					"name": "Only Publisher of The Rooster Bar",
-					"address": "1 Rooster Ave., Waterloo, Ontario",
-					"yearOpened": 2022,
-					"book_id": "bae-b6c078f2-3427-5b99-bafd-97dcd7c2e935"
-			    }`,
-				`{
-					"name": "Only Publisher of Theif Lord",
-					"address": "1 Theif Lord, Waterloo, Ontario",
-					"yearOpened": 2020,
-					"book_id": "bae-b8091c4f-7594-5d7a-98e8-272aadcedfdf"
-			    }`,
-				`{
-					"name": "Only Publisher of Painted House",
-					"address": "600 Madison Ave., New York, New York",
-					"yearOpened": 1995,
-					"book_id": "bae-b9b83269-1f28-5c3b-ae75-3fb4c00d559d"
-			    }`,
-				`{
-					"name": "Only Publisher of A Time for Mercy",
-					"address": "123 Andrew Street, Flin Flon, Manitoba",
-					"yearOpened": 2013,
-					"book_id": "bae-c674e3b0-ebb6-5b89-bfa3-d1128288d21a"
-			    }`,
-				`{
-					"name": "Only Publisher of Sooley",
-					"address": "11 Sooley Ave., Waterloo, Ontario",
-					"yearOpened": 1999,
-					"book_id": "bae-7ba73251-c935-5f44-ac04-d2061149cc14"
-			    }`,
-			},
-		},
-		Results: []map[string]any{
-			{
-				"name":   "Sooley",
-				"rating": 3.2,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of Sooley",
-					"yearOpened": uint64(1999),
-				},
-			},
-			{
-				"name":   "The Rooster Bar",
-				"rating": 4.0,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of The Rooster Bar",
-					"yearOpened": uint64(2022),
-				},
-			},
-			{
-				"name":      "The Associate",
-				"rating":    4.2,
-				"publisher": nil,
-			},
-			{
-				"name":   "A Time for Mercy",
-				"rating": 4.5,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of A Time for Mercy",
-					"yearOpened": uint64(2013),
-				},
-			},
-			{
-				"name":   "Theif Lord",
-				"rating": 4.8,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of Theif Lord",
-					"yearOpened": uint64(2020),
-				},
-			},
-			{
-				"name":   "Painted House",
-				"rating": 4.9,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of Painted House",
-					"yearOpened": uint64(1995),
+				Results: []map[string]any{
+					{
+						"name":   "Sooley",
+						"rating": 3.2,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of Sooley",
+							"yearOpened": uint64(1999),
+						},
+					},
+					{
+						"name":   "The Rooster Bar",
+						"rating": 4.0,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of The Rooster Bar",
+							"yearOpened": uint64(2022),
+						},
+					},
+					{
+						"name":      "The Associate",
+						"rating":    4.2,
+						"publisher": nil,
+					},
+					{
+						"name":   "A Time for Mercy",
+						"rating": 4.5,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of A Time for Mercy",
+							"yearOpened": uint64(2013),
+						},
+					},
+					{
+						"name":   "Theif Lord",
+						"rating": 4.8,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of Theif Lord",
+							"yearOpened": uint64(2020),
+						},
+					},
+					{
+						"name":   "Painted House",
+						"rating": 4.9,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of Painted House",
+							"yearOpened": uint64(1995),
+						},
+					},
 				},
 			},
 		},
 	}
 
-	executeTestCase(t, test)
+	testUtils.ExecuteTestCase(t, []string{"Author", "Book", "Publisher"}, test)
 }
 
 func TestMultipleOrderByWithDepthGreaterThanOneOrderSwitched(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Multiple orderby with depth greater than 1, order switched.",
-		Request: `query {
+		Actions: []any{
+			gqlSchemaOneToManyToOne(),
+			createDocsWith6BooksAnd5Publishers(),
+			testUtils.Request{
+				Request: `query {
 			Book (order: {publisher: {yearOpened: DESC}, rating: ASC}) {
 				name
 				rating
@@ -192,152 +104,56 @@ func TestMultipleOrderByWithDepthGreaterThanOneOrderSwitched(t *testing.T) {
 				}
 			}
 		}`,
-
-		Docs: map[int][]string{
-			// Authors
-			0: {
-				// bae-41598f0c-19bc-5da6-813b-e80f14a10df3, Has written 5 books
-				`{
-					"name": "John Grisham",
-					"age": 65,
-					"verified": true
-				}`,
-				// bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04, Has written 1 book
-				`{
-					"name": "Cornelia Funke",
-					"age": 62,
-					"verified": false
-				}`,
-				// Has written no book
-				`{
-					"name": "Not a Writer",
-					"age": 6,
-					"verified": false
-				}`,
-			},
-
-			// Books
-			1: {
-				// "bae-b6c078f2-3427-5b99-bafd-97dcd7c2e935", Has 1 publisher
-				`{
-					"name": "The Rooster Bar",
-					"rating": 4,
-					"author_id": "bae-b769708d-f552-5c3d-a402-ccfd7ac7fb04"
-				}`,
-				// "bae-b8091c4f-7594-5d7a-98e8-272aadcedfdf", Has 1 publisher
-				`{
-					"name": "Theif Lord",
-					"rating": 4.8,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-4fb9e3e9-d1d3-5404-bf15-10e4c995d9ca", Has no publisher.
-				`{
-					"name": "The Associate",
-					"rating": 4.2,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-b9b83269-1f28-5c3b-ae75-3fb4c00d559d", Has 1 publisher
-				`{
-					"name": "Painted House",
-					"rating": 4.9,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-c674e3b0-ebb6-5b89-bfa3-d1128288d21a", Has 1 publisher
-				`{
-					"name": "A Time for Mercy",
-					"rating": 4.5,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-				// "bae-7ba73251-c935-5f44-ac04-d2061149cc14", Has 1 Publisher
-				`{
-					"name": "Sooley",
-					"rating": 3.2,
-					"author_id": "bae-41598f0c-19bc-5da6-813b-e80f14a10df3"
-				}`,
-			},
-
-			// Publishers
-			2: {
-				`{
-					"name": "Only Publisher of The Rooster Bar",
-					"address": "1 Rooster Ave., Waterloo, Ontario",
-					"yearOpened": 2022,
-					"book_id": "bae-b6c078f2-3427-5b99-bafd-97dcd7c2e935"
-			    }`,
-				`{
-					"name": "Only Publisher of Theif Lord",
-					"address": "1 Theif Lord, Waterloo, Ontario",
-					"yearOpened": 2020,
-					"book_id": "bae-b8091c4f-7594-5d7a-98e8-272aadcedfdf"
-			    }`,
-				`{
-					"name": "Only Publisher of Painted House",
-					"address": "600 Madison Ave., New York, New York",
-					"yearOpened": 1995,
-					"book_id": "bae-b9b83269-1f28-5c3b-ae75-3fb4c00d559d"
-			    }`,
-				`{
-					"name": "Only Publisher of A Time for Mercy",
-					"address": "123 Andrew Street, Flin Flon, Manitoba",
-					"yearOpened": 2013,
-					"book_id": "bae-c674e3b0-ebb6-5b89-bfa3-d1128288d21a"
-			    }`,
-				`{
-					"name": "Only Publisher of Sooley",
-					"address": "11 Sooley Ave., Waterloo, Ontario",
-					"yearOpened": 1999,
-					"book_id": "bae-7ba73251-c935-5f44-ac04-d2061149cc14"
-			    }`,
-			},
-		},
-		Results: []map[string]any{
-			{
-				"name":   "The Rooster Bar",
-				"rating": 4.0,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of The Rooster Bar",
-					"yearOpened": uint64(2022),
+				Results: []map[string]any{
+					{
+						"name":   "The Rooster Bar",
+						"rating": 4.0,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of The Rooster Bar",
+							"yearOpened": uint64(2022),
+						},
+					},
+					{
+						"name":   "Theif Lord",
+						"rating": 4.8,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of Theif Lord",
+							"yearOpened": uint64(2020),
+						},
+					},
+					{
+						"name":   "A Time for Mercy",
+						"rating": 4.5,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of A Time for Mercy",
+							"yearOpened": uint64(2013),
+						},
+					},
+					{
+						"name":   "Sooley",
+						"rating": 3.2,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of Sooley",
+							"yearOpened": uint64(1999),
+						},
+					},
+					{
+						"name":   "Painted House",
+						"rating": 4.9,
+						"publisher": map[string]any{
+							"name":       "Only Publisher of Painted House",
+							"yearOpened": uint64(1995),
+						},
+					},
+					{
+						"name":      "The Associate",
+						"rating":    4.2,
+						"publisher": nil,
+					},
 				},
-			},
-			{
-				"name":   "Theif Lord",
-				"rating": 4.8,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of Theif Lord",
-					"yearOpened": uint64(2020),
-				},
-			},
-			{
-				"name":   "A Time for Mercy",
-				"rating": 4.5,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of A Time for Mercy",
-					"yearOpened": uint64(2013),
-				},
-			},
-			{
-				"name":   "Sooley",
-				"rating": 3.2,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of Sooley",
-					"yearOpened": uint64(1999),
-				},
-			},
-			{
-				"name":   "Painted House",
-				"rating": 4.9,
-				"publisher": map[string]any{
-					"name":       "Only Publisher of Painted House",
-					"yearOpened": uint64(1995),
-				},
-			},
-			{
-				"name":      "The Associate",
-				"rating":    4.2,
-				"publisher": nil,
 			},
 		},
 	}
 
-	executeTestCase(t, test)
+	testUtils.ExecuteTestCase(t, []string{"Author", "Book", "Publisher"}, test)
 }
