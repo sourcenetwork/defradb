7e76896bef3ef43653ff106c2d9fe4f327df5e29
Andrew Sisley <a.j.sisley@hotmail.com>
2023-03-15 16:02:00 -0400

    Add peer create tests across schema versions

---
+++ tests/integration/net/state/simple/peer/with_create_add_field_test.go
@@ -0,0 +1,225 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package peer_test
+
+import (
+	"testing"
+
+	"github.com/sourcenetwork/immutable"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestP2PPeerCreateWithNewFieldSyncsDocsToOlderSchemaVersion(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.RandomNetworkingConfig(),
+			testUtils.RandomNetworkingConfig(),
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {
+						Name: String
+					}
+				`,
+			},
+			testUtils.SchemaPatch{
+				// Patch the schema on the node that we will directly create a doc on
+				NodeID: immutable.Some(0),
+				Patch: `
+					[
+						{ "op": "add", "path": "/Users/Schema/Fields/-", "value": {"Name": "Email", "Kind": 11} }
+					]
+				`,
+			},
+			testUtils.ConnectPeers{
+				SourceNodeID: 1,
+				TargetNodeID: 0,
+			},
+			testUtils.SubscribeToCollection{
+				NodeID:       1,
+				CollectionID: 0,
+			},
+			testUtils.CreateDoc{
+				NodeID: immutable.Some(0),
+				Doc: `{
+					"Name": "John",
+					"Email": "imnotyourbuddyguy@source.ca"
+				}`,
+			},
+			testUtils.WaitForSync{},
+			testUtils.Request{
+				NodeID: immutable.Some(0),
+				Request: `query {
+					Users {
+						Name
+						Email
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"Name":  "John",
+						"Email": "imnotyourbuddyguy@source.ca",
+					},
+				},
+			},
+			testUtils.Request{
+				// John should still be synced to the second node, even though it has
+				// not been updated to contain the new 'Email' field.
+				NodeID: immutable.Some(1),
+				Request: `query {
+					Users {
+						Name
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"Name": "John",
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, []string{"Users"}, test)
+}
+
+func TestP2PPeerCreateWithNewFieldSyncsDocsToNewerSchemaVersion(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.RandomNetworkingConfig(),
+			testUtils.RandomNetworkingConfig(),
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {
+						Name: String
+					}
+				`,
+			},
+			testUtils.SchemaPatch{
+				// Patch the schema on the node that we will sync docs to
+				NodeID: immutable.Some(1),
+				Patch: `
+					[
+						{ "op": "add", "path": "/Users/Schema/Fields/-", "value": {"Name": "Email", "Kind": 11} }
+					]
+				`,
+			},
+			testUtils.ConnectPeers{
+				SourceNodeID: 1,
+				TargetNodeID: 0,
+			},
+			testUtils.SubscribeToCollection{
+				NodeID:       1,
+				CollectionID: 0,
+			},
+			testUtils.CreateDoc{
+				NodeID: immutable.Some(0),
+				Doc: `{
+					"Name": "John"
+				}`,
+			},
+			testUtils.WaitForSync{},
+			testUtils.Request{
+				// John should still be synced to the second node, even though it has
+				// been updated with a new 'Email' field that does not exist on the
+				// source node.
+				Request: `query {
+					Users {
+						Name
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"Name": "John",
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, []string{"Users"}, test)
+}
+
+// Documentation test: This may be undesirable behaviour and the test should be
+// updated once the behaviour has been fixed.
+func TestP2PPeerCreateWithNewFieldSyncsDocsToUpdatedSchemaVersion(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.RandomNetworkingConfig(),
+			testUtils.RandomNetworkingConfig(),
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {
+						Name: String
+					}
+				`,
+			},
+			testUtils.SchemaPatch{
+				// Patch the schema on all nodes
+				Patch: `
+					[
+						{ "op": "add", "path": "/Users/Schema/Fields/-", "value": {"Name": "Email", "Kind": 11} }
+					]
+				`,
+			},
+			testUtils.ConnectPeers{
+				SourceNodeID: 1,
+				TargetNodeID: 0,
+			},
+			testUtils.SubscribeToCollection{
+				NodeID:       1,
+				CollectionID: 0,
+			},
+			testUtils.CreateDoc{
+				NodeID: immutable.Some(0),
+				Doc: `{
+					"Name": "John",
+					"Email": "imnotyourbuddyguy@source.ca"
+				}`,
+			},
+			testUtils.WaitForSync{},
+			testUtils.Request{
+				NodeID: immutable.Some(0),
+				Request: `query {
+					Users {
+						Name
+						Email
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"Name":  "John",
+						"Email": "imnotyourbuddyguy@source.ca",
+					},
+				},
+			},
+			testUtils.Request{
+				// Even though 'Email' exists on both nodes, the value of the field has not
+				// successfully been synced.
+				NodeID: immutable.Some(1),
+				Request: `query {
+					Users {
+						Name
+						Email
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"Name":  "John",
+						"Email": nil,
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, []string{"Users"}, test)
+}
