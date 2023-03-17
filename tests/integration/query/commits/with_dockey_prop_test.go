fccc5030aaa155a678773497d198f391439e74ae
Islam Aliev <aliev.islam@gmail.com>
2023-03-17 15:45:05 +0100

    Add a new test with dockey prop of commit

---
+++ tests/integration/query/commits/with_dockey_prop_test.go
@@ -0,0 +1,59 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package commits
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestQueryCommitsWithDockeyProperty(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Simple commits query with dockey property",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: userCollectionGQLSchema,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 0,
+				Doc: `{
+					"Name": "John",
+					"Age": 21
+				}`,
+			},
+			testUtils.Request{
+				Request: `query {
+					commits {
+						cid
+						dockey
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"cid":    "bafybeiaeic6vhiiw5zu6ju7e47cclvctn6t5pb36fj3mczchyhmctbrr6m",
+						"dockey": "bae-52b9170d-b77a-5887-b877-cbdbb99b009f",
+					},
+					{
+						"cid":    "bafybeibsaubd2ptp6qqsszv24p73j474amc4pll4oyssnpilofrl575hmy",
+						"dockey": "bae-52b9170d-b77a-5887-b877-cbdbb99b009f",
+					},
+					{
+						"cid":    "bafybeidr2z5ahvvss5j664gxyna5wjil5ndfjbmllnsewkjf6cnsvsmmqu",
+						"dockey": "bae-52b9170d-b77a-5887-b877-cbdbb99b009f",
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, []string{"users"}, test)
+}
