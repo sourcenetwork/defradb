f39f3e5853d522fb831f861f87bee19f24dc7ec9
AndrewSisley <a.j.sisley@hotmail.com>
2023-03-27 14:21:17 -0400

    test: Add gql introspection tests for patch schema (#1219)
    * Make DefaultFields public
    
    Will be used outside of this package shortly
    
    * Make Append public
    
    Will be used outside of this package shortly
    
    * Make Field public
    
    Will be used outside of this package shortly
    
    * Make Tidy public
    
    Will be used outside of this package shortly
    
    * Add gql introspection tests for patch schema

---
+++ tests/integration/schema/updates/add/field/with_introspection_test.go
@@ -0,0 +1,117 @@
+// Copyright 2023 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package field
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	introspectionUtils "github.com/sourcenetwork/defradb/tests/integration/schema"
+)
+
+func TestSchemaUpdatesAddFieldIntrospection(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Test schema update, add field with gql introspection",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {}
+				`,
+			},
+			testUtils.SchemaPatch{
+				Patch: `
+					[
+						{ "op": "add", "path": "/Users/Schema/Fields/-", "value": {"Name": "Name", "Kind": 11} }
+					]
+				`,
+			},
+			testUtils.IntrospectionRequest{
+				Request: `
+					query IntrospectionQuery {
+						__type (name: "Users") {
+							name
+							fields {
+								name
+								type {
+									name
+									kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "Users",
+						"fields": introspectionUtils.DefaultFields.Append(
+							introspectionUtils.Field{
+								"name": "Name",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "String",
+								},
+							},
+						).Tidy(),
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, []string{"Users"}, test)
+}
+
+func TestSchemaUpdatesAddFieldIntrospectionDoesNotAmendGQLTypesGivenBadPatch(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Test schema update, add invalid field with gql introspection",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {}
+				`,
+			},
+			testUtils.SchemaPatch{
+				// The [Name] field is valid, but [Email] has an invalid [Kind].
+				// [Name] should not be added to the GQL types.
+				Patch: `
+					[
+						{ "op": "add", "path": "/Users/Schema/Fields/-", "value": {"Name": "Name", "Kind": 11} },
+						{ "op": "add", "path": "/Users/Schema/Fields/-", "value": {"Name": "Email", "Kind": 111} }
+					]
+				`,
+				ExpectedError: "no type found for given name. Type: 111",
+			},
+			testUtils.IntrospectionRequest{
+				Request: `
+					query IntrospectionQuery {
+						__type (name: "Users") {
+							name
+							fields {
+								name
+								type {
+									name
+									kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "Users",
+						// No fields have been added to the GQL [Users] type.
+						"fields": introspectionUtils.DefaultFields.Tidy(),
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, []string{"Users"}, test)
+}
