8fdac05c79362d00aaf9f7146685c17387196326
Andrew Sisley <a.j.sisley@hotmail.com>
2023-04-06 15:30:47 -0400

    PR FIXUP - Add warning to UpdateCollection

--- client/db.go
+++ client/db.go
@@ -120,6 +120,9 @@ type Store interface {
 	// The collection (including the schema version ID) will only be updated if any changes have actually
 	// been made, if the given description matches the current persisted description then no changes will be
 	// applied.
+	//
+	// WARNING: It does not currently update the GQL types, and as such a database restart is required after
+	// calling this if use of the new collection via GQL is desired (for example via [ExecRequest]).
 	UpdateCollection(context.Context, CollectionDescription) (Collection, error)
 
 	// ValidateUpdateCollection validates that the given collection description is a valid update.
