55b61c7899dd808b84ed4f4cac3aea19bc15d6ef
Andrew Sisley <a.j.sisley@hotmail.com>
2023-04-13 19:55:27 -0400

    PR FIXUP - Document command-buffer limitation
    This is a change from the prior solutino, as the (un)subscribe and event channels were separate.

--- events/simple.go
+++ events/simple.go
@@ -16,6 +16,9 @@ type simpleChannel[T any] struct {
 	//
 	// It is important that all stuff gets sent through this single channel to ensure
 	// that the order of operations is preserved.
+	//
+	// WARNING: This does mean that non-event commands can block the database if the buffer
+	// size is breached (e.g. if many subscribe commands occupy the buffer).
 	commandChannel  chan any
 	eventBufferSize int
 	hasClosedChan   chan struct{}
