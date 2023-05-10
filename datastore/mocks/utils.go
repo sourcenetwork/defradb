9a20691cd5aaa585625f2aafeb79c750196d2400
Islam Aleiv <islam@source.network>
2023-05-10 13:15:26 +0200

    Create mocks for Txn and related interfaces

---
+++ datastore/mocks/utils.go
@@ -0,0 +1,81 @@
+package mocks
+
+import (
+	"testing"
+
+	ds "github.com/ipfs/go-datastore"
+	query "github.com/ipfs/go-datastore/query"
+	mock "github.com/stretchr/testify/mock"
+)
+
+type MultiStoreTxn struct {
+	*Txn
+	MockRootstore   *DSReaderWriter
+	MockDatastore   *DSReaderWriter
+	MockHeadstore   *DSReaderWriter
+	MockDAGstore    *DAGStore
+	MockSystemstore *DSReaderWriter
+}
+
+func prepareDataStore(t *testing.T) *DSReaderWriter {
+	dataStore := NewDSReaderWriter(t)
+	dataStore.EXPECT().Get(mock.Anything, mock.Anything).Return([]byte{}, ds.ErrNotFound).Maybe()
+	dataStore.EXPECT().Put(mock.Anything, mock.Anything, mock.Anything).Return(nil).Maybe()
+	dataStore.EXPECT().Has(mock.Anything, mock.Anything).Return(true, nil).Maybe()
+	return dataStore
+}
+
+func prepareRootStore(t *testing.T) *DSReaderWriter {
+	return NewDSReaderWriter(t)
+}
+
+func prepareHeadStore(t *testing.T) *DSReaderWriter {
+	headStore := NewDSReaderWriter(t)
+
+	queryResults := NewResults(t)
+	resultChan := make(chan query.Result)
+	close(resultChan)
+	queryResults.EXPECT().Next().Return(resultChan).Maybe()
+	queryResults.EXPECT().Close().Return(nil).Maybe()
+	headStore.EXPECT().Query(mock.Anything, mock.Anything).Return(queryResults, nil).Maybe()
+
+	headStore.EXPECT().Get(mock.Anything, mock.Anything).Return([]byte{}, ds.ErrNotFound).Maybe()
+	headStore.EXPECT().Put(mock.Anything, mock.Anything, mock.Anything).Return(nil).Maybe()
+	headStore.EXPECT().Has(mock.Anything, mock.Anything).Return(false, nil).Maybe()
+	return headStore
+}
+
+func prepareSystemStore(t *testing.T) *DSReaderWriter {
+	systemStore := NewDSReaderWriter(t)
+	systemStore.EXPECT().Get(mock.Anything, mock.Anything).Return([]byte{}, nil).Maybe()
+	return systemStore
+}
+
+func prepareDAGStore(t *testing.T) *DAGStore {
+	dagStore := NewDAGStore(t)
+	dagStore.EXPECT().Put(mock.Anything, mock.Anything).Return(nil).Maybe()
+	dagStore.EXPECT().Has(mock.Anything, mock.Anything).Return(false, nil).Maybe()
+	return dagStore
+}
+
+func NewTxnWithMultistore(t *testing.T) *MultiStoreTxn {
+	txn := NewTxn(t)
+	txn.EXPECT().OnSuccess(mock.Anything)
+
+	result := &MultiStoreTxn{
+		Txn:             txn,
+		MockRootstore:   prepareRootStore(t),
+		MockDatastore:   prepareDataStore(t),
+		MockHeadstore:   prepareHeadStore(t),
+		MockDAGstore:    prepareDAGStore(t),
+		MockSystemstore: prepareSystemStore(t),
+	}
+
+	txn.EXPECT().Rootstore().Return(result.MockRootstore).Maybe()
+	txn.EXPECT().Datastore().Return(result.MockDatastore).Maybe()
+	txn.EXPECT().Headstore().Return(result.MockHeadstore).Maybe()
+	txn.EXPECT().DAGstore().Return(result.MockDAGstore).Maybe()
+	txn.EXPECT().Systemstore().Return(result.MockSystemstore).Maybe()
+
+	return result
+}
