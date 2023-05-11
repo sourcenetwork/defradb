81d9249506865fd9cbf027b2ec568bd198b7d567
Shahzad Lone <shahzadlone@gmail.com>
2023-05-11 12:31:00 +0500

    test: Convert explain sum tests to new explain setup (#1489)
    ## Relevant issue(s)
    - Part of #953
    - Resolves #1485
    
    ## Description
    Continue converting explain tests to the new explain setup before we can
    integrate the entire setup to the new action-based setup. #953 Has a lot
    more detail on the entire plan.
    
    - This PR converts all the default explain sum tests.
    - Splits sum join tests in a separate file.

--- tests/integration/explain/default/with_sum_test.go
+++ tests/integration/explain/default/with_sum_test.go
@@ -13,254 +13,25 @@ package test_explain_default
 import (
 	"testing"
 
-	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	explainUtils "github.com/sourcenetwork/defradb/tests/integration/explain"
 )
 
-func TestExplainQuerySumOfRelatedOneToManyField(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Explain a simple sum query of a One-to-Many realted sub-type.",
-
-		Request: `query @explain {
-			Author {
-				name
-				_key
-				TotalPages: _sum(
-					books: {field: pages}
-				)
-			}
-		}`,
-
-		Docs: map[int][]string{
-			// books
-			1: {
-				`{
-					"name": "Painted House",
-					"author_id": "bae-25fafcc7-f251-58c1-9495-ead73e676fb8",
-					"pages": 22
-				}`,
-				`{
-					"name": "A Time for Mercy",
-					"author_id": "bae-25fafcc7-f251-58c1-9495-ead73e676fb8",
-					"pages": 101
-				}`,
-				`{
-					"name": "Theif Lord",
-					"author_id": "bae-3dddb519-3612-5e43-86e5-49d6295d4f84",
-					"pages": 321
-				}`,
-			},
-
-			// authors
-			2: {
-				// _key: "bae-25fafcc7-f251-58c1-9495-ead73e676fb8"
-				`{
-					"name": "John Grisham",
-					"age": 65,
-					"verified": true,
-					"contact_id": "bae-1fe427b8-ab8d-56c3-9df2-826a6ce86fed"
-				}`,
-				// _key: "bae-3dddb519-3612-5e43-86e5-49d6295d4f84"
-				`{
-					"name": "Cornelia Funke",
-					"age": 62,
-					"verified": false,
-					"contact_id": "bae-c0960a29-b704-5c37-9c2e-59e1249e4559"
-				}`,
-			},
-		},
-
-		Results: []dataMap{
-			{
-				"explain": dataMap{
-					"selectTopNode": dataMap{
-						"sumNode": dataMap{
-							"sources": []dataMap{
-								{
-									"fieldName":      "books",
-									"childFieldName": "pages",
-									"filter":         nil,
-								},
-							},
-							"selectNode": dataMap{
-								"filter": nil,
-								"typeIndexJoin": dataMap{
-									"joinType": "typeJoinMany",
-									"rootName": "author",
-									"root": dataMap{
-										"scanNode": dataMap{
-											"collectionID":   "3",
-											"collectionName": "Author",
-											"filter":         nil,
-											"spans": []dataMap{
-												{
-													"start": "/3",
-													"end":   "/4",
-												},
-											},
-										},
-									},
-									"subTypeName": "books",
-									"subType": dataMap{
-										"selectTopNode": dataMap{
-											"selectNode": dataMap{
-												"filter": nil,
-												"scanNode": dataMap{
-													"collectionID":   "2",
-													"collectionName": "Book",
-													"filter":         nil,
-													"spans": []dataMap{
-														{
-															"start": "/2",
-															"end":   "/3",
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
+var sumPattern = dataMap{
+	"explain": dataMap{
+		"selectTopNode": dataMap{
+			"sumNode": dataMap{
+				"selectNode": dataMap{
+					"scanNode": dataMap{},
 				},
 			},
 		},
-	}
-
-	executeTestCase(t, test)
+	},
 }
 
-func TestExplainQuerySumOfRelatedOneToManyFieldWithFilter(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Explain a simple sum query of a One-to-Many realted sub-type with a filter.",
+func TestDefaultExplainRequestWithSumOnInlineArrayField_ChildFieldWillBeEmpty(t *testing.T) {
+	test := explainUtils.ExplainRequestTestCase{
 
-		Request: `query @explain {
-			Author {
-				name
-				TotalPages: _sum(
-					articles: {
-						field: pages,
-						filter: {
-							name: {
-								_eq: "To my dear readers"
-							}
-						}
-					}
-				)
-			}
-		}`,
-
-		Docs: map[int][]string{
-			// articles
-			0: {
-				`{
-					"name": "After Guantánamo, Another Injustice",
-					"author_id": "bae-25fafcc7-f251-58c1-9495-ead73e676fb8",
-					"pages": 2
-				}`,
-				`{
-					"name": "To my dear readers",
-					"author_id": "bae-3dddb519-3612-5e43-86e5-49d6295d4f84",
-					"pages": 11
-				}`,
-				`{
-					"name": "Twinklestar's Favourite Xmas Cookie",
-					"author_id": "bae-3dddb519-3612-5e43-86e5-49d6295d4f84",
-					"pages": 31
-				}`,
-			},
-
-			// authors
-			2: {
-				// _key: "bae-25fafcc7-f251-58c1-9495-ead73e676fb8"
-				`{
-					"name": "John Grisham",
-					"age": 65,
-					"verified": true,
-					"contact_id": "bae-1fe427b8-ab8d-56c3-9df2-826a6ce86fed"
-				}`,
-				// _key: "bae-3dddb519-3612-5e43-86e5-49d6295d4f84"
-				`{
-					"name": "Cornelia Funke",
-					"age": 62,
-					"verified": false,
-					"contact_id": "bae-c0960a29-b704-5c37-9c2e-59e1249e4559"
-				}`,
-			},
-		},
-
-		Results: []dataMap{
-			{
-				"explain": dataMap{
-					"selectTopNode": dataMap{
-						"sumNode": dataMap{
-							"sources": []dataMap{
-								{
-									"fieldName":      "articles",
-									"childFieldName": "pages",
-									"filter": dataMap{
-										"name": dataMap{
-											"_eq": "To my dear readers",
-										},
-									},
-								},
-							},
-							"selectNode": dataMap{
-								"filter": nil,
-								"typeIndexJoin": dataMap{
-									"joinType": "typeJoinMany",
-									"rootName": "author",
-									"root": dataMap{
-										"scanNode": dataMap{
-											"collectionID":   "3",
-											"collectionName": "Author",
-											"filter":         nil,
-											"spans": []dataMap{
-												{
-													"start": "/3",
-													"end":   "/4",
-												},
-											},
-										},
-									},
-									"subTypeName": "articles",
-									"subType": dataMap{
-										"selectTopNode": dataMap{
-											"selectNode": dataMap{
-												"filter": nil,
-												"scanNode": dataMap{
-													"collectionID":   "1",
-													"collectionName": "Article",
-													"filter": dataMap{
-														"name": dataMap{
-															"_eq": "To my dear readers",
-														},
-													},
-													"spans": []dataMap{
-														{
-															"start": "/1",
-															"end":   "/2",
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	executeTestCase(t, test)
-}
-
-func TestExplainQuerySumOfInlineArrayField_ShouldHaveEmptyChildField(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Explain a simple sum query on an  inline array field (childFieldName is nil).",
+		Description: "Explain (default) request with sum on an inline array field.",
 
 		Request: `query @explain {
 			Book {
@@ -295,32 +66,33 @@ func TestExplainQuerySumOfInlineArrayField_ShouldHaveEmptyChildField(t *testing.
 			},
 		},
 
-		Results: []dataMap{
+		ExpectedPatterns: []dataMap{sumPattern},
+
+		ExpectedTargets: []explainUtils.PlanNodeTargetCase{
 			{
-				"explain": dataMap{
-					"selectTopNode": dataMap{
-						"sumNode": dataMap{
-							"sources": []dataMap{
-								{
-									"fieldName":      "chapterPages",
-									"childFieldName": nil,
-									"filter":         nil,
-								},
-							},
-							"selectNode": dataMap{
-								"filter": nil,
-								"scanNode": dataMap{
-									"collectionID":   "2",
-									"collectionName": "Book",
-									"filter":         nil,
-									"spans": []dataMap{
-										{
-											"start": "/2",
-											"end":   "/3",
-										},
-									},
-								},
-							},
+				TargetNodeName:    "sumNode",
+				IncludeChildNodes: false,
+				ExpectedAttributes: dataMap{
+					"sources": []dataMap{
+						{
+							"fieldName":      "chapterPages",
+							"childFieldName": nil,
+							"filter":         nil,
+						},
+					},
+				},
+			},
+			{
+				TargetNodeName:    "scanNode",
+				IncludeChildNodes: true, // should be leaf of it's branch, so will have no child nodes.
+				ExpectedAttributes: dataMap{
+					"collectionID":   "2",
+					"collectionName": "Book",
+					"filter":         nil,
+					"spans": []dataMap{
+						{
+							"start": "/2",
+							"end":   "/3",
 						},
 					},
 				},
@@ -328,186 +100,5 @@ func TestExplainQuerySumOfInlineArrayField_ShouldHaveEmptyChildField(t *testing.
 		},
 	}
 
-	executeTestCase(t, test)
-}
-
-func TestExplainQuerySumOfRelatedOneToManyFieldWithManySources(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Explain a simple sum query of a One-to-Many realted sub-type with many sources.",
-
-		Request: `query @explain {
-			Author {
-				name
-				TotalPages: _sum(
-					books: {field: pages},
-					articles: {field: pages}
-				)
-			}
-		}`,
-
-		Docs: map[int][]string{
-			// articles
-			0: {
-				`{
-					"name": "After Guantánamo, Another Injustice",
-					"author_id": "bae-25fafcc7-f251-58c1-9495-ead73e676fb8",
-					"pages": 2
-				}`,
-				`{
-					"name": "To my dear readers",
-					"author_id": "bae-3dddb519-3612-5e43-86e5-49d6295d4f84",
-					"pages": 11
-				}`,
-				`{
-					"name": "Twinklestar's Favourite Xmas Cookie",
-					"author_id": "bae-3dddb519-3612-5e43-86e5-49d6295d4f84",
-					"pages": 31
-				}`,
-			},
-
-			// books
-			1: {
-				`{
-					"name": "Painted House",
-					"author_id": "bae-25fafcc7-f251-58c1-9495-ead73e676fb8",
-					"pages": 22
-				}`,
-				`{
-					"name": "A Time for Mercy",
-					"author_id": "bae-25fafcc7-f251-58c1-9495-ead73e676fb8",
-					"pages": 101
-				}`,
-				`{
-					"name": "Theif Lord",
-					"author_id": "bae-3dddb519-3612-5e43-86e5-49d6295d4f84",
-					"pages": 321
-				}`,
-			},
-
-			// authors
-			2: {
-				// _key: "bae-25fafcc7-f251-58c1-9495-ead73e676fb8"
-				`{
-					"name": "John Grisham",
-					"age": 65,
-					"verified": true,
-					"contact_id": "bae-1fe427b8-ab8d-56c3-9df2-826a6ce86fed"
-				}`,
-				// _key: "bae-3dddb519-3612-5e43-86e5-49d6295d4f84"
-				`{
-					"name": "Cornelia Funke",
-					"age": 62,
-					"verified": false,
-					"contact_id": "bae-c0960a29-b704-5c37-9c2e-59e1249e4559"
-				}`,
-			},
-		},
-
-		Results: []dataMap{
-			{
-				"explain": dataMap{
-					"selectTopNode": dataMap{
-						"sumNode": dataMap{
-							"sources": []dataMap{
-								{
-									"childFieldName": "pages",
-									"fieldName":      "books",
-									"filter":         nil,
-								},
-
-								{
-									"childFieldName": "pages",
-									"fieldName":      "articles",
-									"filter":         nil,
-								},
-							},
-							"selectNode": dataMap{
-								"filter": nil,
-								"parallelNode": []dataMap{
-									{
-										"typeIndexJoin": dataMap{
-											"joinType": "typeJoinMany",
-											"rootName": "author",
-											"root": dataMap{
-												"scanNode": dataMap{
-													"collectionID":   "3",
-													"collectionName": "Author",
-													"filter":         nil,
-													"spans": []dataMap{
-														{
-															"start": "/3",
-															"end":   "/4",
-														},
-													},
-												},
-											},
-											"subTypeName": "books",
-											"subType": dataMap{
-												"selectTopNode": dataMap{
-													"selectNode": dataMap{
-														"filter": nil,
-														"scanNode": dataMap{
-															"collectionID":   "2",
-															"collectionName": "Book",
-															"filter":         nil,
-															"spans": []dataMap{
-																{
-																	"start": "/2",
-																	"end":   "/3",
-																},
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-									{
-										"typeIndexJoin": dataMap{
-											"joinType": "typeJoinMany",
-											"rootName": "author",
-											"root": dataMap{
-												"scanNode": dataMap{
-													"collectionID":   "3",
-													"collectionName": "Author",
-													"filter":         nil,
-													"spans": []dataMap{
-														{
-															"start": "/3",
-															"end":   "/4",
-														},
-													},
-												},
-											},
-											"subTypeName": "articles",
-											"subType": dataMap{
-												"selectTopNode": dataMap{
-													"selectNode": dataMap{
-														"filter": nil,
-														"scanNode": dataMap{
-															"collectionID":   "1",
-															"collectionName": "Article",
-															"filter":         nil,
-															"spans": []dataMap{
-																{
-																	"start": "/1",
-																	"end":   "/2",
-																},
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	executeTestCase(t, test)
+	runExplainTest(t, test)
 }
