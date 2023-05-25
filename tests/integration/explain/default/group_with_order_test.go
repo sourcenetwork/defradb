29182d32624affcc9455e181718b5132b3c33300
Shahzad Lone <shahzadlone@gmail.com>
2023-05-25 06:56:29 -0400

    PR: Add tests to cover the missing test gap.

--- tests/integration/explain/default/group_with_order_test.go
+++ tests/integration/explain/default/group_with_order_test.go
@@ -114,6 +114,199 @@ func TestDefaultExplainRequestWithDescendingOrderOnParentGroupBy(t *testing.T) {
 	runExplainTest(t, test)
 }
 
+func TestDefaultExplainRequestWithAscendingOrderOnParentGroupBy(t *testing.T) {
+	test := explainUtils.ExplainRequestTestCase{
+
+		Description: "Explain (default) request with order (ascending) on parent groupBy.",
+
+		Request: `query @explain {
+			Author(groupBy: [name], order: {name: ASC}) {
+				name
+				_group {
+					age
+				}
+			}
+		}`,
+
+		Docs: map[int][]string{
+			//authors
+			2: {
+				`{
+					"name": "John Grisham",
+					"verified": true,
+					"age": 65
+				}`,
+				`{
+					"name": "John Grisham",
+					"verified": false,
+					"age": 2
+				}`,
+				`{
+					"name": "John Grisham",
+					"verified": true,
+					"age": 50
+				}`,
+				`{
+					"name": "Cornelia Funke",
+					"verified": true,
+					"age": 62
+				}`,
+				`{
+					"name": "Twin",
+					"verified": true,
+					"age": 63
+				}`,
+				`{
+					"name": "Twin",
+					"verified": true,
+					"age": 63
+				}`,
+			},
+		},
+
+		ExpectedFullGraph: []dataMap{
+			{
+				"explain": dataMap{
+					"selectTopNode": dataMap{
+						"orderNode": dataMap{
+							"orderings": []dataMap{
+								{
+									"direction": "ASC",
+									"fields":    []string{"name"},
+								},
+							},
+							"groupNode": dataMap{
+								"groupByFields": []string{"name"},
+								"childSelects": []dataMap{
+									{
+										"collectionName": "Author",
+										"docKeys":        nil,
+										"orderBy":        nil,
+										"groupBy":        nil,
+										"limit":          nil,
+										"filter":         nil,
+									},
+								},
+								"selectNode": dataMap{
+									"filter": nil,
+									"scanNode": dataMap{
+										"collectionID":   "3",
+										"collectionName": "Author",
+										"filter":         nil,
+										"spans": []dataMap{
+											{
+												"start": "/3",
+												"end":   "/4",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	runExplainTest(t, test)
+}
+
+func TestDefaultExplainRequestWithDescendingOrderOnInnerGroupSelection(t *testing.T) {
+	test := explainUtils.ExplainRequestTestCase{
+
+		Description: "Explain (default) request with order (descending) on inner _group selection.",
+
+		Request: `query @explain {
+			Author(groupBy: [name]) {
+				name
+				_group (order: {age: DESC}){
+					age
+				}
+			}
+		}`,
+
+		Docs: map[int][]string{
+			//authors
+			2: {
+				`{
+					"name": "John Grisham",
+					"verified": true,
+					"age": 65
+				}`,
+				`{
+					"name": "John Grisham",
+					"verified": false,
+					"age": 2
+				}`,
+				`{
+					"name": "John Grisham",
+					"verified": true,
+					"age": 50
+				}`,
+				`{
+					"name": "Cornelia Funke",
+					"verified": true,
+					"age": 62
+				}`,
+				`{
+					"name": "Twin",
+					"verified": true,
+					"age": 63
+				}`,
+				`{
+					"name": "Twin",
+					"verified": true,
+					"age": 63
+				}`,
+			},
+		},
+
+		ExpectedFullGraph: []dataMap{
+			{
+				"explain": dataMap{
+					"selectTopNode": dataMap{
+						"groupNode": dataMap{
+							"groupByFields": []string{"name"},
+							"childSelects": []dataMap{
+								{
+									"collectionName": "Author",
+									"orderBy": []dataMap{
+										{
+											"direction": "DESC",
+											"fields":    []string{"age"},
+										},
+									},
+									"docKeys": nil,
+									"groupBy": nil,
+									"limit":   nil,
+									"filter":  nil,
+								},
+							},
+							"selectNode": dataMap{
+								"filter": nil,
+								"scanNode": dataMap{
+									"collectionID":   "3",
+									"collectionName": "Author",
+									"filter":         nil,
+									"spans": []dataMap{
+										{
+											"start": "/3",
+											"end":   "/4",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	runExplainTest(t, test)
+}
+
 func TestDefaultExplainRequestWithAscendingOrderOnInnerGroupSelection(t *testing.T) {
 	test := explainUtils.ExplainRequestTestCase{
 
