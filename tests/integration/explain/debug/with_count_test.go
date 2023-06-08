5a1baf0245c2da3f8908156bda9d8c31b87b53fc
Shahzad Lone <shahzadlone@gmail.com>
2023-06-08 11:58:10 -0400

    PR(TEST): Add count tests

---
+++ tests/integration/explain/debug/with_count_test.go
@@ -0,0 +1,55 @@
+// Copyright 2023 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_explain_debug
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	explainUtils "github.com/sourcenetwork/defradb/tests/integration/explain"
+)
+
+var countPattern = dataMap{
+	"explain": dataMap{
+		"selectTopNode": dataMap{
+			"countNode": dataMap{
+				"selectNode": dataMap{
+					"scanNode": dataMap{},
+				},
+			},
+		},
+	},
+}
+
+func TestDebugExplainRequestWithCountOnInlineArrayField(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Explain (debug) request with count on an inline array field.",
+
+		Actions: []any{
+			explainUtils.SchemaForExplainTests,
+
+			testUtils.ExplainRequest{
+
+				Request: `query @explain(type: debug) {
+					Book {
+						name
+						_count(chapterPages: {})
+					}
+				}`,
+
+				ExpectedPatterns: []dataMap{countPattern},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
