1e186cc10c68435d78b8fb062c367670a0f2d334
Shahzad Lone <shahzadlone@gmail.com>
2023-06-08 11:59:27 -0400

    PR(TEST): Add group tests

---
+++ tests/integration/explain/debug/group_with_dockey_test.go
@@ -0,0 +1,82 @@
+// Copyright 2023 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_explain_debug
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	explainUtils "github.com/sourcenetwork/defradb/tests/integration/explain"
+)
+
+func TestDebugExplainRequestWithDockeyOnParentGroupBy(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Explain (debug) request with a dockey on parent groupBy.",
+
+		Actions: []any{
+			explainUtils.SchemaForExplainTests,
+
+			testUtils.ExplainRequest{
+
+				Request: `query @explain(type: debug) {
+					Author(
+						groupBy: [age],
+						dockey: "bae-6a4c5bc5-b044-5a03-a868-8260af6f2254"
+					) {
+						age
+						_group {
+							name
+						}
+					}
+				}`,
+
+				ExpectedPatterns: []dataMap{groupPattern},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
+
+func TestDebugExplainRequestWithDockeysAndFilterOnParentGroupBy(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Explain (debug) request with dockeys and filter on parent groupBy.",
+
+		Actions: []any{
+			explainUtils.SchemaForExplainTests,
+
+			testUtils.ExplainRequest{
+
+				Request: `query @explain(type: debug) {
+					Author(
+						groupBy: [age],
+						filter: {age: {_eq: 20}},
+						dockeys: [
+							"bae-6a4c5bc5-b044-5a03-a868-8260af6f2254",
+							"bae-4ea9d148-13f3-5a48-a0ef-9ffd344caeed"
+						]
+					) {
+						age
+						_group {
+							name
+						}
+					}
+				}`,
+
+				ExpectedPatterns: []dataMap{groupPattern},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
---
+++ tests/integration/explain/debug/group_with_order_test.go
@@ -0,0 +1,124 @@
+// Copyright 2023 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_explain_debug
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	explainUtils "github.com/sourcenetwork/defradb/tests/integration/explain"
+)
+
+var debugGroupOrderPattern = dataMap{
+	"explain": dataMap{
+		"selectTopNode": dataMap{
+			"orderNode": dataMap{
+				"groupNode": dataMap{
+					"selectNode": dataMap{
+						"pipeNode": dataMap{
+							"scanNode": dataMap{},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+func TestDebugExplainRequestWithDescendingOrderOnParentGroupBy(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Explain (debug) request with order (descending) on parent groupBy.",
+
+		Actions: []any{
+			explainUtils.SchemaForExplainTests,
+
+			testUtils.ExplainRequest{
+
+				Request: `query @explain(type: debug) {
+					Author(
+						groupBy: [name],
+						order: {name: DESC}
+					) {
+						name
+						_group {
+							age
+						}
+					}
+				}`,
+
+				ExpectedPatterns: []dataMap{debugGroupOrderPattern},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
+
+func TestDebugExplainRequestWithAscendingOrderOnParentGroupBy(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Explain (debug) request with order (ascending) on parent groupBy.",
+
+		Actions: []any{
+			explainUtils.SchemaForExplainTests,
+
+			testUtils.ExplainRequest{
+
+				Request: `query @explain(type: debug) {
+					Author(
+						groupBy: [name],
+						order: {name: ASC}
+					) {
+						name
+						_group {
+							age
+						}
+					}
+				}`,
+
+				ExpectedPatterns: []dataMap{debugGroupOrderPattern},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
+
+func TestDebugExplainRequestWithOrderOnParentGroupByAndOnInnerGroupSelection(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Explain (debug) request with order on parent groupBy and inner _group selection.",
+
+		Actions: []any{
+			explainUtils.SchemaForExplainTests,
+
+			testUtils.ExplainRequest{
+
+				Request: `query @explain(type: debug) {
+					Author(
+						groupBy: [name],
+						order: {name: DESC}
+					) {
+						name
+						_group (order: {age: ASC}){
+							age
+						}
+					}
+				}`,
+
+				ExpectedPatterns: []dataMap{debugGroupOrderPattern},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
