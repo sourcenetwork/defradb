69786643441d363f4932486af34a07c4c2b927a2
Islam Aliev <islam@source.network>
2023-07-12 16:30:27 +0200

    refactor: Remove first CRDT byte from field encoded values (#1622)
    ## Relevant issue(s)
    
    Resolves #1620
    
    ## Description
    
    Remove 0th-byte CRDT type from field encoded values

--- db/fetcher/encoded_doc.go
+++ db/fetcher/encoded_doc.go
@@ -49,13 +49,11 @@ type encProperty struct {
 }
 
 // Decode returns the decoded value and CRDT type for the given property.
-func (e encProperty) Decode() (client.CType, any, error) {
-	ctype := client.CType(e.Raw[0])
-	buf := e.Raw[1:]
+func (e encProperty) Decode() (any, error) {
 	var val any
-	err := cbor.Unmarshal(buf, &val)
+	err := cbor.Unmarshal(e.Raw, &val)
 	if err != nil {
-		return ctype, nil, err
+		return nil, err
 	}
 
 	if array, isArray := val.([]any); isArray {
@@ -66,7 +64,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 			for i, untypedValue := range array {
 				boolArray[i], ok = untypedValue.(bool)
 				if !ok {
-					return ctype, nil, client.NewErrUnexpectedType[bool](e.Desc.Name, untypedValue)
+					return nil, client.NewErrUnexpectedType[bool](e.Desc.Name, untypedValue)
 				}
 			}
 			val = boolArray
@@ -74,7 +72,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 		case client.FieldKind_NILLABLE_BOOL_ARRAY:
 			val, err = convertNillableArray[bool](e.Desc.Name, array)
 			if err != nil {
-				return ctype, nil, err
+				return nil, err
 			}
 
 		case client.FieldKind_INT_ARRAY:
@@ -82,7 +80,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 			for i, untypedValue := range array {
 				intArray[i], err = convertToInt(fmt.Sprintf("%s[%v]", e.Desc.Name, i), untypedValue)
 				if err != nil {
-					return ctype, nil, err
+					return nil, err
 				}
 			}
 			val = intArray
@@ -90,7 +88,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 		case client.FieldKind_NILLABLE_INT_ARRAY:
 			val, err = convertNillableArrayWithConverter(e.Desc.Name, array, convertToInt)
 			if err != nil {
-				return ctype, nil, err
+				return nil, err
 			}
 
 		case client.FieldKind_FLOAT_ARRAY:
@@ -98,7 +96,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 			for i, untypedValue := range array {
 				floatArray[i], ok = untypedValue.(float64)
 				if !ok {
-					return ctype, nil, client.NewErrUnexpectedType[float64](e.Desc.Name, untypedValue)
+					return nil, client.NewErrUnexpectedType[float64](e.Desc.Name, untypedValue)
 				}
 			}
 			val = floatArray
@@ -106,7 +104,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 		case client.FieldKind_NILLABLE_FLOAT_ARRAY:
 			val, err = convertNillableArray[float64](e.Desc.Name, array)
 			if err != nil {
-				return ctype, nil, err
+				return nil, err
 			}
 
 		case client.FieldKind_STRING_ARRAY:
@@ -114,7 +112,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 			for i, untypedValue := range array {
 				stringArray[i], ok = untypedValue.(string)
 				if !ok {
-					return ctype, nil, client.NewErrUnexpectedType[string](e.Desc.Name, untypedValue)
+					return nil, client.NewErrUnexpectedType[string](e.Desc.Name, untypedValue)
 				}
 			}
 			val = stringArray
@@ -122,7 +120,7 @@ func (e encProperty) Decode() (client.CType, any, error) {
 		case client.FieldKind_NILLABLE_STRING_ARRAY:
 			val, err = convertNillableArray[string](e.Desc.Name, array)
 			if err != nil {
-				return ctype, nil, err
+				return nil, err
 			}
 		}
 	} else { // CBOR often encodes values typed as floats as ints
@@ -130,18 +128,18 @@ func (e encProperty) Decode() (client.CType, any, error) {
 		case client.FieldKind_FLOAT:
 			switch v := val.(type) {
 			case int64:
-				return ctype, float64(v), nil
+				return float64(v), nil
 			case int:
-				return ctype, float64(v), nil
+				return float64(v), nil
 			case uint64:
-				return ctype, float64(v), nil
+				return float64(v), nil
 			case uint:
-				return ctype, float64(v), nil
+				return float64(v), nil
 			}
 		}
 	}
 
-	return ctype, val, nil
+	return val, nil
 }
 
 func convertNillableArray[T any](propertyName string, items []any) ([]immutable.Option[T], error) {
@@ -237,11 +235,11 @@ func (encdoc *encodedDocument) Decode() (*client.Document, error) {
 	}
 	doc := client.NewDocWithKey(key)
 	for _, prop := range encdoc.Properties {
-		ctype, val, err := prop.Decode()
+		val, err := prop.Decode()
 		if err != nil {
 			return nil, err
 		}
-		err = doc.SetAs(prop.Desc.Name, val, ctype)
+		err = doc.SetAs(prop.Desc.Name, val, prop.Desc.Typ)
 		if err != nil {
 			return nil, err
 		}
@@ -276,7 +274,7 @@ func (encdoc *encodedDocument) decodeToDoc(filter bool) (core.Doc, error) {
 		if filter && !prop.IsFilter { // only get filter fields if filter=true
 			continue
 		}
-		_, val, err := prop.Decode()
+		val, err := prop.Decode()
 		if err != nil {
 			return core.Doc{}, err
 		}
