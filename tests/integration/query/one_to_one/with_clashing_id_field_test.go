ffec14bd3f06a3f31309e8356ae67c0c7cf8a5c5
AndrewSisley <a.j.sisley@hotmail.com>
2023-08-15 17:58:03 -0400

    fix: Handle the querying of secondary relation id fields (#1768)
    ## Relevant issue(s)
    
    Resolves #1654
    
    ## Description
    
    Handles the querying of secondary relation id fields.
    
    It does this by joining the related object and copying the dockey value
    into the secondary related id field.

--- tests/integration/query/one_to_one/with_clashing_id_field_test.go
+++ tests/integration/query/one_to_one/with_clashing_id_field_test.go
@@ -16,7 +16,8 @@ import (
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
 )
 
-func TestQueryOneToOneWithIdFieldOnSecondary(t *testing.T) {
+// This documents unwanted behaviour, see https://github.com/sourcenetwork/defradb/issues/1520
+func TestQueryOneToOneWithClashingIdFieldOnSecondary(t *testing.T) {
 	test := testUtils.TestCase{
 		Description: "One-to-one relation secondary direction, id field with name clash on secondary side",
 		Actions: []any{
@@ -62,7 +63,7 @@ func TestQueryOneToOneWithIdFieldOnSecondary(t *testing.T) {
 				Results: []map[string]any{
 					{
 						"name":      "Painted House",
-						"author_id": uint64(123456),
+						"author_id": "bae-9d67a886-64e3-520b-8cd5-1ca7b098fabe",
 						"author": map[string]any{
 							"name": "John Grisham",
 						},
@@ -76,7 +77,7 @@ func TestQueryOneToOneWithIdFieldOnSecondary(t *testing.T) {
 }
 
 // This documents unwanted behaviour, see https://github.com/sourcenetwork/defradb/issues/1520
-func TestQueryOneToOneWithIdFieldOnPrimary(t *testing.T) {
+func TestQueryOneToOneWithClashingIdFieldOnPrimary(t *testing.T) {
 	test := testUtils.TestCase{
 		Description: "One-to-one relation primary direction, id field with name clash on primary side",
 		Actions: []any{
