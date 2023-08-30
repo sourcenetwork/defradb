7080bc618f01e8dab8fd71d015550d59015efeca
Andrew Sisley <a.j.sisley@hotmail.com>
2023-08-30 15:43:52 -0400

    Move update simple tests to new dir
    Notable changes:
    - The datetime boolean tests have been removed - the boolean filter param is irrelevant to the datetime field type and it only complicates the reading of the test without offering any significant advantage RE protection.
    - The datetime ids tests have been removed for the same reason as the boolean filter tests.
    - A test using an UpdateDoc test action has been added, this gives basic coverage of the field type happy path
    - A test using an update request has been added, this ensures that updating multiple docs in a single request is functional.
    - All references to the dockeys have been removed, returning them in the query protects against very-little at best here whilst incuring an annoying maintanence cost.
    
    The withFilter tests remain as is, whilst they are really not comprehensive, they do cover the tests removed from with_datetime_test, and are not particularly relevant to the goal of this PR (which has nothing to do with filters).

--- tests/integration/mutation/update/underscored_schema_test.go
+++ tests/integration/mutation/update/underscored_schema_test.go
@@ -8,7 +8,7 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-package special
+package update
 
 import (
 	"testing"
@@ -16,39 +16,43 @@ import (
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
 )
 
-var myUserSchema = (`
-	type My_User {
-		name: String
-	}
-`)
-
-func executeTestCase(t *testing.T, test testUtils.RequestTestCase) {
-	testUtils.ExecuteRequestTestCase(t, myUserSchema, []string{"My_User"}, test)
-}
-
 func TestMutationUpdateUnderscoredSchema(t *testing.T) {
-	test := testUtils.RequestTestCase{
+	test := testUtils.TestCase{
 		Description: "Simple update of schema with underscored name",
-		Request: `mutation {
-			update_My_User(data: "{\"name\": \"Fred\"}") {
-				_key
-				name
-			}
-		}`,
-		Docs: map[int][]string{
-			0: {
-				`{
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type My_User {
+						name: String
+					}
+				`,
+			},
+			testUtils.CreateDoc{
+				Doc: `{
 					"name": "John"
 				}`,
 			},
-		},
-		Results: []map[string]any{
-			{
-				"_key": "bae-decf6467-4c7c-50d7-b09d-0a7097ef6bad",
-				"name": "Fred",
+			testUtils.UpdateDoc{
+				Doc: `{
+					"name": "Fred"
+				}`,
+			},
+			testUtils.Request{
+				Request: `
+					query {
+						My_User {
+							name
+						}
+					}
+				`,
+				Results: []map[string]any{
+					{
+						"name": "Fred",
+					},
+				},
 			},
 		},
 	}
 
-	executeTestCase(t, test)
+	testUtils.ExecuteTestCase(t, test)
 }
---
+++ tests/integration/mutation/update/with_id_test.go
@@ -0,0 +1,97 @@
+// Copyright 2023 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package update
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestMutationUpdate_WithId(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Simple update mutation with id",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {
+						name: String
+						points: Float
+					}
+				`,
+			},
+			testUtils.CreateDoc{
+				// bae-cc36febf-4029-52b3-a876-c99c6293f588
+				Doc: `{
+					"name": "John",
+					"points": 42.1
+				}`,
+			},
+			testUtils.CreateDoc{
+				Doc: `{
+					"name": "Bob",
+					"points": 66.6
+				}`,
+			},
+			testUtils.Request{
+				Request: `mutation {
+					update_Users(id: "bae-cc36febf-4029-52b3-a876-c99c6293f588", data: "{\"points\": 59}") {
+						name
+						points
+					}
+				}`,
+				Results: []map[string]any{
+					{
+						"name":   "John",
+						"points": float64(59),
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestMutationUpdate_WithNonExistantId(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Simple update mutation with non existant id",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {
+						name: String
+						points: Float
+					}
+				`,
+			},
+			testUtils.CreateDoc{
+				// bae-cc36febf-4029-52b3-a876-c99c6293f588
+				Doc: `{
+					"name": "John",
+					"points": 42.1
+				}`,
+			},
+			testUtils.Request{
+				Request: `mutation {
+					update_Users(id: "bae-does-not-exist", data: "{\"points\": 59}") {
+						_key
+						name
+						points
+					}
+				}`,
+				Results: []map[string]any{},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
