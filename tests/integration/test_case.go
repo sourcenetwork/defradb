0196bbb1aa623d7b983d26eb14cba11e56e6246f
AndrewSisley <a.j.sisley@hotmail.com>
2023-08-31 08:33:31 -0400

    test(i): Test CreateDoc tests with gql and Collection.Save (#1837)
    ## Relevant issue(s)
    
    Resolves #1830
    
    ## Description
    
    Tests CreateDoc tests with gql and Collection.Save using the
    `mutationType` (environment) variable.

--- tests/integration/test_case.go
+++ tests/integration/test_case.go
@@ -79,7 +79,7 @@ type SchemaPatch struct {
 }
 
 // CreateDoc will attempt to create the given document in the given collection
-// using the collection api.
+// using the set [MutationType].
 type CreateDoc struct {
 	// NodeID may hold the ID (index) of a node to apply this create to.
 	//
@@ -97,6 +97,13 @@ type CreateDoc struct {
 	// String can be a partial, and the test will pass if an error is returned that
 	// contains this string.
 	ExpectedError string
+
+	// If provided a value, SupportedMutationTypes will cause this test to be skipped
+	// if the active mutation type is not within the given set.
+	//
+	// This is to only be used in the very rare cases where we really do want behavioural
+	// differences between mutation types, or we need to temporarily document a bug.
+	SupportedMutationTypes immutable.Option[[]MutationType]
 }
 
 // DeleteDoc will attempt to delete the given document in the given collection
