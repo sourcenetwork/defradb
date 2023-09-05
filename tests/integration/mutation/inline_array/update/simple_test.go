f48f20204a500f4cb8cfe595bfa031d5b3f07240
Andrew Sisley <a.j.sisley@hotmail.com>
2023-09-05 13:39:20 -0400

    Migrate update nillable float array tests to new location
    Also updates them to the new framework

--- tests/integration/mutation/inline_array/update/simple_test.go
+++ tests/integration/mutation/inline_array/update/simple_test.go
@@ -19,39 +19,6 @@ import (
 	inlineArray "github.com/sourcenetwork/defradb/tests/integration/mutation/inline_array"
 )
 
-func TestMutationInlineArrayWithNillableFloats(t *testing.T) {
-	test := testUtils.RequestTestCase{
-		Description: "Simple inline array with no filter, nillable floats",
-		Request: `mutation {
-					update_Users(data: "{\"pageRatings\": [3.1425, -0.00000000001, null, 10]}") {
-						name
-						pageRatings
-					}
-				}`,
-		Docs: map[int][]string{
-			0: {
-				`{
-					"name": "John",
-					"pageRatings": [3.1425, null, -0.00000000001, 10]
-				}`,
-			},
-		},
-		Results: []map[string]any{
-			{
-				"name": "John",
-				"pageRatings": []immutable.Option[float64]{
-					immutable.Some(3.1425),
-					immutable.Some(-0.00000000001),
-					immutable.None[float64](),
-					immutable.Some[float64](10),
-				},
-			},
-		},
-	}
-
-	inlineArray.ExecuteTestCase(t, test)
-}
-
 func TestMutationInlineArrayUpdateWithStrings(t *testing.T) {
 	tests := []testUtils.RequestTestCase{
 		{
