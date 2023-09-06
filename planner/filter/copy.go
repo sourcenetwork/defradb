5784290024095966aefcb0e5ff4b00de175940e2
Islam Aleiv <islam@source.network>
2023-09-06 19:18:24 +0200

    Implement copy filter

---
+++ planner/filter/copy.go
@@ -0,0 +1,28 @@
+package filter
+
+import (
+	"github.com/sourcenetwork/defradb/connor"
+)
+
+func copyFilterConditions(conditions any) any {
+	switch typedCond := conditions.(type) {
+	case map[connor.FilterKey]any:
+		result := make(map[connor.FilterKey]any)
+		for key, clause := range typedCond {
+			result[key] = copyFilterConditions(clause)
+		}
+		return result
+	case []any:
+		resultArr := make([]any, len(typedCond))
+		for i, elementClause := range typedCond {
+			resultArr[i] = copyFilterConditions(elementClause)
+		}
+		return resultArr
+	default:
+		return conditions
+	}
+}
+
+func Copy(filter map[connor.FilterKey]any) map[connor.FilterKey]any {
+	return copyFilterConditions(filter).(map[connor.FilterKey]any)
+}
