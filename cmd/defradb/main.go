26d8f72c4ed1e067ec1559221d4096b7350a3388
Keenan Nemetz <keenan.nemetz@gmail.com>
2023-09-28 16:33:28 -0700

    add comment about ignored error in cmd/defradb/main.go

--- cmd/defradb/main.go
+++ cmd/defradb/main.go
@@ -22,6 +22,11 @@ import (
 func main() {
 	defraCmd := cli.NewDefraCommand(config.DefaultConfig())
 	if err := defraCmd.Execute(); err != nil {
+		// this error is okay to discard because cobra
+		// logs any errors encountered during execution
+		//
+		// exiting with a non-zero status code signals
+		// that an error has ocurred during execution
 		os.Exit(1)
 	}
 }
