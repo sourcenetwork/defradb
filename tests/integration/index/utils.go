d1d05730d0a9e76e5c81f790a7bac32c764bfb9d
Islam Aleiv <islam@source.network>
2023-10-16 13:24:34 +0200

    Dependency graph

--- tests/integration/index/utils.go
+++ tests/integration/index/utils.go
@@ -12,6 +12,7 @@ package index
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 
 	"github.com/sourcenetwork/immutable"
@@ -34,11 +35,103 @@ func createSchemaWithDocs(schema string) []any {
 	parser := schemaParser{}
 	typeDefs := parser.Parse(schema)
 	generator := createDocGenerator{types: typeDefs}
-	for _, doc := range userDocs.docs {
+	order := findDependencyOrder(typeDefs)
+	randomDocsCols := generateRandomDocs(6, typeDefs, order)
+	for _, col := range randomDocsCols {
+		for _, doc := range col.docs {
+			actions := generator.GenerateDocs(doc, col.colName)
+			resultActions = append(resultActions, actions...)
+		}
+	}
+	/*for _, doc := range userDocs.docs {
 		actions := generator.GenerateDocs(doc, userDocs.colName)
 		resultActions = append(resultActions, actions...)
-	}
+	}*/
 	return resultActions
+}
+
+func getRandomString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func generateRandomValue(typeStr string) any {
+	switch typeStr {
+	case "String":
+		return getRandomString(10)
+	case "Int":
+		return rand.Intn(100)
+	case "Boolean":
+		return rand.Float32() < 0.5
+	case "Float":
+		return rand.Float64()
+	}
+	panic("Can not generate random value for unknown type: " + typeStr)
+}
+
+type doc = map[string]any
+type docRec struct {
+	doc    doc
+	docKey string
+}
+
+func generateRandomDocs(count int, types map[string]typeDefinition, order []string) []docsCollection {
+	counter := make(map[string]map[string]map[string]int)
+	cols := make(map[string][]docRec)
+	incrementCounter := func(primary, secondary, secondaryProp string) int {
+		if counter[primary] == nil {
+			counter[primary] = make(map[string]map[string]int)
+		}
+		if counter[primary][secondary] == nil {
+			counter[primary][secondary] = make(map[string]int)
+		}
+		prev := counter[primary][secondary][secondaryProp]
+		if prev >= len(cols[primary]) {
+			panic(fmt.Sprintf("Not enough docs for type %s", primary))
+		}
+		counter[primary][secondary][secondaryProp]++
+		return prev
+	}
+
+	getDocKey := func(typeName string, ind int) string {
+		if cols[typeName][ind].docKey == "" {
+			typeDef := types[typeName]
+			clientDoc, err := client.NewDocFromJSON([]byte(createDocJSON(cols[typeName][ind].doc, &typeDef)))
+			if err != nil {
+				panic("Failed to create doc from JSON: " + err.Error())
+			}
+			cols[typeName][ind].docKey = clientDoc.Key().String()
+		}
+		return cols[typeName][ind].docKey
+	}
+
+	result := []docsCollection{}
+	for _, typeName := range order {
+		col := docsCollection{colName: typeName}
+		for i := 0; i < count; i++ {
+			typeDef := types[typeName]
+			newDoc := make(doc)
+			for _, prop := range typeDef.props {
+				if prop.isRelation {
+					if prop.isPrimary.Value() {
+						relDocInd := incrementCounter(prop.typeStr, typeName, prop.name)
+						docKey := getDocKey(prop.typeStr, relDocInd)
+						newDoc[prop.name+request.RelatedObjectID] = docKey
+					}
+				} else {
+					newDoc[prop.name] = generateRandomValue(prop.typeStr)
+				}
+			}
+			cols[typeName] = append(cols[typeName], docRec{doc: newDoc})
+			col.docs = append(col.docs, newDoc)
+		}
+		result = append(result, col)
+	}
+	return result
 }
 
 type createDocGenerator struct {
@@ -175,6 +268,61 @@ type typeDefinition struct {
 	name  string
 	index int
 	props map[string]propDefinition
+}
+
+func findDependencyOrder(parsedTypes map[string]typeDefinition) []string {
+	graph := make(map[string][]string)
+	visited := make(map[string]bool)
+	stack := []string{}
+
+	appendUnique := func(slice []string, val string) []string {
+		for _, item := range slice {
+			if item == val {
+				return slice
+			}
+		}
+		return append(slice, val)
+	}
+
+	for typeName, typeDef := range parsedTypes {
+		for _, propDef := range typeDef.props {
+			if propDef.isRelation {
+				if propDef.isPrimary.Value() {
+					graph[propDef.typeStr] = appendUnique(graph[propDef.typeStr], typeName)
+				} else {
+					graph[typeName] = appendUnique(graph[typeName], propDef.typeStr)
+				}
+			}
+		}
+	}
+
+	var dfs func(node string)
+	dfs = func(node string) {
+		if visited[node] {
+			return
+		}
+		visited[node] = true
+
+		for _, neighbor := range graph[node] {
+			if !visited[neighbor] {
+				dfs(neighbor)
+			}
+		}
+
+		stack = append(stack, node)
+	}
+
+	for typeName := range parsedTypes {
+		if !visited[typeName] {
+			dfs(typeName)
+		}
+	}
+
+	for i, j := 0, len(stack)-1; i < j; i, j = i+1, j-1 {
+		stack[i], stack[j] = stack[j], stack[i]
+	}
+
+	return stack
 }
 
 type schemaParser struct {
