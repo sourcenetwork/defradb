e6d5da876b23b3b8e40991fd20ebb1add669cf45
Fred Carle <fredcarle@users.noreply.github.com>
2023-10-30 18:54:08 -0400

    update unit tests for dag worker.

--- net/dag_test.go
+++ net/dag_test.go
@@ -18,14 +18,13 @@ import (
 
 	dag "github.com/ipfs/boxo/ipld/merkledag"
 	"github.com/ipfs/go-cid"
-	format "github.com/ipfs/go-ipld-format"
 	ipld "github.com/ipfs/go-ipld-format"
 	mh "github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/require"
 
 	"github.com/sourcenetwork/defradb/client"
 	"github.com/sourcenetwork/defradb/core"
-	"github.com/sourcenetwork/defradb/core/crdt"
+	"github.com/sourcenetwork/defradb/merkle/clock"
 	netutils "github.com/sourcenetwork/defradb/net/utils"
 )
 
@@ -39,49 +38,6 @@ func TestSendJobWorker_ExitOnContextClose_NoError(t *testing.T) {
 		n.sendJobWorker()
 		close(done)
 	}()
-	n.Close()
-	select {
-	case <-done:
-	case <-time.After(timeout):
-		t.Error("failed to close sendJobWorker")
-	}
-}
-
-func TestSendJobWorker_WithNewJobWithClosePriorToProcessing_NoError(t *testing.T) {
-	ctx := context.Background()
-	db, n := newTestNode(ctx, t)
-	done := make(chan struct{})
-	go func() {
-		n.sendJobWorker()
-		close(done)
-	}()
-	_, err := db.AddSchema(ctx, `type User {
-		name: String
-		age: Int
-	}`)
-	require.NoError(t, err)
-
-	col, err := db.GetCollectionByName(ctx, "User")
-	require.NoError(t, err)
-
-	doc, err := client.NewDocFromJSON([]byte(`{"name": "John", "age": 30}`))
-	require.NoError(t, err)
-	dsKey := core.DataStoreKeyFromDocKey(doc.Key())
-
-	txn, err := db.NewTxn(ctx, false)
-	require.NoError(t, err)
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	n.sendJobs <- &dagJob{
-		session:    &wg,
-		node:       &EmptyNode{},
-		collection: col,
-		dsKey:      dsKey,
-		txn:        txn,
-	}
-
 	n.Close()
 	select {
 	case <-done:
@@ -104,9 +60,6 @@ func TestSendJobWorker_WithNewJob_NoError(t *testing.T) {
 	}`)
 	require.NoError(t, err)
 
-	col, err := db.GetCollectionByName(ctx, "User")
-	require.NoError(t, err)
-
 	doc, err := client.NewDocFromJSON([]byte(`{"name": "John", "age": 30}`))
 	require.NoError(t, err)
 	dsKey := core.DataStoreKeyFromDocKey(doc.Key())
@@ -118,11 +71,9 @@ func TestSendJobWorker_WithNewJob_NoError(t *testing.T) {
 	wg.Add(1)
 
 	n.sendJobs <- &dagJob{
-		session:    &wg,
-		node:       &EmptyNode{},
-		collection: col,
-		dsKey:      dsKey,
-		txn:        txn,
+		session: &wg,
+		dsKey:   dsKey,
+		txn:     txn,
 	}
 	// Give the jobworker time to process the job.
 	time.Sleep(100 * time.Microsecond)
@@ -148,9 +99,6 @@ func TestSendJobWorker_WithCloseJob_NoError(t *testing.T) {
 	}`)
 	require.NoError(t, err)
 
-	col, err := db.GetCollectionByName(ctx, "User")
-	require.NoError(t, err)
-
 	doc, err := client.NewDocFromJSON([]byte(`{"name": "John", "age": 30}`))
 	require.NoError(t, err)
 	dsKey := core.DataStoreKeyFromDocKey(doc.Key())
@@ -162,11 +110,9 @@ func TestSendJobWorker_WithCloseJob_NoError(t *testing.T) {
 	wg.Add(1)
 
 	n.sendJobs <- &dagJob{
-		session:    &wg,
-		node:       &EmptyNode{},
-		collection: col,
-		dsKey:      dsKey,
-		txn:        txn,
+		session: &wg,
+		dsKey:   dsKey,
+		txn:     txn,
 	}
 
 	n.closeJob <- dsKey.DocKey
@@ -179,7 +125,7 @@ func TestSendJobWorker_WithCloseJob_NoError(t *testing.T) {
 	}
 }
 
-func TestSendJobWorker_WithPeerAndNoChildren_NoError(t *testing.T) {
+func TestSendJobWorker_WithPeer_NoError(t *testing.T) {
 	ctx := context.Background()
 	db1, n1 := newTestNode(ctx, t)
 	db2, n2 := newTestNode(ctx, t)
@@ -188,6 +134,10 @@ func TestSendJobWorker_WithPeerAndNoChildren_NoError(t *testing.T) {
 	require.NoError(t, err)
 	n2.Bootstrap(addrs)
 
+	err = n1.WaitForPeerConnectionEvent(n2.PeerID())
+	require.NoError(t, err)
+	err = n2.WaitForPeerConnectionEvent(n1.PeerID())
+	require.NoError(t, err)
 	done := make(chan struct{})
 	go func() {
 		n2.sendJobWorker()
@@ -215,132 +165,48 @@ func TestSendJobWorker_WithPeerAndNoChildren_NoError(t *testing.T) {
 	err = col.Create(ctx, doc)
 	require.NoError(t, err)
 
-	txn, err := db2.NewTxn(ctx, false)
+	txn1, _ := db1.NewTxn(ctx, false)
+	heads, _, err := clock.NewHeadSet(txn1.Headstore(), dsKey.ToHeadStoreKey().WithFieldId(core.COMPOSITE_NAMESPACE)).List(ctx)
+	require.NoError(t, err)
+	txn1.Discard(ctx)
+
+	txn2, err := db2.NewTxn(ctx, false)
 	require.NoError(t, err)
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 
-	delta := &crdt.CompositeDAGDelta{
-		SchemaVersionID: col.Schema().VersionID,
-		Priority:        1,
-		DocKey:          doc.Key().Bytes(),
-	}
-
-	node, err := makeNode(delta, []cid.Cid{})
-	require.NoError(t, err)
-
-	var getter format.NodeGetter = n2.Peer.newDAGSyncerTxn(txn)
+	var getter ipld.NodeGetter = n2.Peer.newDAGSyncerTxn(txn2)
 	if sessionMaker, ok := getter.(SessionDAGSyncer); ok {
 		log.Debug(ctx, "Upgrading DAGSyncer with a session")
 		getter = sessionMaker.Session(ctx)
 	}
 
 	n2.sendJobs <- &dagJob{
-		session:    &wg,
-		nodeGetter: getter,
-		node:       node,
-		collection: col,
-		dsKey:      dsKey,
-		txn:        txn,
+		bp:          newBlockProcessor(n2.Peer),
+		session:     &wg,
+		nodeGetter:  getter,
+		dsKey:       dsKey,
+		txn:         txn2,
+		cid:         heads[0],
+		isComposite: true,
 	}
-	// Give the jobworker time to process the job.
-	time.Sleep(100 * time.Microsecond)
-	n1.Close()
-	n2.Close()
-	select {
-	case <-done:
-	case <-time.After(timeout):
-		t.Error("failed to close sendJobWorker")
-	}
-}
+	wg.Wait()
 
-func TestSendJobWorker_WithPeerAndChildren_NoError(t *testing.T) {
-	ctx := context.Background()
-	db1, n1 := newTestNode(ctx, t)
-	db2, n2 := newTestNode(ctx, t)
-
-	addrs, err := netutils.ParsePeers([]string{n1.host.Addrs()[0].String() + "/p2p/" + n1.PeerID().String()})
-	require.NoError(t, err)
-	n2.Bootstrap(addrs)
-
-	done := make(chan struct{})
-	go func() {
-		n2.sendJobWorker()
-		close(done)
-	}()
-
-	_, err = db1.AddSchema(ctx, `type User {
-		name: String
-		age: Int
-	}`)
-	require.NoError(t, err)
-	_, err = db2.AddSchema(ctx, `type User {
-		name: String
-		age: Int
-	}`)
+	err = txn2.Commit(ctx)
 	require.NoError(t, err)
 
-	col, err := db1.GetCollectionByName(ctx, "User")
+	block, err := n1.db.Blockstore().Get(ctx, heads[0])
+	require.NoError(t, err)
+	nd, err := dag.DecodeProtobufBlock(block)
 	require.NoError(t, err)
 
-	doc, err := client.NewDocFromJSON([]byte(`{"name": "John", "age": 30}`))
-	require.NoError(t, err)
-	dsKey := core.DataStoreKeyFromDocKey(doc.Key())
-
-	err = col.Create(ctx, doc)
-	require.NoError(t, err)
-
-	txn, err := db2.NewTxn(ctx, false)
-	require.NoError(t, err)
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	links := []core.DAGLink{}
-	for k := range doc.Fields() {
-		delta := &crdt.LWWRegDelta{
-			SchemaVersionID: col.Schema().VersionID,
-			Priority:        1,
-			DocKey:          doc.Key().Bytes(),
-			FieldName:       k,
-		}
-
-		node, err := makeNode(delta, []cid.Cid{})
+	for _, link := range nd.Links() {
+		exists, err := n2.db.Blockstore().Has(ctx, link.Cid)
 		require.NoError(t, err)
-
-		links = append(links, core.DAGLink{
-			Name: k,
-			Cid:  node.Cid(),
-		})
+		require.True(t, exists)
 	}
 
-	delta := &crdt.CompositeDAGDelta{
-		SchemaVersionID: col.Schema().VersionID,
-		Priority:        1,
-		DocKey:          doc.Key().Bytes(),
-		SubDAGs:         links,
-	}
-
-	node, err := makeNode(delta, []cid.Cid{})
-	require.NoError(t, err)
-
-	var getter format.NodeGetter = n2.Peer.newDAGSyncerTxn(txn)
-	if sessionMaker, ok := getter.(SessionDAGSyncer); ok {
-		log.Debug(ctx, "Upgrading DAGSyncer with a session")
-		getter = sessionMaker.Session(ctx)
-	}
-
-	n2.sendJobs <- &dagJob{
-		session:    &wg,
-		nodeGetter: getter,
-		node:       node,
-		collection: col,
-		dsKey:      dsKey,
-		txn:        txn,
-	}
-	// Give the jobworker time to process the job.
-	time.Sleep(100 * time.Microsecond)
 	n1.Close()
 	n2.Close()
 	select {
