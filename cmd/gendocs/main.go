195d5b77eeade26f0b873eacb97212309512b0a5
Islam Aleiv <islam@source.network>
2023-12-01 13:37:50 +0100

    Add documentation

--- cmd/gendocs/main.go
+++ cmd/gendocs/main.go
@@ -1,3 +1,16 @@
+// Copyright 2023 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+/*
+gendocs is a tool to generate the collections' documents automatically.
+*/
 package main
 
 import (
