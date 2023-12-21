eef92614a2e1e8d0c113aba40ed1e5d4b20d3470
Shahzad Lone <shahzadlone@gmail.com>
2023-12-21 13:04:03 -0500

    PR(RESOLVE-CONFLICT): Fix conflict from views PR

--- client/request/select.go
+++ client/request/select.go
@@ -114,7 +114,7 @@ func (s *Select) validateGroupBy() []error {
 // of `Select` objects.
 type selectJson struct {
 	Field
-	DocKeys     immutable.Option[[]string]
+	DocIDs      immutable.Option[[]string]
 	CID         immutable.Option[string]
 	Root        SelectionType
 	Limit       immutable.Option[uint64]
@@ -140,7 +140,7 @@ func (s *Select) UnmarshalJSON(bytes []byte) error {
 	}
 
 	s.Field = selectMap.Field
-	s.DocKeys = selectMap.DocKeys
+	s.DocIDs = selectMap.DocIDs
 	s.CID = selectMap.CID
 	s.Root = selectMap.Root
 	s.Limit = selectMap.Limit
