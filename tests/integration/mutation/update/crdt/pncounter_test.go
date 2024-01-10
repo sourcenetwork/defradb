f0687c9c4bc5ce6194e0b57fd5851f6b98487c70
Keenan Nemetz <keenan.nemetz@gmail.com>
2024-01-10 14:06:31 -0800

    fix pncounter test

--- tests/integration/mutation/update/crdt/pncounter_test.go
+++ tests/integration/mutation/update/crdt/pncounter_test.go
@@ -16,6 +16,7 @@ import (
 	"testing"
 
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	"github.com/sourcenetwork/immutable"
 )
 
 func TestPNCounterUpdate_IntKindWithPositiveIncrement_ShouldIncrement(t *testing.T) {
@@ -72,6 +73,12 @@ func TestPNCounterUpdate_IntKindWithPositiveIncrement_ShouldIncrement(t *testing
 func TestPNCounterUpdate_IntKindWithPositiveIncrementOverflow_RollsOverToMinInt64(t *testing.T) {
 	test := testUtils.TestCase{
 		Description: "Positive increments of a PN Counter with Int type causing overflow behaviour",
+		SupportedMutationTypes: immutable.Some([]testUtils.MutationType{
+			// GQL mutation will return an error
+			// when integer type overflows
+			testUtils.CollectionNamedMutationType,
+			testUtils.CollectionSaveMutationType,
+		}),
 		Actions: []any{
 			testUtils.SchemaUpdate{
 				Schema: `
