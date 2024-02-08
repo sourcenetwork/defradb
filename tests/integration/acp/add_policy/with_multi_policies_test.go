c098d7dffad4bd5141c5922b4ce788a43f333f03
Shahzad Lone <shahzadlone@gmail.com>
2024-02-08 15:02:23 -0500

    PR(ACP-TEST): Test 'Adding Of Policy' With ACP

---
+++ tests/integration/acp/add_policy/with_multi_policies_test.go
@@ -0,0 +1,351 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_acp_add_policy
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestACP_AddPolicy_AddMultipleDifferentPolicies_ValidPolicyIDs(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add multiple different policies",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: a policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+
+                `,
+
+				ExpectedPolicyID: "dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a",
+			},
+
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: another policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner + reader
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                          reader:
+                            types:
+                              - actor
+                          admin:
+                            manages:
+                              - reader
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: "53980e762616fcffbe76307995895e862f87ef3f21d509325d1dc772a770b001",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestACP_AddPolicy_AddMultipleDifferentPoliciesInDifferentFmts_ValidPolicyIDs(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add multiple different policies in different formats",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    {
+                      "description": "a policy",
+                      "actor": {
+                        "name": "actor"
+                      },
+                      "resources": {
+                        "users": {
+                          "permissions": {
+                            "read": {
+                              "expr": "owner"
+                            },
+                            "write": {
+                              "expr": "owner"
+                            }
+                          },
+                          "relations": {
+                            "owner": {
+                              "types": [
+                                "actor"
+                              ]
+                            }
+                          }
+                        }
+                      }
+                    }
+                `,
+
+				ExpectedPolicyID: "dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a",
+			},
+
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: another policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner + reader
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                          reader:
+                            types:
+                              - actor
+                          admin:
+                            manages:
+                              - reader
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: "53980e762616fcffbe76307995895e862f87ef3f21d509325d1dc772a770b001",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestACP_AddPolicy_AddDuplicatePolicyByOtherCreator_ValidPolicyIDs(t *testing.T) {
+	const policyUsedByBoth string = `
+        description: a policy
+
+        actor:
+          name: actor
+
+        resources:
+          users:
+            permissions:
+              read:
+                expr: owner
+              write:
+                expr: owner
+
+            relations:
+              owner:
+                types:
+                  - actor
+    `
+
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add duplicate policies by different actors, valid",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: policyUsedByBoth,
+
+				ExpectedPolicyID: "dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a",
+			},
+
+			testUtils.AddPolicy{
+				Creator: actor2Signature,
+
+				Policy: policyUsedByBoth,
+
+				ExpectedPolicyID: "551c57323f33decfdc23312e5e1036e3ab85d2414e962814dab9101619dd9ff9",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestACP_AddPolicy_AddMultipleDuplicatePolicies_Error(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add duplicate policies, error",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: a policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+
+                `,
+
+				ExpectedPolicyID: "dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a",
+			},
+
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: a policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+
+                `,
+
+				ExpectedError: "policy dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a: policy exists",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestACP_AddPolicy_AddMultipleDuplicatePoliciesDifferentFmts_Error(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add duplicate policies different formats, error",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: a policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: "dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a",
+			},
+
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                   {
+                     "description": "a policy",
+                     "actor": {
+                       "name": "actor"
+                     },
+                     "resources": {
+                       "users": {
+                         "permissions": {
+                           "read": {
+                             "expr": "owner"
+                           },
+                           "write": {
+                             "expr": "owner"
+                           }
+                         },
+                         "relations": {
+                           "owner": {
+                             "types": [
+                               "actor"
+                             ]
+                           }
+                         }
+                       }
+                     }
+                   }
+               `,
+
+				ExpectedError: "policy dfe202ffb4f0fe9b46157c313213a3839e08a6f0a7c3aba55e4724cb49ffde8a: policy exists",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
---
+++ tests/integration/acp/add_policy/with_no_resources_test.go
@@ -0,0 +1,92 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_acp_add_policy
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+// Eventhough empty resources make no sense from a DefraDB (DPI) perspective,
+// it is still a valid sourcehub policy for now.
+func TestACP_AddPolicy_NoResource_ValidID(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add policy, no resource, valid policy",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: a policy
+
+                    actor:
+                      name: actor
+
+                    resources:
+                `,
+
+				ExpectedPolicyID: "b72d8ec56ffb141922781d2b1b0803404bef57be0eeec98f1662f3017fc2de35",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+// Eventhough empty resources make no sense from a DefraDB (DPI) perspective,
+// it is still a valid sourcehub policy for now.
+func TestACP_AddPolicy_NoResourceLabel_ValidID(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add policy, no resource label, valid policy",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: a policy
+
+                    actor:
+                      name: actor
+                `,
+
+				ExpectedPolicyID: "b72d8ec56ffb141922781d2b1b0803404bef57be0eeec98f1662f3017fc2de35",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+// Eventhough empty resources make no sense from a DefraDB (DPI) perspective,
+// it is still a valid sourcehub policy for now.
+func TestACP_AddPolicy_PolicyWithOnlySpace_ValidID(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Description: "Test acp, adding a policy that has only space",
+
+		Actions: []any{
+			testUtils.AddPolicy{
+				Creator: actor1Signature,
+
+				Policy: " ",
+
+				ExpectedPolicyID: "b72d8ec56ffb141922781d2b1b0803404bef57be0eeec98f1662f3017fc2de35",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
