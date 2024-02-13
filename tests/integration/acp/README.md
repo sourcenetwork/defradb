9cbb7ea5bea8755b1a7c22c78169c7bfb6684b89
Shahzad Lone <shahzadlone@gmail.com>
2024-02-13 01:47:06 -0500

    PR(ACP-TEST): Test Permission Schema Reject/Accept
    Note: These tests are responsible for ensuring validation occurs
    properly when a schema is linked to a resource and policyID, ensure that
    we only accepting the schema if the policyID is linked to a valid DPI
    or atleast the resource that is linked is DPI compliant, otherwise the
    schema MUST be rejected.

--- tests/integration/acp/README.md
+++ tests/integration/acp/README.md
@@ -1,6 +1,20 @@
-## More Information on what each directory tests.
+## More Information on ACP test directories.
 
 
-1) `./add_policy`
+1) `./defradb/tests/integration/acp/add_policy`
     - This directory tests ONLY the `Adding of a Policy` through DefraDB.
+    - Does NOT assert the schema.
     - Does NOT test DPI validation.
+
+2) `./defradb/tests/integration/acp/schema/add_dpi`
+    - This directory tests the loading/adding of a schema that has `@policy(id, resource)`
+      specified. The tests ensure that only a schema linking to
+      a valid DPI policy is accepted. Naturally these tests will also be `Adding a Policy`
+      through DefraDB like in (1) before actually adding the schema. If a schema has a
+      policy specified that doesn't exist (or wasn't added yet), that schema WILL/MUST
+      be rejected in these tests.
+    - The tests assert the schema after to ensure rejection/acceptance.
+    - Tests DPI validation.
+
+
+### Learn more about [DPI Rules](/acp/README.md)
---
+++ tests/integration/acp/schema/add_dpi/accept_same_resource_on_diff_schemas_test.go
@@ -0,0 +1,172 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_acp_schema_add_dpi
+
+import (
+	"fmt"
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	schemaUtils "github.com/sourcenetwork/defradb/tests/integration/schema"
+)
+
+func TestACP_AddDPISchema_UseSameResourceOnDifferentSchemas_AcceptSchemas(t *testing.T) {
+	policyIDOfValidDPI := "4f13c5084c3d0e1e5c5db702fceef84c3b6ab948949ca8e27fcaad3fb8bc39f4"
+	sharedSameResourceName := "users"
+
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add dpi schema, where one resource is specified on different schemas, schemas accepted",
+
+		Actions: []any{
+
+			testUtils.AddPolicy{
+
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: A Valid Defra Policy Interface (DPI)
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner + reader
+                          write:
+                            expr: owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                          reader:
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: policyIDOfValidDPI,
+			},
+
+			testUtils.SchemaUpdate{
+				Schema: fmt.Sprintf(`
+					type OldUsers @policy(
+						id: "%s",
+						resource: "%s"
+					) {
+						name: String
+						age: Int
+					}
+				`,
+					policyIDOfValidDPI,
+					sharedSameResourceName,
+				),
+			},
+
+			testUtils.IntrospectionRequest{
+				Request: `
+					query {
+						__type (name: "OldUsers") {
+							name
+							fields {
+								name
+								type {
+								name
+								kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "OldUsers", // NOTE: "OldUsers" MUST exist
+						"fields": schemaUtils.DefaultFields.Append(
+							schemaUtils.Field{
+								"name": "name",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "String",
+								},
+							},
+						).Append(
+							schemaUtils.Field{
+								"name": "age",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "Int",
+								},
+							},
+						).Tidy(),
+					},
+				},
+			},
+
+			testUtils.SchemaUpdate{
+				Schema: fmt.Sprintf(`
+					type NewUsers @policy(
+						id: "%s",
+						resource: "%s"
+					) {
+						name: String
+						age: Int
+					}
+				`,
+					policyIDOfValidDPI,
+					sharedSameResourceName,
+				),
+			},
+
+			testUtils.IntrospectionRequest{
+				Request: `
+					query {
+						__type (name: "NewUsers") {
+							name
+							fields {
+								name
+								type {
+								name
+								kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "NewUsers", // NOTE: "NewUsers" MUST exist
+						"fields": schemaUtils.DefaultFields.Append(
+							schemaUtils.Field{
+								"name": "name",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "String",
+								},
+							},
+						).Append(
+							schemaUtils.Field{
+								"name": "age",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "Int",
+								},
+							},
+						).Tidy(),
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
