77fca7b958a6907c7a07126213b9fcbf04b5bbd7
Shahzad Lone <shahzadlone@gmail.com>
2024-02-13 01:47:06 -0500

    PR(ACP-TEST): Test Permission Schema Reject/Accept
    Note: These tests are responsible for ensuring validation occurs
    properly when a schema is linked to a resource and policyID, ensure that
    we only accepting the schema if the policyID is linked to a valid DPI
    or atleast the resource that is linked is DPI compliant, otherwise the
    schema MUST be rejected.

---
+++ tests/integration/acp/schema/add_dpi/accept_extra_permissions_on_dpi_test.go
@@ -0,0 +1,316 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_acp_schema_add_dpi
+
+import (
+	"fmt"
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	schemaUtils "github.com/sourcenetwork/defradb/tests/integration/schema"
+)
+
+func TestACP_AddDPISchema_WithExtraPermsHavingRequiredRelation_AcceptSchema(t *testing.T) {
+	policyIDOfValidDPI := "16e39e650d4cbd5161ae0c572edad6f7e2950c1c4afa37e427af3c8708e68f0f"
+
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add dpi schema, with extra permissions having required relation, schema accepted",
+
+		Actions: []any{
+
+			testUtils.AddPolicy{
+
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: A Valid Defra Policy Interface (DPI)
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner + reader
+                          write:
+                            expr: owner + reader
+                          magic:
+                            expr: owner - reader
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                          reader:
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: policyIDOfValidDPI,
+			},
+
+			testUtils.SchemaUpdate{
+				Schema: fmt.Sprintf(`
+					type Users @policy(
+						id: "%s",
+						resource: "users"
+					) {
+						name: String
+						age: Int
+					}
+				`,
+					policyIDOfValidDPI,
+				),
+			},
+
+			testUtils.IntrospectionRequest{
+				Request: `
+					query {
+						__type (name: "Users") {
+							name
+							fields {
+								name
+								type {
+								name
+								kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "Users", // NOTE: "Users" MUST exist
+						"fields": schemaUtils.DefaultFields.Append(
+							schemaUtils.Field{
+								"name": "name",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "String",
+								},
+							},
+						).Append(
+							schemaUtils.Field{
+								"name": "age",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "Int",
+								},
+							},
+						).Tidy(),
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestACP_AddDPISchema_WithExtraPermsHavingRequiredRelationInTheEnd_AcceptSchema(t *testing.T) {
+	policyIDOfValidDPI := "35b6f3db54cfb0f451a4faba77d2c71d8718215caeb5a15a8570dfdba07b694d"
+
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add dpi schema, with extra permissions having required relation in the end, schema accepted",
+
+		Actions: []any{
+
+			testUtils.AddPolicy{
+
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: A Valid Defra Policy Interface (DPI)
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner + reader
+                          write:
+                            expr: owner
+                          magic:
+                            expr: reader & owner
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                          reader:
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: policyIDOfValidDPI,
+			},
+
+			testUtils.SchemaUpdate{
+				Schema: fmt.Sprintf(`
+					type Users @policy(
+						id: "%s",
+						resource: "users"
+					) {
+						name: String
+						age: Int
+					}
+				`,
+					policyIDOfValidDPI,
+				),
+			},
+
+			testUtils.IntrospectionRequest{
+				Request: `
+					query {
+						__type (name: "Users") {
+							name
+							fields {
+								name
+								type {
+								name
+								kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "Users", // NOTE: "Users" MUST exist
+						"fields": schemaUtils.DefaultFields.Append(
+							schemaUtils.Field{
+								"name": "name",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "String",
+								},
+							},
+						).Append(
+							schemaUtils.Field{
+								"name": "age",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "Int",
+								},
+							},
+						).Tidy(),
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestACP_AddDPISchema_WithExtraPermsHavingNoRequiredRelation_AcceptSchema(t *testing.T) {
+	policyIDOfValidDPI := "7b6266a93bfb6920bf57884f55c3823a5a5147c4ce445a9fc703b7c1e59b2d12"
+
+	test := testUtils.TestCase{
+
+		Description: "Test acp, add dpi schema, with extra permissions having no required relation, schema accepted",
+
+		Actions: []any{
+
+			testUtils.AddPolicy{
+
+				Creator: actor1Signature,
+
+				Policy: `
+                    description: A Valid Defra Policy Interface (DPI)
+
+                    actor:
+                      name: actor
+
+                    resources:
+                      users:
+                        permissions:
+                          read:
+                            expr: owner + reader
+                          write:
+                            expr: owner
+                          magic:
+                            expr: reader
+
+                        relations:
+                          owner:
+                            types:
+                              - actor
+                          reader:
+                            types:
+                              - actor
+                `,
+
+				ExpectedPolicyID: policyIDOfValidDPI,
+			},
+
+			testUtils.SchemaUpdate{
+				Schema: fmt.Sprintf(`
+					type Users @policy(
+						id: "%s",
+						resource: "users"
+					) {
+						name: String
+						age: Int
+					}
+				`,
+					policyIDOfValidDPI,
+				),
+			},
+
+			testUtils.IntrospectionRequest{
+				Request: `
+					query {
+						__type (name: "Users") {
+							name
+							fields {
+								name
+								type {
+								name
+								kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "Users", // NOTE: "Users" MUST exist
+						"fields": schemaUtils.DefaultFields.Append(
+							schemaUtils.Field{
+								"name": "name",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "String",
+								},
+							},
+						).Append(
+							schemaUtils.Field{
+								"name": "age",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "Int",
+								},
+							},
+						).Tidy(),
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
