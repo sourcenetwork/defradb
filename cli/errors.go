52b4ecf32fb13f64442b632e7a3310a2334c8683
Shahzad Lone <shahzadlone@gmail.com>
2024-02-27 07:01:18 -0500

    PR(ACP): Add CLI Client For Adding Policy Command

--- cli/errors.go
+++ cli/errors.go
@@ -11,24 +11,36 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/sourcenetwork/defradb/errors"
 )
 
 const (
 	errInvalidLensConfig        string = "invalid lens configuration"
 	errSchemaVersionNotOfSchema string = "the given schema version is from a different schema"
+	errRequiredFlag             string = "the required flag [--%s|-%s] is %s"
 )
 
 var (
-	ErrNoDocOrFile              = errors.New("document or file must be defined")
-	ErrInvalidDocument          = errors.New("invalid document")
-	ErrNoDocIDOrFilter          = errors.New("docID or filter must be defined")
-	ErrInvalidExportFormat      = errors.New("invalid export format")
-	ErrNoLensConfig             = errors.New("lens config cannot be empty")
-	ErrInvalidLensConfig        = errors.New("invalid lens configuration")
-	ErrSchemaVersionNotOfSchema = errors.New(errSchemaVersionNotOfSchema)
-	ErrViewAddMissingArgs       = errors.New("please provide a base query and output SDL for this view")
+	ErrNoDocOrFile                = errors.New("document or file must be defined")
+	ErrInvalidDocument            = errors.New("invalid document")
+	ErrNoDocIDOrFilter            = errors.New("docID or filter must be defined")
+	ErrInvalidExportFormat        = errors.New("invalid export format")
+	ErrNoLensConfig               = errors.New("lens config cannot be empty")
+	ErrInvalidLensConfig          = errors.New("invalid lens configuration")
+	ErrSchemaVersionNotOfSchema   = errors.New(errSchemaVersionNotOfSchema)
+	ErrViewAddMissingArgs         = errors.New("please provide a base query and output SDL for this view")
+	ErrPolicyFileArgCanNotBeEmpty = errors.New("policy file argument can not be empty")
 )
+
+func NewErrRequiredFlagEmpty(longName string, shortName string) error {
+	return errors.New(fmt.Sprintf(errRequiredFlag, longName, shortName, "empty"))
+}
+
+func NewErrRequiredFlagInvalid(longName string, shortName string) error {
+	return errors.New(fmt.Sprintf(errRequiredFlag, longName, shortName, "invalid"))
+}
 
 func NewErrInvalidLensConfig(inner error) error {
 	return errors.Wrap(errInvalidLensConfig, inner)
