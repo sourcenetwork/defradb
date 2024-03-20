ccb38d6bc1c708129ac67731c2591867bf04a662
Islam Aleiv <islam@source.network>
2024-03-20 13:12:06 +0100

    Add documentation

--- client/normal_value.go
+++ client/normal_value.go
@@ -17,63 +17,118 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// NormalValue is the interface for the normal value types.
+// It is used to represent the normal (or standard) values across the system and to avoid
+// asserting all possible types like int, int32, int64, etc.
+//
+// All methods returning a specific type returns the value and the second boolean flag indicating
+// if the value is of the requested type.
+//
+// All nillable values are represented as immutable.Option[T].
 type NormalValue interface {
+	// Any returns the underlying value.
 	Any() any
 
+	// IsNil returns if the value is nil.
 	IsNil() bool
+	// IsNillable returns if the value can be nil.
 	IsNillable() bool
 
+	// Bool returns the value as a bool.
 	Bool() (bool, bool)
+	// Int returns the value as an int64.
 	Int() (int64, bool)
+	// Float returns the value as a float64.
 	Float() (float64, bool)
+	// String returns the value as a string.
 	String() (string, bool)
+	// Bytes returns the value as a byte slice.
 	Bytes() ([]byte, bool)
+	// Time returns the value as a time.Time.
 	Time() (time.Time, bool)
+	// Document returns the value as a *Document.
 	Document() (*Document, bool)
 
+	// NillableBool returns the value as a nillable bool.
 	NillableBool() (immutable.Option[bool], bool)
+	// NillableInt returns the value as a nillable int64.
 	NillableInt() (immutable.Option[int64], bool)
+	// NillableFloat returns the value as a nillable float64.
 	NillableFloat() (immutable.Option[float64], bool)
+	// NillableString returns the value as a nillable string.
 	NillableString() (immutable.Option[string], bool)
+	// NillableBytes returns the value as a nillable byte slice.
 	NillableBytes() (immutable.Option[[]byte], bool)
+	// NillableTime returns the value as a nillable time.Time.
 	NillableTime() (immutable.Option[time.Time], bool)
+	// NillableDocument returns the value as a nillable *Document.
 	NillableDocument() (immutable.Option[*Document], bool)
 
+	// IsArray returns if the value is an array.
 	IsArray() bool
 
+	// BoolArray returns the value as a bool array.
 	BoolArray() ([]bool, bool)
+	// IntArray returns the value as an int64 array.
 	IntArray() ([]int64, bool)
+	// FloatArray returns the value as a float64 array.
 	FloatArray() ([]float64, bool)
+	// StringArray returns the value as a string array.
 	StringArray() ([]string, bool)
+	// BytesArray returns the value as a byte slice array.
 	BytesArray() ([][]byte, bool)
+	// TimeArray returns the value as a time.Time array.
 	TimeArray() ([]time.Time, bool)
+	// DocumentArray returns the value as a *Document array.
 	DocumentArray() ([]*Document, bool)
 
+	// NillableBoolArray returns the value as nillable array of bool elements.
 	BoolNillableArray() (immutable.Option[[]bool], bool)
+	// NillableIntArray returns the value as nillable array of int64 elements.
 	IntNillableArray() (immutable.Option[[]int64], bool)
+	// NillableFloatArray returns the value as nillable array of float64 elements.
 	FloatNillableArray() (immutable.Option[[]float64], bool)
+	// NillableStringArray returns the value as nillable array of string elements.
 	StringNillableArray() (immutable.Option[[]string], bool)
+	// NillableBytesArray returns the value as nillable array of byte slice elements.
 	BytesNillableArray() (immutable.Option[[][]byte], bool)
+	// NillableTimeArray returns the value as nillable array of time.Time elements.
 	TimeNillableArray() (immutable.Option[[]time.Time], bool)
+	// NillableDocumentArray returns the value as nillable array of *Document elements.
 	DocumentNillableArray() (immutable.Option[[]*Document], bool)
 
+	// NillableBoolArray returns the value as array of nillable bool elements.
 	NillableBoolArray() ([]immutable.Option[bool], bool)
+	// NillableIntArray returns the value as array of nillable int64 elements.
 	NillableIntArray() ([]immutable.Option[int64], bool)
+	// NillableFloatArray returns the value as array of nillable float64 elements.
 	NillableFloatArray() ([]immutable.Option[float64], bool)
+	// NillableStringArray returns the value as array of nillable string elements.
 	NillableStringArray() ([]immutable.Option[string], bool)
+	// NillableBytesArray returns the value as array of nillable byte slice elements.
 	NillableBytesArray() ([]immutable.Option[[]byte], bool)
+	// NillableTimeArray returns the value as array of nillable time.Time elements.
 	NillableTimeArray() ([]immutable.Option[time.Time], bool)
+	// NillableDocumentArray returns the value as array of nillable *Document elements.
 	NillableDocumentArray() ([]immutable.Option[*Document], bool)
 
+	// NillableBoolNillableArray returns the value as nillable array of nillable bool elements.
 	NillableBoolNillableArray() (immutable.Option[[]immutable.Option[bool]], bool)
+	// NillableIntNillableArray returns the value as nillable array of nillable int64 elements.
 	NillableIntNillableArray() (immutable.Option[[]immutable.Option[int64]], bool)
+	// NillableFloatNillableArray returns the value as nillable array of nillable float64 elements.
 	NillableFloatNillableArray() (immutable.Option[[]immutable.Option[float64]], bool)
+	// NillableStringNillableArray returns the value as nillable array of nillable string elements.
 	NillableStringNillableArray() (immutable.Option[[]immutable.Option[string]], bool)
+	// NillableBytesNillableArray returns the value as nillable array of nillable byte slice elements.
 	NillableBytesNillableArray() (immutable.Option[[]immutable.Option[[]byte]], bool)
+	// NillableTimeNillableArray returns the value as nillable array of nillable time.Time elements.
 	NillableTimeNillableArray() (immutable.Option[[]immutable.Option[time.Time]], bool)
+	// NillableDocumentNillableArray returns the value as nillable array of nillable *Document elements.
 	NillableDocumentNillableArray() (immutable.Option[[]immutable.Option[*Document]], bool)
 }
 
+// NormalValue is dummy implementation of NormalValue to be embedded in other types.
 type NormalVoid struct{}
 
 func (NormalVoid) IsNil() bool {
@@ -696,6 +751,15 @@ func newNormalFloat(val float64) NormalValue {
 	return normalFloat{newBaseNormalValue(val)}
 }
 
+// NewNormalValue creates a new NormalValue from the given value.
+// It will normalize all known types that can be converted to normal ones.
+// For example, is the given type is `[]int32`, it will be converted to `[]int64`.
+// If the given value is of type `[]any` is will to through every element and try to convert it
+// minimal common type and normalizes it.
+// For examples, the following conversions will be made:
+//   - `[]any{int32(1), int64(2)}` -> `[]int64{1, 2}`.
+//   - `[]any{int32(1), int64(2), float32(1.5)}` -> `[]float64{1.0, 2.0, 1.5}`.
+//   - `[]any{int32(1), nil}` -> `[]immutable.Option[int64]{immutable.Some(1), immutable.None[int64]()}`.
 func NewNormalValue(val any) (NormalValue, error) {
 	if val == nil {
 		return normalNil{}, nil
@@ -1135,156 +1199,194 @@ func convertAnyArrToNillableTypedArr[T any](
 	return newNormalNillableArr(result), nil
 }
 
+// NewNormalNil creates a new NormalValue that represents a nil value.
 func NewNormalNil() NormalValue {
 	return normalNil{}
 }
 
+// NewNormalBool creates a new NormalValue that represents a `bool` value.
 func NewNormalBool(val bool) NormalValue {
 	return normalBool{baseNormalValue[bool]{val: val}}
 }
 
+// NewNormalInt creates a new NormalValue that represents an `int64` value.
 func NewNormalInt[T constraints.Integer | constraints.Float](val T) NormalValue {
 	return normalInt{baseNormalValue[int64]{val: int64(val)}}
 }
 
+// NewNormalFloat creates a new NormalValue that represents a `float64` value.
 func NewNormalFloat[T constraints.Integer | constraints.Float](val T) NormalValue {
 	return normalFloat{baseNormalValue[float64]{val: float64(val)}}
 }
 
+// NewNormalString creates a new NormalValue that represents a `string` value.
 func NewNormalString[T string | []byte](val T) NormalValue {
 	return normalString{baseNormalValue[string]{val: string(val)}}
 }
 
+// NewNormalBytes creates a new NormalValue that represents a `[]byte` value.
 func NewNormalBytes[T string | []byte](val T) NormalValue {
 	return normalBytes{baseNormalValue[[]byte]{val: []byte(val)}}
 }
 
+// NewNormalTime creates a new NormalValue that represents a `time.Time` value.
 func NewNormalTime(val time.Time) NormalValue {
 	return normalTime{baseNormalValue[time.Time]{val: val}}
 }
 
+// NewNormalDocument creates a new NormalValue that represents a `*Document` value.
 func NewNormalDocument(val *Document) NormalValue {
 	return normalDocument{baseNormalValue[*Document]{val: val}}
 }
 
+// NewNormalNillableBool creates a new NormalValue that represents a `immutable.Option[bool]` value.
 func NewNormalNillableBool(val immutable.Option[bool]) NormalValue {
 	return normalNillableBool{newBaseNillableNormalValue(val)}
 }
 
+// NewNormalNillableInt creates a new NormalValue that represents a `immutable.Option[int64]` value.
 func NewNormalNillableInt[T constraints.Integer | constraints.Float](val immutable.Option[T]) NormalValue {
 	return normalNillableInt{newBaseNillableNormalValue(normalizeNillableNum[int64](val))}
 }
 
+// NewNormalNillableFloat creates a new NormalValue that represents a `immutable.Option[float64]` value.
 func NewNormalNillableFloat[T constraints.Integer | constraints.Float](val immutable.Option[T]) NormalValue {
 	return normalNillableFloat{newBaseNillableNormalValue(normalizeNillableNum[float64](val))}
 }
 
+// NewNormalNillableString creates a new NormalValue that represents a `immutable.Option[string]` value.
 func NewNormalNillableString[T string | []byte](val immutable.Option[T]) NormalValue {
 	return normalNillableString{newBaseNillableNormalValue(normalizeNillableChars[string](val))}
 }
 
+// NewNormalNillableBytes creates a new NormalValue that represents a `immutable.Option[[]byte]` value.
 func NewNormalNillableBytes[T string | []byte](val immutable.Option[T]) NormalValue {
 	return normalNillableBytes{newBaseNillableNormalValue(normalizeNillableChars[[]byte](val))}
 }
 
+// NewNormalNillableTime creates a new NormalValue that represents a `immutable.Option[time.Time]` value.
 func NewNormalNillableTime(val immutable.Option[time.Time]) NormalValue {
 	return normalNillableTime{newBaseNillableNormalValue(val)}
 }
 
+// NewNormalNillableDocument creates a new NormalValue that represents a `immutable.Option[*Document]` value.
 func NewNormalNillableDocument(val immutable.Option[*Document]) NormalValue {
 	return normalNillableDocument{newBaseNillableNormalValue(val)}
 }
 
+// NewNormalBoolArray creates a new NormalValue that represents a `[]bool` value.
 func NewNormalBoolArray(val []bool) NormalValue {
 	return normalBoolArray{newBaseArrayNormalValue(val)}
 }
 
+// NewNormalIntArray creates a new NormalValue that represents a `[]int64` value.
 func NewNormalIntArray[T constraints.Integer | constraints.Float](val []T) NormalValue {
 	return normalIntArray{newBaseArrayNormalValue(normalizeNumArr[int64](val))}
 }
 
+// NewNormalFloatArray creates a new NormalValue that represents a `[]float64` value.
 func NewNormalFloatArray[T constraints.Integer | constraints.Float](val []T) NormalValue {
 	return normalFloatArray{newBaseArrayNormalValue(normalizeNumArr[float64](val))}
 }
 
+// NewNormalStringArray creates a new NormalValue that represents a `[]string` value.
 func NewNormalStringArray[T string | []byte](val []T) NormalValue {
 	return normalStringArray{newBaseArrayNormalValue(normalizeCharsArr[string](val))}
 }
 
+// NewNormalBytesArray creates a new NormalValue that represents a `[][]byte` value.
 func NewNormalBytesArray[T string | []byte](val []T) NormalValue {
 	return normalBytesArray{newBaseArrayNormalValue(normalizeCharsArr[[]byte](val))}
 }
 
+// NewNormalTimeArray creates a new NormalValue that represents a `[]time.Time` value.
 func NewNormalTimeArray(val []time.Time) NormalValue {
 	return normalTimeArray{newBaseArrayNormalValue(val)}
 }
 
+// NewNormalDocumentArray creates a new NormalValue that represents a `[]*Document` value.
 func NewNormalDocumentArray(val []*Document) NormalValue {
 	return normalDocumentArray{newBaseArrayNormalValue(val)}
 }
 
+// NewNormalNillableBoolArray creates a new NormalValue that represents a `immutable.Option[[]bool]` value.
 func NewNormalBoolNillableArray(val immutable.Option[[]bool]) NormalValue {
 	return normalBoolNillableArray{newBaseNillableArrayNormalValue(val)}
 }
 
+// NewNormalNillableIntArray creates a new NormalValue that represents a `immutable.Option[[]int64]` value.
 func NewNormalIntNillableArray[T constraints.Integer | constraints.Float](val immutable.Option[[]T]) NormalValue {
 	return normalIntNillableArray{newBaseNillableArrayNormalValue(normalizeNumNillableArr[int64](val))}
 }
 
+// NewNormalNillableFloatArray creates a new NormalValue that represents a `immutable.Option[[]float64]` value.
 func NewNormalFloatNillableArray[T constraints.Integer | constraints.Float](val immutable.Option[[]T]) NormalValue {
 	return normalFloatNillableArray{newBaseNillableArrayNormalValue(normalizeNumNillableArr[float64](val))}
 }
 
+// NewNormalNillableStringArray creates a new NormalValue that represents a `immutable.Option[[]string]` value.
 func NewNormalStringNillableArray[T string | []byte](val immutable.Option[[]T]) NormalValue {
 	return normalStringNillableArray{newBaseNillableArrayNormalValue(normalizeCharsNillableArr[string](val))}
 }
 
+// NewNormalNillableBytesArray creates a new NormalValue that represents a `immutable.Option[[][]byte]` value.
 func NewNormalBytesNillableArray[T string | []byte](val immutable.Option[[]T]) NormalValue {
 	return normalBytesNillableArray{newBaseNillableArrayNormalValue(normalizeCharsNillableArr[[]byte](val))}
 }
 
+// NewNormalNillableTimeArray creates a new NormalValue that represents a `immutable.Option[[]time.Time]` value.
 func NewNormalTimeNillableArray(val immutable.Option[[]time.Time]) NormalValue {
 	return normalTimeNillableArray{newBaseNillableArrayNormalValue(val)}
 }
 
+// NewNormalNillableDocumentArray creates a new NormalValue that represents a `immutable.Option[[]*Document]` value.
 func NewNormalDocumentNillableArray(val immutable.Option[[]*Document]) NormalValue {
 	return normalDocumentNillableArray{newBaseNillableArrayNormalValue(val)}
 }
 
+// NewNormalNillableBoolNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[bool]]` value.
 func NewNormalNillableBoolArray(val []immutable.Option[bool]) NormalValue {
 	return normalNillableBoolArray{newBaseArrayNormalValue(val)}
 }
 
+// NewNormalNillableIntArray creates a new NormalValue that represents a `[]immutable.Option[int64]` value.
 func NewNormalNillableIntArray[T constraints.Integer | constraints.Float](val []immutable.Option[T]) NormalValue {
 	return normalNillableIntArray{newBaseArrayNormalValue(normalizeNillableNumArr[int64](val))}
 }
 
+// NewNormalNillableFloatArray creates a new NormalValue that represents a `[]immutable.Option[float64]` value.
 func NewNormalNillableFloatArray[T constraints.Integer | constraints.Float](
 	val []immutable.Option[T],
 ) NormalValue {
 	return normalNillableFloatArray{newBaseArrayNormalValue(normalizeNillableNumArr[float64](val))}
 }
 
+// NewNormalNillableStringArray creates a new NormalValue that represents a `[]immutable.Option[string]` value.
 func NewNormalNillableStringArray[T string | []byte](val []immutable.Option[T]) NormalValue {
 	return normalNillableStringArray{newBaseArrayNormalValue(normalizeNillableCharsArr[string](val))}
 }
 
+// NewNormalNillableBytesArray creates a new NormalValue that represents a `[]immutable.Option[[]byte]` value.
 func NewNormalNillableBytesArray[T string | []byte](val []immutable.Option[T]) NormalValue {
 	return normalNillableBytesArray{newBaseArrayNormalValue(normalizeNillableCharsArr[[]byte](val))}
 }
 
+// NewNormalNillableTimeArray creates a new NormalValue that represents a `[]immutable.Option[time.Time]` value.
 func NewNormalNillableTimeArray(val []immutable.Option[time.Time]) NormalValue {
 	return normalNillableTimeArray{newBaseArrayNormalValue(val)}
 }
 
+// NewNormalNillableDocumentArray creates a new NormalValue that represents a `[]immutable.Option[*Document]` value.
 func NewNormalNillableDocumentArray(val []immutable.Option[*Document]) NormalValue {
 	return normalNillableDocumentArray{newBaseArrayNormalValue(val)}
 }
 
+// NewNormalNillableBoolNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[bool]]` value.
 func NewNormalNillableBoolNillableArray(val immutable.Option[[]immutable.Option[bool]]) NormalValue {
 	return normalNillableBoolNillableArray{newBaseNillableArrayNormalValue(val)}
 }
 
+// NewNormalNillableIntNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[int64]]` value.
 func NewNormalNillableIntNillableArray[T constraints.Integer | constraints.Float](
 	val immutable.Option[[]immutable.Option[T]],
 ) NormalValue {
@@ -1293,6 +1395,7 @@ func NewNormalNillableIntNillableArray[T constraints.Integer | constraints.Float
 	}
 }
 
+// NewNormalNillableFloatNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[float64]]` value.
 func NewNormalNillableFloatNillableArray[T constraints.Integer | constraints.Float](
 	val immutable.Option[[]immutable.Option[T]],
 ) NormalValue {
@@ -1301,22 +1404,26 @@ func NewNormalNillableFloatNillableArray[T constraints.Integer | constraints.Flo
 	}
 }
 
+// NewNormalNillableStringNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[string]]` value.
 func NewNormalNillableStringNillableArray[T string | []byte](val immutable.Option[[]immutable.Option[T]]) NormalValue {
 	return normalNillableStringNillableArray{
 		newBaseNillableArrayNormalValue(normalizeNillableCharsNillableArr[string](val)),
 	}
 }
 
+// NewNormalNillableBytesNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[[]byte]]` value.
 func NewNormalNillableBytesNillableArray[T string | []byte](val immutable.Option[[]immutable.Option[T]]) NormalValue {
 	return normalNillableBytesNillableArray{
 		newBaseNillableArrayNormalValue(normalizeNillableCharsNillableArr[[]byte](val)),
 	}
 }
 
+// NewNormalNillableTimeNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[time.Time]]` value.
 func NewNormalNillableTimeNillableArray(val immutable.Option[[]immutable.Option[time.Time]]) NormalValue {
 	return normalNillableTimeNillableArray{newBaseNillableArrayNormalValue(val)}
 }
 
+// NewNormalNillableDocumentNillableArray creates a new NormalValue that represents a `immutable.Option[[]immutable.Option[*Document]]` value.
 func NewNormalNillableDocumentNillableArray(val immutable.Option[[]immutable.Option[*Document]]) NormalValue {
 	return normalNillableDocumentNillableArray{newBaseNillableArrayNormalValue(val)}
 }
@@ -1425,6 +1532,8 @@ func normalizeNillableCharsNillableArr[R string | []byte, T string | []byte](
 	return immutable.None[[]immutable.Option[R]]()
 }
 
+// ToArrayOfNormalValues converts a NormalValue into a slice of NormalValue is the given value
+// is an array. If the given value is not an array, an error is returned.
 func ToArrayOfNormalValues(val NormalValue) ([]NormalValue, error) {
 	if !val.IsArray() {
 		return nil, NewCanNotTurnNormalValueIntoArray(val)
