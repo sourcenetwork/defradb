26905680b67684da3e675bc1f30fad3599f2dc59
Keenan Nemetz <keenan.nemetz@gmail.com>
2024-04-19 12:10:38 -0700

    simplify ScalarArrayKind IsNillable

--- client/schema_field_description.go
+++ client/schema_field_description.go
@@ -154,14 +154,7 @@ func (k ScalarArrayKind) Underlying() string {
 }
 
 func (k ScalarArrayKind) IsNillable() bool {
-	return k == FieldKind_BOOL_ARRAY ||
-		k == FieldKind_INT_ARRAY ||
-		k == FieldKind_FLOAT_ARRAY ||
-		k == FieldKind_STRING_ARRAY ||
-		k == FieldKind_NILLABLE_BOOL_ARRAY ||
-		k == FieldKind_NILLABLE_INT_ARRAY ||
-		k == FieldKind_NILLABLE_FLOAT_ARRAY ||
-		k == FieldKind_NILLABLE_STRING_ARRAY
+	return true
 }
 
 func (k ScalarArrayKind) IsObject() bool {
