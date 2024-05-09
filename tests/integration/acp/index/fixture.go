3fa87a7b8991b29cf0a5b4d10645bfa3b4ffef26
Islam Aleiv <islam@source.network>
2024-05-09 08:21:06 +0200

    Added comments to policies

--- tests/integration/acp/index/fixture.go
+++ tests/integration/acp/index/fixture.go
@@ -10,6 +10,7 @@
 
 package test_acp_index
 
+// policy id: "53980e762616fcffbe76307995895e862f87ef3f21d509325d1dc772a770b001"
 const userPolicy = `
 description: a test policy which marks a collection in a database as a resource
 
@@ -38,6 +39,7 @@ resources:
           - actor
 `
 
+// policy id: "68a4e64d5034b8a0565a90cd36483de0d61e0ea2450cf57c1fa8d27cbbf17c2c"
 const bookAuthorPolicy = `
 description: a test policy which marks a collection in a database as a resource
 
