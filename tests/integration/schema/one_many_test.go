175c547b752f2abc53675ad199edf8b94eb37e6d
Andrew Sisley <a.j.sisley@hotmail.com>
2024-05-14 12:04:30 -0400

    Add self reference sdl tests

--- tests/integration/schema/one_many_test.go
+++ tests/integration/schema/one_many_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/sourcenetwork/immutable"
 
 	"github.com/sourcenetwork/defradb/client"
+	"github.com/sourcenetwork/defradb/client/request"
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
 )
 
@@ -165,6 +166,125 @@ func TestSchemaOneMany_SelfReferenceManyFieldLexographicallyFirst(t *testing.T) 
 								RelationName: immutable.Some("user_user"),
 							},
 						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestSchemaOneMany_SelfUsingActualName(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				// Note: The @primary directive is required due to
+				// https://github.com/sourcenetwork/defradb/issues/2620
+				// it should be removed when that ticket is closed.
+				Schema: `
+					type User {
+						boss: User @primary
+						minions: [User]
+					}
+				`,
+			},
+			testUtils.GetCollections{
+				ExpectedResults: []client.CollectionDescription{
+					{
+						Name: immutable.Some("User"),
+						Fields: []client.CollectionFieldDescription{
+							{
+								Name: request.DocIDFieldName,
+							},
+							{
+								Name:         "boss",
+								ID:           1,
+								Kind:         immutable.Some[client.FieldKind](client.ObjectKind("User")),
+								RelationName: immutable.Some("user_user"),
+							},
+							{
+								Name:         "boss_id",
+								ID:           2,
+								Kind:         immutable.Some[client.FieldKind](client.FieldKind_DocID),
+								RelationName: immutable.Some("user_user"),
+							},
+							{
+								Name:         "minions",
+								ID:           3,
+								Kind:         immutable.Some[client.FieldKind](client.ObjectArrayKind("User")),
+								RelationName: immutable.Some("user_user"),
+							},
+						},
+					},
+				},
+			},
+			testUtils.GetSchema{
+				ExpectedResults: []client.SchemaDescription{
+					{
+						Name:      "User",
+						Root:      "bafkreiabowhib7ym6wumgywbc6f747e4u665ddxa5q43povrmjk4rmkzfi",
+						VersionID: "bafkreiabowhib7ym6wumgywbc6f747e4u665ddxa5q43povrmjk4rmkzfi",
+						Fields: []client.SchemaFieldDescription{
+							{
+								Name: request.DocIDFieldName,
+								Kind: client.FieldKind_DocID,
+							},
+							{
+								Name: "boss",
+								Kind: client.ObjectKind("User"),
+								Typ:  client.LWW_REGISTER,
+							},
+							{
+								Name: "boss_id",
+								Kind: client.FieldKind_DocID,
+								Typ:  client.LWW_REGISTER,
+							},
+						},
+					},
+				},
+			},
+			testUtils.IntrospectionRequest{
+				Request: `
+					query {
+						__type (name: "User") {
+							name
+							fields {
+								name
+								type {
+									name
+									kind
+								}
+							}
+						}
+					}
+				`,
+				ExpectedData: map[string]any{
+					"__type": map[string]any{
+						"name": "User",
+						"fields": append(DefaultFields,
+							Field{
+								"name": "boss",
+								"type": map[string]any{
+									"kind": "OBJECT",
+									"name": "User",
+								},
+							},
+							Field{
+								"name": "boss_id",
+								"type": map[string]any{
+									"kind": "SCALAR",
+									"name": "ID",
+								},
+							},
+							Field{
+								"name": "minions",
+								"type": map[string]any{
+									"kind": "LIST",
+									"name": nil,
+								},
+							},
+						).Tidy(),
 					},
 				},
 			},
