cbee9361d4107ffd46b0536833645e55a9a227dd
Keenan Nemetz <keenan.nemetz@gmail.com>
2024-06-12 11:29:50 -0700

    replace global package flags

--- cli/root.go
+++ cli/root.go
@@ -12,79 +12,7 @@ package cli
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 )
-
-// rootFlags is a set of persistent flags that are bound to config values.
-var rootFlags = pflag.NewFlagSet("root", pflag.ContinueOnError)
-
-func init() {
-	rootFlags.String(
-		"rootdir",
-		"",
-		"Directory for persistent data (default: $HOME/.defradb)",
-	)
-	rootFlags.String(
-		"log-level",
-		"info",
-		"Log level to use. Options are debug, info, error, fatal",
-	)
-	rootFlags.String(
-		"log-output",
-		"stderr",
-		"Log output path. Options are stderr or stdout.",
-	)
-	rootFlags.String(
-		"log-format",
-		"text",
-		"Log format to use. Options are text or json",
-	)
-	rootFlags.Bool(
-		"log-stacktrace",
-		false,
-		"Include stacktrace in error and fatal logs",
-	)
-	rootFlags.Bool(
-		"log-source",
-		false,
-		"Include source location in logs",
-	)
-	rootFlags.String(
-		"log-overrides",
-		"",
-		"Logger config overrides. Format <name>,<key>=<val>,...;<name>,...",
-	)
-	rootFlags.Bool(
-		"no-log-color",
-		false,
-		"Disable colored log output",
-	)
-	rootFlags.String(
-		"url",
-		"127.0.0.1:9181",
-		"URL of HTTP endpoint to listen on or connect to",
-	)
-	rootFlags.String(
-		"keyring-namespace",
-		"defradb",
-		"Service name to use when using the system backend",
-	)
-	rootFlags.String(
-		"keyring-backend",
-		"file",
-		"Keyring backend to use. Options are file or system",
-	)
-	rootFlags.String(
-		"keyring-path",
-		"keys",
-		"Path to store encrypted keys when using the file backend",
-	)
-	rootFlags.Bool(
-		"no-keyring",
-		false,
-		"Disable the keyring and generate ephemeral keys",
-	)
-}
 
 func MakeRootCommand() *cobra.Command {
 	var cmd = &cobra.Command{
@@ -102,8 +30,72 @@ Start a DefraDB node, interact with a local or remote node, and much more.
 			return setContextConfig(cmd)
 		},
 	}
-
-	cmd.PersistentFlags().AddFlagSet(rootFlags)
-
+	// set default flag values from config
+	cfg := defaultConfig()
+	cmd.PersistentFlags().String(
+		"rootdir",
+		"",
+		"Directory for persistent data (default: $HOME/.defradb)",
+	)
+	cmd.PersistentFlags().String(
+		"log-level",
+		cfg.GetString(configFlags["log-level"]),
+		"Log level to use. Options are debug, info, error, fatal",
+	)
+	cmd.PersistentFlags().String(
+		"log-output",
+		cfg.GetString(configFlags["log-output"]),
+		"Log output path. Options are stderr or stdout.",
+	)
+	cmd.PersistentFlags().String(
+		"log-format",
+		cfg.GetString(configFlags["log-format"]),
+		"Log format to use. Options are text or json",
+	)
+	cmd.PersistentFlags().Bool(
+		"log-stacktrace",
+		cfg.GetBool(configFlags["log-stacktrace"]),
+		"Include stacktrace in error and fatal logs",
+	)
+	cmd.PersistentFlags().Bool(
+		"log-source",
+		cfg.GetBool(configFlags["log-source"]),
+		"Include source location in logs",
+	)
+	cmd.PersistentFlags().String(
+		"log-overrides",
+		cfg.GetString(configFlags["log-overrides"]),
+		"Logger config overrides. Format <name>,<key>=<val>,...;<name>,...",
+	)
+	cmd.PersistentFlags().Bool(
+		"no-log-color",
+		cfg.GetBool(configFlags["no-log-color"]),
+		"Disable colored log output",
+	)
+	cmd.PersistentFlags().String(
+		"url",
+		cfg.GetString(configFlags["url"]),
+		"URL of HTTP endpoint to listen on or connect to",
+	)
+	cmd.PersistentFlags().String(
+		"keyring-namespace",
+		cfg.GetString(configFlags["keyring-namespace"]),
+		"Service name to use when using the system backend",
+	)
+	cmd.PersistentFlags().String(
+		"keyring-backend",
+		cfg.GetString(configFlags["keyring-backend"]),
+		"Keyring backend to use. Options are file or system",
+	)
+	cmd.PersistentFlags().String(
+		"keyring-path",
+		cfg.GetString(configFlags["keyring-path"]),
+		"Path to store encrypted keys when using the file backend",
+	)
+	cmd.PersistentFlags().Bool(
+		"no-keyring",
+		cfg.GetBool(configFlags["no-keyring"]),
+		"Disable the keyring and generate ephemeral keys",
+	)
 	return cmd
 }
