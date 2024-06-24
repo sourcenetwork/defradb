09b2c0df0980ceade2d2858cdd58cd6ae69b293b
Shahzad Lone <shahzadlone@gmail.com>
2024-06-24 12:29:03 -0400

    PR(TEST): Add a test for the cli command

---
+++ cli/identity_new_test.go
@@ -0,0 +1,26 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package cli
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewIdentityGeneration(t *testing.T) {
+	cmd := NewDefraCommand()
+
+	cmd.SetArgs([]string{"identity", "new"})
+
+	err := cmd.Execute()
+	require.NoError(t, err)
+}
