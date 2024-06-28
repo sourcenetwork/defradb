e5ff02c6a68b5e0172fc532f04f0d73710765c7c
AndrewSisley <a.j.sisley@hotmail.com>
2024-06-28 13:28:23 -0400

    fix: Allow primary field declarations on one-many (#2796)
    ## Relevant issue(s)
    
    Resolves #961
    
    ## Description
    
    Allows primary field declarations on one side of one-many.
    
    Declaring it as primary doesn't do anything, but there is no reason it
    shouldn't be permitted and it definitely shouldn't have errored the way
    it did.

---
+++ tests/integration/schema/one_many_test.go
@@ -0,0 +1,85 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package schema
+
+import (
+	"testing"
+
+	"github.com/sourcenetwork/immutable"
+
+	"github.com/sourcenetwork/defradb/client"
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestSchemaOneMany_Primary(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type User {
+						name: String
+						dogs: [Dog]
+					}
+					type Dog {
+						name: String
+						owner: User @primary
+					}
+				`,
+				ExpectedResults: []client.CollectionDescription{
+					{
+						Name: immutable.Some("User"),
+						Fields: []client.CollectionFieldDescription{
+							{
+								Name: "_docID",
+							},
+							{
+								Name:         "dogs",
+								ID:           1,
+								Kind:         immutable.Some[client.FieldKind](client.ObjectArrayKind("Dog")),
+								RelationName: immutable.Some("dog_user"),
+							},
+							{
+								Name: "name",
+								ID:   2,
+							},
+						},
+					},
+					{
+						Name: immutable.Some("Dog"),
+						Fields: []client.CollectionFieldDescription{
+							{
+								Name: "_docID",
+							},
+							{
+								Name: "name",
+								ID:   1,
+							},
+							{
+								Name:         "owner",
+								ID:           2,
+								Kind:         immutable.Some[client.FieldKind](client.ObjectKind("User")),
+								RelationName: immutable.Some("dog_user"),
+							},
+							{
+								Name:         "owner_id",
+								ID:           3,
+								Kind:         immutable.Some[client.FieldKind](client.ScalarKind(client.FieldKind_DocID)),
+								RelationName: immutable.Some("dog_user"),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
