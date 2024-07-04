8fdc7f4c0599e17e43c190a9551e0dbbdfa5470d
Andrew Sisley <a.j.sisley@hotmail.com>
2024-07-04 15:48:20 -0400

    Add tests documenting issue

--- tests/integration/utils2.go
+++ tests/integration/utils2.go
@@ -1300,6 +1300,7 @@ func createDocViaGQL(
 	node client.DB,
 	collections []client.Collection,
 ) ([]*client.Document, error) {
+	println(fmt.Sprint(collections))
 	collection := collections[action.CollectionID]
 	var input string
 
