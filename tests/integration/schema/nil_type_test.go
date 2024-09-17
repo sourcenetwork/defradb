b4773131c503228cfd152f9d97965c27ac923f4f
AndrewSisley <a.j.sisley@hotmail.com>
2024-09-17 14:36:56 -0400

    fix: Handle missing type in an SDL (#3023)
    ## Relevant issue(s)
    
    Resolves #3022
    
    ## Description
    
    Handles missing type in an SDL instead of panicing.

---
+++ tests/integration/schema/nil_type_test.go
@@ -0,0 +1,34 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package schema
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestSchema_WithMissingType_Errors(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type User {
+						name:
+					}
+				`,
+				ExpectedError: "field type not specified. Object: User, Field: name",
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
