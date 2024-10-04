045bbc7e1622e86683729a0754be0d03846973e0
Islam Aleiv <islam@source.network>
2024-10-04 10:36:19 +0200

    Fix lint

--- internal/utils/slice/slice_test.go
+++ internal/utils/slice/slice_test.go
@@ -1,11 +1,21 @@
+// Copyright 2024 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
 package slice_test
 
 import (
 	"testing"
 
-	"github.com/sourcenetwork/defradb/internal/utils/slice"
 	"github.com/sourcenetwork/immutable"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/sourcenetwork/defradb/internal/utils/slice"
 )
 
 func TestRemoveFirstIf(t *testing.T) {
