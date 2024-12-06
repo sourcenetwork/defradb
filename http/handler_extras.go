94ca9d18800e6fdaa742e8f1876fd6e68c8e8e4f
Chris Quigley <chris.quigley2@gmail.com>
2024-12-06 15:18:14 -0500

    Delint

--- http/handler_extras.go
+++ http/handler_extras.go
@@ -22,7 +22,6 @@ import (
 type extrasHandler struct{}
 
 func (s *extrasHandler) Purge(rw http.ResponseWriter, req *http.Request) {
-
 	db := mustGetContextClientDB(req)
 
 	// Send either 200 or 400 response based on whether the server is in dev mode
