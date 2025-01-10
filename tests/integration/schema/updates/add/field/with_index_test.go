519da774b9a672ba0134d8e88a77e642937119ed
Andrew Sisley <a.j.sisley@hotmail.com>
2025-01-10 16:30:09 -0500

    Expand test cases (second test fails)

--- tests/integration/schema/updates/add/field/with_index_test.go
+++ tests/integration/schema/updates/add/field/with_index_test.go
@@ -19,7 +19,7 @@ import (
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
 )
 
-func TestSchemaUpdatesAddFieldSimple_WithExistingIndex(t *testing.T) {
+func TestSchemaUpdatesAddFieldSimple_WithExistingIndexDocsCreatedAfterPatch(t *testing.T) {
 	test := testUtils.TestCase{
 		Description: "Test patching schema for collection with index still works",
 		Actions: []any{
@@ -88,6 +88,91 @@ func TestSchemaUpdatesAddFieldSimple_WithExistingIndex(t *testing.T) {
 					{
 						"name":	"John"
 					}`,
+			},
+			testUtils.Request{
+				Request: `query @explain(type: execute) {
+					Users(filter: {name: {_eq: "John"}}) {
+						name
+					}
+				}`,
+				Asserter: testUtils.NewExplainAsserter().WithFieldFetches(0).WithIndexFetches(1),
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestSchemaUpdatesAddFieldSimple_WithExistingIndexDocsCreatedBeforePatch(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "Test patching schema for collection with index still works",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users {
+						name: String @index
+					}
+				`,
+			},
+			// It is important to test this with docs created *before* the patch, as well as after (see other test).
+			// A bug was missed by missing this test case.
+			testUtils.CreateDoc{
+				CollectionID: 0,
+				Doc: `
+					{
+						"name":	"Shahzad"
+					}`,
+			},
+			testUtils.CreateDoc{
+				CollectionID: 0,
+				Doc: `
+					{
+						"name":	"John"
+					}`,
+			},
+			testUtils.SchemaPatch{
+				Patch: `
+					[
+						{ "op": "add", "path": "/Users/Fields/-", "value": {"Name": "email", "Kind": 11} }
+					]
+				`,
+			},
+			// It is important to test that the index shows up in both the `GetIndexes` call,
+			// *and* the `GetCollections` call, as indexes are stored in multiple places and we had a bug
+			// where patching a schema would result in the index disappearing from one of those locations.
+			testUtils.GetIndexes{
+				ExpectedIndexes: []client.IndexDescription{
+					{
+						Name:   "Users_name_ASC",
+						ID:     1,
+						Unique: false,
+						Fields: []client.IndexedFieldDescription{
+							{
+								Name: "name",
+							},
+						},
+					},
+				},
+			},
+			testUtils.GetCollections{
+				ExpectedResults: []client.CollectionDescription{
+					{
+						ID:             2,
+						Name:           immutable.Some("Users"),
+						IsMaterialized: true,
+						Indexes: []client.IndexDescription{
+							{
+								Name:   "Users_name_ASC",
+								ID:     1,
+								Unique: false,
+								Fields: []client.IndexedFieldDescription{
+									{
+										Name: "name",
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 			testUtils.Request{
 				Request: `query @explain(type: execute) {
