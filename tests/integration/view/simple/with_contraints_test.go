6ff63339ee25aecb59fc46839ad13748eb7cbe9f
Fred Carle <fredcarle@users.noreply.github.com>
2025-02-05 16:37:22 -0500

    document view bug

--- tests/integration/view/simple/with_contraints_test.go
+++ tests/integration/view/simple/with_contraints_test.go
@@ -13,18 +13,25 @@ package simple
 import (
 	"testing"
 
+	"github.com/sourcenetwork/immutable"
+
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
 )
 
-func TestView_SimpleWithSizeConstraint_DoesNotErrorOnSizeViolation(t *testing.T) {
+func TestView_SimpleWithSizeConstraint_CacheLessView_DoesNotErrorOnSizeViolation(t *testing.T) {
 	test := testUtils.TestCase{
+		SupportedViewTypes: immutable.Some([]testUtils.ViewType{
+			testUtils.CachelessViewType,
+		}),
 		Description: "Simple view with size constraint",
 		Actions: []any{
 			testUtils.SchemaUpdate{
 				Schema: `
 					type User {
 						name: String
-						pointsList: [Int!]
+						pointsListInt: [Int!]
+						pointsListFloat32: [Float32!]
+						pointsListFloat64: [Float64!]
 					}
 				`,
 			},
@@ -32,20 +39,26 @@ func TestView_SimpleWithSizeConstraint_DoesNotErrorOnSizeViolation(t *testing.T)
 				Query: `
 					User {
 						name
-						pointsList
+						pointsListInt
+						pointsListFloat32
+						pointsListFloat64
 					}
 				`,
 				SDL: `
 					type UserView @materialized(if: false) {
 						name: String
-						pointsList: [Int!] @constraints(size: 2)
+						pointsListInt: [Int!] @constraints(size: 2)
+						pointsListFloat32: [Float32!] @constraints(size: 2)
+						pointsListFloat64: [Float64!] @constraints(size: 2)
 					}
 				`,
 			},
 			testUtils.CreateDoc{
 				Doc: `{
 					"name":	"Alice",
-					"pointsList": [1, 2, 3]
+					"pointsListInt": [1, 2, 3],
+					"pointsListFloat32": [1, 2, 3],
+					"pointsListFloat64": [1, 2, 3]
 				}`,
 			},
 			testUtils.Request{
@@ -53,7 +66,9 @@ func TestView_SimpleWithSizeConstraint_DoesNotErrorOnSizeViolation(t *testing.T)
 					query {
 						UserView {
 							name
-							pointsList
+							pointsListInt
+							pointsListFloat32
+							pointsListFloat64
 						}
 					}
 				`,
@@ -62,7 +77,84 @@ func TestView_SimpleWithSizeConstraint_DoesNotErrorOnSizeViolation(t *testing.T)
 						{
 							"name": "Alice",
 							// notice the size constraint is not enforced on views
-							"pointsList": []int64{1, 2, 3},
+							"pointsListInt":     []int64{1, 2, 3},
+							"pointsListFloat32": []float32{1, 2, 3},
+							"pointsListFloat64": []float64{1, 2, 3},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+// This test documents a potential bug with the materialized view where the return type for arrays is
+// an interface with float64 values instead of the expected int64, float32 or float64 array types
+// TODO: https://github.com/sourcenetwork/defradb/issues/3428
+func TestView_SimpleWithSizeConstraint_MaterializedView_DoesNotErrorOnSizeViolation(t *testing.T) {
+	test := testUtils.TestCase{
+		SupportedViewTypes: immutable.Some([]testUtils.ViewType{
+			testUtils.MaterializedViewType,
+		}),
+		Description: "Simple view with size constraint",
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type User {
+						name: String
+						pointsListInt: [Int!]
+						pointsListFloat32: [Float32!]
+						pointsListFloat64: [Float64!]
+					}
+				`,
+			},
+			testUtils.CreateView{
+				Query: `
+					User {
+						name
+						pointsListInt
+						pointsListFloat32
+						pointsListFloat64
+					}
+				`,
+				SDL: `
+					type UserView @materialized(if: false) {
+						name: String
+						pointsListInt: [Int!] @constraints(size: 2)
+						pointsListFloat32: [Float32!] @constraints(size: 2)
+						pointsListFloat64: [Float64!] @constraints(size: 2)
+					}
+				`,
+			},
+			testUtils.CreateDoc{
+				Doc: `{
+					"name":	"Alice",
+					"pointsListInt": [1, 2, 3],
+					"pointsListFloat32": [1, 2, 3],
+					"pointsListFloat64": [1, 2, 3]
+				}`,
+			},
+			testUtils.Request{
+				Request: `
+					query {
+						UserView {
+							name
+							pointsListInt
+							pointsListFloat32
+							pointsListFloat64
+						}
+					}
+				`,
+				Results: map[string]any{
+					"UserView": []map[string]any{
+						{
+							"name": "Alice",
+							// notice the size constraint is not enforced on views
+							"pointsListInt":     []any{float64(1), float64(2), float64(3)},
+							"pointsListFloat32": []any{float64(1), float64(2), float64(3)},
+							"pointsListFloat64": []any{float64(1), float64(2), float64(3)},
 						},
 					},
 				},
