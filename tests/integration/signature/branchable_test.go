74fdf453e258d6e3689a592d394e2232a3b4637b
Islam Aleiv <aliev.islam@gmail.com>
2025-02-28 15:37:32 +0100

    Sign collection CRDTs

---
+++ tests/integration/signature/branchable_test.go
@@ -0,0 +1,84 @@
+// Copyright 2025 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package signature
+
+import (
+	"testing"
+
+	"github.com/onsi/gomega"
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestSignature_WithBranchableCollection_ShouldSignCollectionBlocks(t *testing.T) {
+	test := testUtils.TestCase{
+		EnabledBlockSigning: true,
+		Actions: []any{
+			testUtils.SchemaUpdate{
+				Schema: `
+					type Users @branchable {
+						name: String
+					}
+				`,
+			},
+			testUtils.CreateDoc{
+				DocMap: map[string]any{
+					"name": "John",
+				},
+			},
+			testUtils.Request{
+				Request: `query {
+						commits {
+							fieldName
+							fieldId
+							signature {
+								type
+								identity
+								value
+							}
+						}
+					}`,
+				Results: map[string]any{
+					"commits": []map[string]any{
+						{
+							"fieldName": nil,
+							"fieldId":   nil,
+							"signature": map[string]any{
+								"type":     "ECDSA",
+								"identity": gomega.Not(gomega.BeEmpty()),
+								"value":    gomega.Not(gomega.BeEmpty()),
+							},
+						},
+						{
+							"fieldName": "name",
+							"fieldId":   "1",
+							"signature": map[string]any{
+								"type":     "ECDSA",
+								"identity": gomega.Not(gomega.BeEmpty()),
+								"value":    gomega.Not(gomega.BeEmpty()),
+							},
+						},
+						{
+							"fieldName": nil,
+							"fieldId":   "C",
+							"signature": map[string]any{
+								"type":     "ECDSA",
+								"identity": gomega.Not(gomega.BeEmpty()),
+								"value":    gomega.Not(gomega.BeEmpty()),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
