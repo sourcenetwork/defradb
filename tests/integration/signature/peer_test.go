a7ae6691788fd87b15f1219287f84fce9d1cca05
Islam Aleiv <aliev.islam@gmail.com>
2025-03-20 21:41:08 +0100

    Add more tests

--- tests/integration/signature/peer_test.go
+++ tests/integration/signature/peer_test.go
@@ -228,3 +228,109 @@ func TestDocSignature_WithPeersAnDifferentKeyTypes_ShouldSync(t *testing.T) {
 
 	testUtils.ExecuteTestCase(t, test)
 }
+
+func TestDocSignature_WithPeersAnDifferentKeyTypesUpdatingSameDoc_ShouldSync(t *testing.T) {
+	test := testUtils.TestCase{
+		EnableSigning: true,
+		IdentityTypes: map[testUtils.Identity]crypto.KeyType{
+			testUtils.NodeIdentity(0).Value(): crypto.KeyTypeSecp256k1,
+			testUtils.NodeIdentity(1).Value(): crypto.KeyTypeEd25519,
+		},
+		Actions: []any{
+			testUtils.RandomNetworkingConfig(),
+			testUtils.RandomNetworkingConfig(),
+			testUtils.SchemaUpdate{
+				Schema: `
+					type User {
+						name: String
+						age: Int
+						verified: Boolean
+					}
+				`,
+			},
+			testUtils.ConnectPeers{
+				SourceNodeID: 1,
+				TargetNodeID: 0,
+			},
+			testUtils.SubscribeToCollection{
+				NodeID:        0,
+				CollectionIDs: []int{0},
+			},
+			testUtils.SubscribeToCollection{
+				NodeID:        1,
+				CollectionIDs: []int{0},
+			},
+			testUtils.CreateDoc{
+				NodeID: immutable.Some(0),
+				Doc: `{
+					"name":	"John",
+					"age":	21
+				}`,
+			},
+			testUtils.WaitForSync{},
+			testUtils.UpdateDoc{
+				NodeID: immutable.Some(1),
+				Doc: `{
+					"verified": true
+				}`,
+			},
+			testUtils.WaitForSync{},
+			testUtils.UpdateDoc{
+				NodeID: immutable.Some(0),
+				Doc: `{
+					"age": 23
+				}`,
+			},
+			testUtils.WaitForSync{},
+			// both nodes should have the same results
+			testUtils.Request{
+				Request: `query {
+					User {
+						name
+						age
+						verified
+					}
+				}`,
+				Results: map[string]any{
+					"User": []map[string]any{
+						{
+							"name":     "John",
+							"age":      int64(23),
+							"verified": true,
+						},
+					},
+				},
+			},
+			testUtils.Request{
+				Request: `query {
+						commits(fieldId: "C", order: {height: DESC}) {
+							signature {
+								type
+							}
+						}
+					}`,
+				Results: map[string]any{
+					"commits": []map[string]any{
+						{
+							"signature": map[string]any{
+								"type": coreblock.SignatureTypeECDSA256K,
+							},
+						},
+						{
+							"signature": map[string]any{
+								"type": coreblock.SignatureTypeEd25519,
+							},
+						},
+						{
+							"signature": map[string]any{
+								"type": coreblock.SignatureTypeECDSA256K,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
