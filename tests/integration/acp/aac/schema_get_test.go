89f114c2a9371d7e65dc10c27f9689b94f0a1efd
Shahzad Lone <shahzadlone@gmail.com>
2025-06-05 14:32:40 -0400

    PR(MAIN): Gate & Test - Get Schema + Get Schema By Version

---
+++ tests/integration/acp/aac/schema_get_test.go
@@ -0,0 +1,52 @@
+// Copyright 2025 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_acp_aac
+
+import (
+	"testing"
+
+	"github.com/sourcenetwork/defradb/client"
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestAAC_GatesSchemaGet_AllowIfAuthorizedElseError(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "admin acp correctly gates schema get operation, allow if authorized, otherwise error",
+		Actions: []any{
+			// Starting with ACC, so only authorized user(s) can perform operations from here on out.
+			testUtils.Close{},
+			testUtils.Start{
+				Identity:  testUtils.ClientIdentity(1),
+				EnableAAC: true,
+			},
+
+			// We haven't authorized non-identities. So, this should error.
+			testUtils.GetSchema{
+				Identity:      testUtils.NoIdentity(),
+				ExpectedError: "not authorized to perform operation",
+			},
+
+			// Wrong user/identity will also not be authorized.
+			testUtils.GetSchema{
+				Identity:      testUtils.ClientIdentity(2),
+				ExpectedError: "not authorized to perform operation",
+			},
+
+			// This should work as the identity is authorized.
+			testUtils.GetSchema{
+				Identity:        testUtils.ClientIdentity(1),
+				ExpectedResults: []client.SchemaDescription{},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
