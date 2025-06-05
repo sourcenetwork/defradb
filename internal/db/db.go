038bd2e0e037937bbac508fd84fea182d26abfa7
Shahzad Lone <shahzadlone@gmail.com>
2025-06-05 14:32:40 -0400

    PR(MAIN): Gate & Test - DAC Operations
    These operations were gated and tested:
    - AddDACPolicy
    - AddDACActorRelationship
    - DeleteDACActorRelationship

--- internal/db/db.go
+++ internal/db/db.go
@@ -229,6 +229,11 @@ func (db *DB) AddDACPolicy(
 	ctx, span := tracer.Start(ctx)
 	defer span.End()
 
+	err := db.checkAdminAccess(ctx, acpTypes.AdminDACPolicyAddPerm)
+	if err != nil {
+		return client.AddPolicyResult{}, err
+	}
+
 	if !db.documentACP.HasValue() {
 		return client.AddPolicyResult{}, client.ErrACPOperationButACPNotAvailable
 	}
@@ -307,6 +312,11 @@ func (db *DB) AddDACActorRelationship(
 	ctx, span := tracer.Start(ctx)
 	defer span.End()
 
+	err := db.checkAdminAccess(ctx, acpTypes.AdminDACRelationAddPerm)
+	if err != nil {
+		return client.AddActorRelationshipResult{}, err
+	}
+
 	if !db.documentACP.HasValue() {
 		return client.AddActorRelationshipResult{}, client.ErrACPOperationButACPNotAvailable
 	}
@@ -354,6 +364,11 @@ func (db *DB) DeleteDACActorRelationship(
 ) (client.DeleteActorRelationshipResult, error) {
 	ctx, span := tracer.Start(ctx)
 	defer span.End()
+
+	err := db.checkAdminAccess(ctx, acpTypes.AdminDACRelationDeletePerm)
+	if err != nil {
+		return client.DeleteActorRelationshipResult{}, err
+	}
 
 	if !db.documentACP.HasValue() {
 		return client.DeleteActorRelationshipResult{}, client.ErrACPOperationButACPNotAvailable
