8ca467e550c938c605e12dae7718338101fb8203
Chris Quigley <chris.quigley2@gmail.com>
2025-08-05 19:33:37 -0400

    Changed lens import

--- cbindings/logic/lens.go
+++ cbindings/logic/lens.go
@@ -16,8 +16,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/lens-vm/lens/host-go/config/model"
 	"github.com/sourcenetwork/immutable/enumerable"
+	"github.com/sourcenetwork/lens/host-go/config/model"
 
 	"github.com/sourcenetwork/defradb/client"
 )
