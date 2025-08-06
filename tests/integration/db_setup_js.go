fc6d10e629d3d5522be04ae377dd2ec7cad2d001
Chris Quigley <chris.quigley2@gmail.com>
2025-08-05 21:49:26 -0400

    Test

--- tests/integration/db_setup_js.go
+++ tests/integration/db_setup_js.go
@@ -26,6 +26,7 @@ func setupNode(
 	s *state.State,
 	identity immutable.Option[acpIdentity.Identity],
 	testCase TestCase,
+	enableNAC bool,
 	opts ...node.Option,
 ) (*state.NodeState, error) {
 	opts = append(defaultNodeOpts(), opts...)
@@ -67,7 +68,7 @@ func setupNode(
 	if err != nil {
 		return nil, err
 	}
-	c, err := setupClient(s, node)
+	c, err := setupClient(s, node, enableNAC)
 	if err != nil {
 		return nil, err
 	}
