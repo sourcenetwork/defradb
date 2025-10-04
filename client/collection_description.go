136e965ef6800ff68ecc7e5ed82a001bc0f5dde1
Andrew Sisley <a.j.sisley@hotmail.com>
2025-10-04 03:25:09 -0400

    PR FIXUP - Fix comment draws => draw

--- client/collection_description.go
+++ client/collection_description.go
@@ -57,7 +57,7 @@ type CollectionVersion struct {
 	// and it may not (yet) have its documents synced across the P2P network.
 	Query immutable.Option[QuerySource]
 
-	// VersionSources is the set of versions from which this collection may draws data from.
+	// VersionSources is the set of versions from which this collection may draw data from.
 	VersionSources []CollectionSource
 
 	// Fields contains the fields local to the node within this Collection.
