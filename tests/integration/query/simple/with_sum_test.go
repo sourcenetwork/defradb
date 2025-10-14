bdd3579b96ef04b741686dd78ca059b2764e2a08
Chris Quigley <chris.quigley2@gmail.com>
2025-10-14 15:23:00 -0400

    Replace test file

---
+++ tests/integration/query/simple/with_sum_test.go
@@ -0,0 +1,93 @@
+// Copyright 2022 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package simple
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestQuerySimpleWithSumOnUndefinedObject(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.Request{
+				Request: `query {
+					_sum
+				}`,
+				ExpectedError: "aggregate must be provided with a property to aggregate",
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestQuerySimpleWithSumOnUndefinedField(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.Request{
+				Request: `query {
+					_sum(Users: {})
+				}`,
+				ExpectedError: "Argument \"Users\" has invalid value {}.\nIn field \"field\": Expected \"UsersNumericFieldsArg!\", found null.",
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestQuerySimpleWithSumOnEmptyCollection(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.Request{
+				Request: `query {
+					_sum(Users: {field: Age})
+				}`,
+				Results: map[string]any{
+					"_sum": int64(0),
+				},
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
+
+func TestQuerySimpleWithSum(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			testUtils.CreateDoc{
+				Doc: `{
+					"Name": "John",
+					"Age": 21
+				}`,
+			},
+			testUtils.CreateDoc{
+				Doc: `{
+					"Name": "Bob",
+					"Age": 30
+				}`,
+			},
+			testUtils.Request{
+				Request: `query {
+					_sum(Users: {field: Age})
+				}`,
+				Results: map[string]any{
+					"_sum": int64(51),
+				},
+			},
+		},
+	}
+
+	executeTestCase(t, test)
+}
