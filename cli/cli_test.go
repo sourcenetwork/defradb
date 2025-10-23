349746e299e8ea497907e4bf9852b0cce94deb71
John-Alan Simmons <simmons.johnalan@gmail.com>
2025-10-22 18:46:59 -0700

    Added example registry test runner

---
+++ cli/cli_test.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/shlex"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCLIExamples(t *testing.T) {
+	ctx := context.Background()
+	registry := newExampleRegistry()
+	ctx = withExampleRegistry(ctx, registry)
+	cmd := NewDefraCommand(ctx)
+
+	for name, usage := range registry.examples {
+		t.Run(name, func(t *testing.T) {
+			args, err := shlex.Split(usage)
+			require.NoError(t, err)
+			err = validateCLIArgs(cmd, args[1:])
+			require.NoError(t, err, "%s: `%s`", name, usage)
+		})
+	}
+}
