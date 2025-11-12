d9580df40e378848542ff537dc752c807e12b463
Andrew Sisley <a.j.sisley@hotmail.com>
2025-11-12 15:19:37 -0500

    Add panicing test

---
+++ tests/integration/query/simple/with_version_order_test.go
@@ -0,0 +1,74 @@
+// Copyright 2025 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package simple
+
+import (
+	"testing"
+
+	"github.com/sourcenetwork/defradb/tests/action"
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestQuerySimpleWithVersionAndOrder(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddSchema{
+				Schema: `
+					type Users {
+						Name: String
+					}
+				`,
+			},
+			testUtils.CreateDoc{
+				DocMap: map[string]any{
+					"Name": "John",
+				},
+			},
+			testUtils.CreateDoc{
+				DocMap: map[string]any{
+					"Name": "Chris",
+				},
+			},
+			testUtils.Request{
+				Request: `query {
+					Users(order: {Name: ASC}) {
+						Name
+						_version {
+							fieldName
+						}
+					}
+				}`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{
+							"Name": "Chris",
+							"_version": []map[string]any{
+								{
+									"fieldName": "_C",
+								},
+							},
+						},
+						{
+							"Name": "John",
+							"_version": []map[string]any{
+								{
+									"fieldName": "_C",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
