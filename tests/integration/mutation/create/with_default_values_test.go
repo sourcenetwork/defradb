66f03f46f89951a4f1b671940f41ae470411d47b
Chris Quigley <chris.quigley2@gmail.com>
2025-12-05 15:51:40 -0500

    Moved and renamed an integration test

--- tests/integration/mutation/create/with_default_values_test.go
+++ tests/integration/mutation/create/with_default_values_test.go
@@ -513,38 +513,6 @@ func TestMutationCreate_WithDefaultJSONDeepObjectValue_ShouldBeSet(t *testing.T)
 	testUtils.ExecuteTestCase(t, test)
 }
 
-func TestMutationCreate_WithDefaultValues_SetsTwoEqualUTCNowDefaultValue(t *testing.T) {
-	timestampMatcher := testUtils.NewSameValue()
-	test := testUtils.TestCase{
-		Actions: []any{
-			&action.AddSchema{
-				Schema: `
-					type User {
-						name: String
-						created: DateTime
-					}
-				`,
-			},
-			testUtils.Request{
-				Request: `mutation {
-					bob: create_User(input: { name: "Bob", created: UTC_NOW }) {
-						created
-					}
-
-					alice: create_User(input: { name: "Alice", created: UTC_NOW }) {
-						created
-					}
-                }`,
-				Results: map[string]any{
-					"bob":   []map[string]any{{"created": timestampMatcher}},
-					"alice": []map[string]any{{"created": timestampMatcher}},
-				},
-			},
-		},
-	}
-
-	testUtils.ExecuteTestCase(t, test)
-}
 func TestMutationCreate_WithDefaultValues_NoValuesProvided_SetsTwoEqualUTCNowDefaultValue(t *testing.T) {
 	timestampMatcher := testUtils.NewSameValue()
 
