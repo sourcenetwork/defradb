265a4dddcd71ce17948758d281315d447f357353
Chris Quigley <chris.quigley2@gmail.com>
2025-12-22 14:09:05 -0500

    Rollback strange change to cbindings?

--- cbindings/block.go
+++ cbindings/block.go
@@ -27,14 +27,13 @@ func BlockVerifySignature(nodePtr C.uintptr_t,
 	keyType *C.char,
 	publicKey *C.char,
 	cid *C.char,
-	options C.CollectionOptions,
+	identityPtr C.uintptr_t,
 ) C.Result {
 	ctx := context.Background()
-	ctx, err := contextWithIdentity(ctx, options.identityPtr)
+	ctx, err := contextWithIdentity(ctx, identityPtr)
 	if err != nil {
 		return returnC(returnGoC(1, err.Error(), ""))
 	}
-
 	keyTypeStr := C.GoString(keyType)
 	pubKeyStr := C.GoString(publicKey)
 	cryptoKeyType := crypto.KeyTypeSecp256k1
