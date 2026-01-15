4483394f6ec8f68d4fb31166414be62f088c5ca8
Islam Aleiv <aliev.islam@gmail.com>
2026-01-15 12:10:03 +0100

    Format

--- tests/integration/index/query_with_relation_sub_order_test.go
+++ tests/integration/index/query_with_relation_sub_order_test.go
@@ -506,7 +506,7 @@ func TestQueryWithOrderOnOneToMany_WithSubFilterAndOrderAndRelationIndex_ShouldF
 							},
 						},
 						{
-							"name": "Fred",
+							"name":      "Fred",
 							"published": []map[string]any{},
 						},
 					},
@@ -606,7 +606,7 @@ func TestQueryWithOrderOnOneToMany_WithParentFilterOnRelationAndSubOrder_ShouldO
 			testUtils.Request{
 				Request: makeExplainQuery(req),
 				// 5 indexFetch: parent filter uses rating index via inverted join (1 book matches _gte: 4.0)
-				// For the matched author full index scan is done to get all 4 books 
+				// For the matched author full index scan is done to get all 4 books
 				Asserter: testUtils.NewExplainAsserter().WithIndexFetches(5),
 			},
 		},
