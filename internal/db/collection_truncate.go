f5c8da10715885bd8ceb65d8d190c7a76cc28e78
Andrew Sisley <a.j.sisley@hotmail.com>
2026-01-15 14:53:48 -0500

    PR FIXUP - Document heastore deletion

--- internal/db/collection_truncate.go
+++ internal/db/collection_truncate.go
@@ -104,6 +104,11 @@ func (c *collection) truncate(
 	return nil
 }
 
+// hardDeleteDocKeysAndHeadstore iterates through the `keys.DataStoreKey` for this collection
+// and deletes both them, *and* the headstore keys for those found documents.
+//
+// The headstore keys must be discovered based on datastore keys, as the headstore keys are not
+// indexed by collection id, and so cannot be found independently.
 func (c *collection) hardDeleteDocKeysAndHeadstore(
 	ctx context.Context,
 	colShortID uint32,
