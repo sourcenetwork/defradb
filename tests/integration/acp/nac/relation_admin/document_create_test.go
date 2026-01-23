ba02353bc757d1638b6cc6ed1eadadf0c665b8a0
Chris Quigley <chris.quigley2@gmail.com>
2026-01-23 14:40:46 -0500

    Checking in work

--- tests/integration/acp/nac/relation_admin/document_create_test.go
+++ tests/integration/acp/nac/relation_admin/document_create_test.go
@@ -13,12 +13,25 @@ package test_acp_nac_relation_admin
 import (
 	"testing"
 
+	acpTypes "github.com/sourcenetwork/defradb/acp/types"
 	"github.com/sourcenetwork/defradb/tests/action"
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+	"github.com/sourcenetwork/defradb/tests/state"
+	"github.com/sourcenetwork/immutable"
 )
 
 func TestNAC_AdminRelation_CanDocumentCreate(t *testing.T) {
 	test := testUtils.TestCase{
+		// todo: Investigate and test this behavior across all client types when implementing granular NAC permissions.
+		// See: https://github.com/sourcenetwork/defradb/issues/4383
+		SupportedClientTypes: immutable.Some(
+			[]state.ClientType{
+				state.GoClientType,
+				state.HTTPClientType,
+				state.CClientType,
+				state.JSClientType,
+			},
+		),
 		Actions: []any{
 			// Starting with NAC, so only authorized user(s) can perform operations from here on out.
 			testUtils.Close{},
@@ -38,7 +51,58 @@ func TestNAC_AdminRelation_CanDocumentCreate(t *testing.T) {
 				Identity:      testUtils.ClientIdentity(2),
 				CollectionID:  0,
 				Doc:           `{ "name": "Shahzad" }`,
-				ExpectedError: "not authorized to perform operation",
+				ExpectedError: testUtils.FormatExpectedErrorWithPermission(acpTypes.NodeDocumentUpdatePerm.String()),
+			},
+
+			// Grant access to user.
+			testUtils.AddNACActorRelationship{
+				RequestorIdentity: testUtils.ClientIdentity(1),
+				TargetIdentity:    testUtils.ClientIdentity(2), // Grant this user "admin" relation
+				Relation:          "admin",
+				ExpectedExistence: false,
+			},
+
+			// This user, can now perform this gated operation.
+			testUtils.CreateDoc{
+				Identity:     testUtils.ClientIdentity(2),
+				CollectionID: 0,
+				Doc:          `{ "name": "Shahzad" }`,
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestNAC_AdminRelation_CLIClient_CanDocumentCreate(t *testing.T) {
+	test := testUtils.TestCase{
+		// todo: Investigate and test this behavior across all client types when implementing granular NAC permissions.
+		// See: https://github.com/sourcenetwork/defradb/issues/4383
+		SupportedClientTypes: immutable.Some(
+			[]state.ClientType{
+				state.CLIClientType,
+			},
+		),
+		Actions: []any{
+			// Starting with NAC, so only authorized user(s) can perform operations from here on out.
+			testUtils.Close{},
+			testUtils.Start{
+				Identity:  testUtils.ClientIdentity(1),
+				EnableNAC: true,
+			},
+			// Note: Doing setup steps after starting with nac enabled, otherwise the in-memory tests
+			// will lose setup state when the restart happens (i.e. the restart that started nac).
+			&action.AddSchema{
+				Identity: testUtils.ClientIdentity(1),
+				Schema:   `type User { name: String }`,
+			},
+
+			// This user, can not perform this gated operation yet.
+			testUtils.CreateDoc{
+				Identity:      testUtils.ClientIdentity(2),
+				CollectionID:  0,
+				Doc:           `{ "name": "Shahzad" }`,
+				ExpectedError: testUtils.FormatExpectedErrorWithPermission(acpTypes.NodeCollectionGetPerm.String()),
 			},
 
 			// Grant access to user.
