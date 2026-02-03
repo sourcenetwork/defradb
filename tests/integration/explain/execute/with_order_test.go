f40e5c946a650aea13becb03da4480b5d932f7fc
Islam Aleiv <aliev.islam@gmail.com>
2026-02-03 14:54:00 +0100

    Explain for non-indexes

--- tests/integration/explain/execute/with_order_test.go
+++ tests/integration/explain/execute/with_order_test.go
@@ -309,3 +309,227 @@ func TestExecuteExplainRequestWhereParentFieldIsOrderedByChildField(t *testing.T
 
 	explainUtils.ExecuteTestCase(t, test)
 }
+
+func TestExecuteExplainRequestWithSubqueryOrderByNestedRelationField(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Actions: []any{
+			&action.AddSchema{
+				Schema: `
+					type Author {
+						name: String
+						published: [Book]
+					}
+					type Book {
+						title: String
+						author: Author
+						publisher: Publisher
+					}
+					type Publisher {
+						name: String
+						establishedYear: Int
+						book: Book @primary
+					}
+				`,
+			},
+
+			&action.CreateDoc{
+				CollectionID: 0,
+				Doc:          `{"name": "John"}`,
+			},
+
+			&action.CreateDoc{
+				CollectionID: 1,
+				DocMap: map[string]any{
+					"title":  "Book2020",
+					"author": testUtils.NewDocIndex(0, 0),
+				},
+			},
+
+			&action.CreateDoc{
+				CollectionID: 1,
+				DocMap: map[string]any{
+					"title":  "Book2010",
+					"author": testUtils.NewDocIndex(0, 0),
+				},
+			},
+
+			&action.CreateDoc{
+				CollectionID: 2,
+				DocMap: map[string]any{
+					"name":            "Publisher2020",
+					"establishedYear": 2020,
+					"book":            testUtils.NewDocIndex(1, 0),
+				},
+			},
+
+			&action.CreateDoc{
+				CollectionID: 2,
+				DocMap: map[string]any{
+					"name":            "Publisher2010",
+					"establishedYear": 2010,
+					"book":            testUtils.NewDocIndex(1, 1),
+				},
+			},
+
+			&action.ExplainRequest{
+				Request: `query @explain(type: execute) {
+					Author {
+						name
+						published(order: {publisher: {establishedYear: DESC}}, limit: 2) {
+							title
+						}
+					}
+				}`,
+
+				ExpectedFullGraph: dataMap{
+					"explain": dataMap{
+						"executionSuccess": true,
+						"sizeOfResult":     1,
+						"planExecutions":   uint64(2),
+						"operationNode": []dataMap{
+							{
+								"selectTopNode": dataMap{
+									"selectNode": dataMap{
+										"iterations":    uint64(2),
+										"filterMatches": uint64(1),
+										"typeIndexJoin": dataMap{
+											"iterations": uint64(2),
+											// Author scanNode
+											"scanNode": dataMap{
+												"iterations":   uint64(2),
+												"docFetches":   uint64(1),
+												"fieldFetches": uint64(1),
+												"indexFetches": uint64(0),
+											},
+											// Book scanNode
+											"subTypeScanNode": dataMap{
+												"iterations":   uint64(3),
+												"docFetches":   uint64(2),
+												"fieldFetches": uint64(4),
+												"indexFetches": uint64(0),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
+
+func TestExecuteExplainRequestWithSubqueryOrderByNestedRelationFieldASC(t *testing.T) {
+	test := testUtils.TestCase{
+
+		Actions: []any{
+			&action.AddSchema{
+				Schema: `
+					type Author {
+						name: String
+						published: [Book]
+					}
+					type Book {
+						title: String
+						author: Author
+						publisher: Publisher
+					}
+					type Publisher {
+						name: String
+						establishedYear: Int
+						book: Book @primary
+					}
+				`,
+			},
+
+			&action.CreateDoc{
+				CollectionID: 0,
+				Doc:          `{"name": "John"}`,
+			},
+
+			&action.CreateDoc{
+				CollectionID: 1,
+				DocMap: map[string]any{
+					"title":  "Book2020",
+					"author": testUtils.NewDocIndex(0, 0),
+				},
+			},
+
+			&action.CreateDoc{
+				CollectionID: 1,
+				DocMap: map[string]any{
+					"title":  "Book2010",
+					"author": testUtils.NewDocIndex(0, 0),
+				},
+			},
+
+			&action.CreateDoc{
+				CollectionID: 2,
+				DocMap: map[string]any{
+					"name":            "Publisher2020",
+					"establishedYear": 2020,
+					"book":            testUtils.NewDocIndex(1, 0),
+				},
+			},
+
+			&action.CreateDoc{
+				CollectionID: 2,
+				DocMap: map[string]any{
+					"name":            "Publisher2010",
+					"establishedYear": 2010,
+					"book":            testUtils.NewDocIndex(1, 1),
+				},
+			},
+
+			&action.ExplainRequest{
+				Request: `query @explain(type: execute) {
+					Author {
+						name
+						published(order: {publisher: {establishedYear: ASC}}, limit: 2) {
+							title
+						}
+					}
+				}`,
+
+				ExpectedFullGraph: dataMap{
+					"explain": dataMap{
+						"executionSuccess": true,
+						"sizeOfResult":     1,
+						"planExecutions":   uint64(2),
+						"operationNode": []dataMap{
+							{
+								"selectTopNode": dataMap{
+									"selectNode": dataMap{
+										"iterations":    uint64(2),
+										"filterMatches": uint64(1),
+										"typeIndexJoin": dataMap{
+											"iterations": uint64(2),
+											"scanNode": dataMap{
+												"iterations":   uint64(2),
+												"docFetches":   uint64(1),
+												"fieldFetches": uint64(1),
+												"indexFetches": uint64(0),
+											},
+											"subTypeScanNode": dataMap{
+												"iterations":   uint64(3),
+												"docFetches":   uint64(2),
+												"fieldFetches": uint64(4),
+												"indexFetches": uint64(0),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	explainUtils.ExecuteTestCase(t, test)
+}
