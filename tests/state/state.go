5f07cd97298812c276f9077327a5145117de4cdc
Shahzad Lone <shahzadlone@gmail.com>
2026-02-06 14:09:59 +0500

    PR(RENAME): Rename to distinguish better

--- tests/state/state.go
+++ tests/state/state.go
@@ -40,8 +40,8 @@ type StatefulMatcher interface {
 type TestState interface {
 	// GetClientType returns the client type of the test.
 	GetClientType() ClientType
-	// GetCurrentNodeID returns the node id that is currently being asserted.
-	GetCurrentNodeID() int
+	// GetCurrentAssertingNodeID returns the node id that is currently being asserted.
+	GetCurrentAssertingNodeID() int
 	// GetIdentity returns the identity for the given node index.
 	GetIdentity(Identity) acpIdentity.Identity
 	// GetDocID returns the document ID for the given collection index and document index.
@@ -315,7 +315,7 @@ type State struct {
 	StatefulMatchers []StatefulMatcher
 
 	// CurrentSetupNodeID is used during setup stage to find specific attributes that are unique to a
-	// node's unique ID, for example finding a specific node's NodeIdentity inorder to bypass NAC.
+	// node, for example finding a specific node's NodeIdentity inorder to bypass NAC.
 	CurrentSetupNodeID int
 
 	// node id that is currently being asserted. This is used by [StatefulMatcher]s to know for which
@@ -323,7 +323,7 @@ type State struct {
 	// called with a value that it didn't see before, but the value should be the same for different
 	// nodes, e.g. within the same node Cids should be unique, but across different nodes the same block
 	// should have the same Cid.
-	CurrentNodeID int
+	CurrentAssertingNodeID int
 
 	// LenIDs of lenses added to Defra.
 	LensIDs []string
@@ -333,8 +333,8 @@ func (s *State) GetClientType() ClientType {
 	return s.ClientType
 }
 
-func (s *State) GetCurrentNodeID() int {
-	return s.CurrentNodeID
+func (s *State) GetCurrentAssertingNodeID() int {
+	return s.CurrentAssertingNodeID
 }
 
 func (s *State) GetIdentity(ident Identity) acpIdentity.Identity {
