e66f967c40767530d2105b912f997556d0917c44
Chris Quigley <chris.quigley2@gmail.com>
2026-03-20 01:25:26 -0400

    Initial fix

--- internal/request/graphql/parser.go
+++ internal/request/graphql/parser.go
@@ -183,6 +183,16 @@ func (p *parser) SetSchema(ctx context.Context, collections []client.CollectionV
 	return err
 }
 
-func (p *parser) NewFilterFromString(collectionType string, body string) (immutable.Option[request.Filter], error) {
+func (p *parser) NewFilterFromString(ctx context.Context, collectionType string, body string) (immutable.Option[request.Filter], error) {
+	// If the txn ID is in the schema manager map, use that one instead of the default one
+	gotTxn, hadTxn := datastore.CtxTryGetTxn(ctx)
+	if hadTxn {
+		p.schemaManagerMapLock.RLock()
+		gotSchemaManager, ok := p.schemaManagerMap[gotTxn.ID()]
+		p.schemaManagerMapLock.RUnlock()
+		if ok {
+			return defrap.NewFilterFromString(*gotSchemaManager.Schema(), collectionType, body)
+		}
+	}
 	return defrap.NewFilterFromString(*p.schemaManager.Schema(), collectionType, body)
 }
