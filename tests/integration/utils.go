3f7a74caafc14ddce97c334432562fe1a9a1b909
Andrew Sisley <a.j.sisley@hotmail.com>
2026-04-24 14:29:55 -0400

    Update change detector to use new action

--- tests/integration/utils.go
+++ tests/integration/utils.go
@@ -788,7 +788,7 @@ ActionLoop:
 			}
 			continue
 
-		case UpdateDoc:
+		case *action.UpdateDoc:
 			if concreteAction.TransactionID.HasValue() {
 				transactionIDset[concreteAction.TransactionID.Value()] = struct{}{}
 			}
