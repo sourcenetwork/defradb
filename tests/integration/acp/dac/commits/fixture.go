db942dd83fb298c1bfde2c2dfc86a2e815f7a686
Shahzad Lone <shahzadlone@gmail.com>
2026-05-10 17:23:11 -0400

    PR(TEST): Add DAC tests for _commits queries
    Added tests under tests/integration/acp/dac/commits/ folder
    for verifying that _commits queries respect DAC or not.
    
    Tests covering passing-by-default scenarios (public docs, owner-identity
    private docs) are enabled.
    
    Also added cases that fail atm (any private-doc read by a non-owner identity),
    but kept them commented, will uncomment them once they start passing.

---
+++ tests/integration/acp/dac/commits/fixture.go
@@ -0,0 +1,43 @@
+// Copyright 2026 Democratized Data Foundation
+//
+// This file is part of the DefraDB test suite.
+//
+// The DefraDB test suite is licensed under either:
+//
+//   (1) GNU Affero General Public License v3
+//   (2) Business Source License 1.1
+//
+// See tests/LICENSE for details.
+
+package test_acp_dac_commits
+
+const usersPolicy = `
+description: a test policy
+name: test
+resources:
+- name: users
+  permissions:
+  - name: delete
+  - name: read
+    expr: reader
+  - name: update
+  relations:
+  - manages:
+    - reader
+    name: admin
+    types:
+    - actor
+  - name: reader
+    types:
+    - actor
+`
+
+const userDoc = `
+{
+	"name": "Shahzad",
+	"age": 28
+}
+`
+
+// userDocID is the docID for the userDoc.
+const userDocID = "bae-cad49a1d-299c-5c34-9dab-a23f233f1a2f"
