e3a5075a6e26b68a85408f673815ba8ac0163b69
Chris Quigley <chris.quigley2@gmail.com>
2026-06-10 08:32:23 -0400

    Adjusuting and adding tests

--- tests/integration/query/inline_array/with_nonnillable_scalars_test.go
+++ tests/integration/query/inline_array/with_nonnillable_scalars_test.go
@@ -107,7 +107,7 @@ func TestQueryInlineArrayWithNonNillableInts_NullElement(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"scores": [1, null, 3]}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field`,
 			},
 		},
 	}
@@ -166,7 +166,7 @@ func TestQueryInlineArrayWithNonNillableFloat64s_NullElement(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"ratings": [1.1, null, 3.3]}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field`,
 			},
 		},
 	}
@@ -225,7 +225,7 @@ func TestQueryInlineArrayWithNonNillableFloat32s_NullElement(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"weights": [1.1, null, 3.3]}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field`,
 			},
 		},
 	}
@@ -284,7 +284,7 @@ func TestQueryInlineArrayWithNonNillableStrings_NullElement(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"tags": ["one", null, "three"]}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field`,
 			},
 		},
 	}
@@ -321,7 +321,7 @@ func TestQueryNonNillableDateTime_Null(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"name": "John", "createdAt": null}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field. Name: createdAt`,
 			},
 		},
 	}
@@ -358,7 +358,7 @@ func TestQueryNonNillableBlob_Null(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"name": "John", "avatar": null}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field. Name: avatar`,
 			},
 		},
 	}
@@ -395,7 +395,7 @@ func TestQueryNonNillableJSON_Null(t *testing.T) {
 			},
 			&action.AddDoc{
 				Doc:           `{"name": "John", "metadata": null}`,
-				ExpectedError: `null`,
+				ExpectedError: `null value provided for non-nillable field. Name: metadata`,
 			},
 		},
 	}
@@ -416,6 +416,182 @@ func TestQueryNonNillableJSON_NotEmpty(t *testing.T) {
 				Results: map[string]any{
 					"Users": []map[string]any{
 						{"name": "John", "metadata": map[string]any{"role": "admin"}},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryInlineArrayWithNonNillableBooleans_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { flags: [Boolean!] }`,
+			},
+			&action.AddDoc{
+				Doc: `{}`,
+			},
+			&action.Request{
+				Request: `query { Users { flags } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"flags": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryInlineArrayWithNonNillableInts_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { scores: [Int!] }`,
+			},
+			&action.AddDoc{
+				Doc: `{}`,
+			},
+			&action.Request{
+				Request: `query { Users { scores } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"scores": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryInlineArrayWithNonNillableFloat64s_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { ratings: [Float64!] }`,
+			},
+			&action.AddDoc{
+				Doc: `{}`,
+			},
+			&action.Request{
+				Request: `query { Users { ratings } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"ratings": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryInlineArrayWithNonNillableFloat32s_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { weights: [Float32!] }`,
+			},
+			&action.AddDoc{
+				Doc: `{}`,
+			},
+			&action.Request{
+				Request: `query { Users { weights } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"weights": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryInlineArrayWithNonNillableStrings_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { tags: [String!] }`,
+			},
+			&action.AddDoc{
+				Doc: `{}`,
+			},
+			&action.Request{
+				Request: `query { Users { tags } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"tags": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryNonNillableDateTime_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { name: String, createdAt: DateTime! }`,
+			},
+			&action.AddDoc{
+				Doc: `{"name": "John"}`,
+			},
+			&action.Request{
+				Request: `query { Users { name, createdAt } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"name": "John", "createdAt": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryNonNillableBlob_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { name: String, avatar: Blob! }`,
+			},
+			&action.AddDoc{
+				Doc: `{"name": "John"}`,
+			},
+			&action.Request{
+				Request: `query { Users { name, avatar } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"name": "John", "avatar": nil},
+					},
+				},
+			},
+		},
+	}
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestQueryNonNillableJSON_Omitted(t *testing.T) {
+	test := testUtils.TestCase{
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `type Users { name: String, metadata: JSON! }`,
+			},
+			&action.AddDoc{
+				Doc: `{"name": "John"}`,
+			},
+			&action.Request{
+				Request: `query { Users { name, metadata } }`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{"name": "John", "metadata": nil},
 					},
 				},
 			},
