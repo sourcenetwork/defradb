7e2bc2d96a8048e60bc4e49198d8c883c87ac140
Chris Quigley <chris.quigley2@gmail.com>
2026-06-23 20:29:26 -0400

    Bugfixes

--- http/errors.go
+++ http/errors.go
@@ -148,6 +148,7 @@ func httpStatusFromError(err error) int {
 	// 404 Not Found
 	if errors.Is(err, client.ErrDocumentNotFoundOrNotAuthorized) ||
 		errors.Is(err, client.ErrCollectionNotFound) ||
+		errors.Is(err, client.ErrTransactionNotFound) ||
 		errors.Is(err, db.ErrDocIDNotFound) ||
 		errors.Is(err, db.ErrIndexWithNameDoesNotExists) ||
 		errors.Is(err, db.ErrEncryptedIndexDoesNotExist) ||
