42bfd69ca989f2e0d452c9c9b5adc43813ab2f98
John-Alan Simmons <simmons.johnalan@gmail.com>
2026-06-24 14:48:21 -0700

    test: expand HTTP transaction TTL coverage

--- tests/integration/txn/ttl_test.go
+++ tests/integration/txn/ttl_test.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/sourcenetwork/immutable"
 
+	"github.com/sourcenetwork/defradb/client"
 	"github.com/sourcenetwork/defradb/client/options"
 	"github.com/sourcenetwork/defradb/tests/action"
 	testUtils "github.com/sourcenetwork/defradb/tests/integration"
@@ -28,10 +29,6 @@ func TestTxnTTL_GivenIdleHTTPTransaction_DiscardsTransaction(t *testing.T) {
 		SupportedClientTypes: immutable.Some([]state.ClientType{
 			state.HTTPClientType,
 			state.CLIClientType,
-		}),
-		SupportedDatabaseTypes: immutable.Some([]state.DatabaseType{
-			testUtils.BadgerIMType,
-			testUtils.DefraIMType,
 		}),
 		HTTP: immutable.Some(options.NodeHTTPOptions{
 			TxnTTL:        120 * time.Millisecond,
@@ -101,6 +98,338 @@ func TestTxnTTL_GivenIdleHTTPTransaction_DiscardsTransaction(t *testing.T) {
 						},
 					},
 				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestTxnTTL_GivenHTTPTransactionActivity_ExtendsTTL(t *testing.T) {
+	test := testUtils.TestCase{
+		SupportedClientTypes: immutable.Some([]state.ClientType{
+			state.HTTPClientType,
+			state.CLIClientType,
+		}),
+		HTTP: immutable.Some(options.NodeHTTPOptions{
+			TxnTTL:        500 * time.Millisecond,
+			TxnTTLTick:    50 * time.Millisecond,
+			TxnTTLBuckets: 20,
+		}),
+		SkipChangeDetector: true,
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `
+					type Users {
+						name: String
+						age: Int
+					}
+				`,
+			},
+			&action.AddDoc{
+				Doc: `{
+					"name": "John",
+					"age": 27
+				}`,
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `mutation {
+					update_Users(filter: {name: {_eq: "John"}}, input: {age: 28}) {
+						name
+						age
+					}
+				}`,
+				Results: map[string]any{
+					"update_Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(28),
+						},
+					},
+				},
+			},
+			&action.Wait{
+				Duration: immutable.Some(300 * time.Millisecond),
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `mutation {
+					update_Users(filter: {name: {_eq: "John"}}, input: {age: 29}) {
+						name
+						age
+					}
+				}`,
+				Results: map[string]any{
+					"update_Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(29),
+						},
+					},
+				},
+			},
+			&action.Wait{
+				Duration: immutable.Some(300 * time.Millisecond),
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `
+					query {
+						Users {
+							name
+							age
+						}
+					}
+				`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(29),
+						},
+					},
+				},
+			},
+			&action.CommitTransaction{
+				TransactionID: 1,
+			},
+			&action.Request{
+				Request: `
+					query {
+						Users {
+							name
+							age
+						}
+					}
+				`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(29),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestTxnTTL_GivenCommittedHTTPTransaction_RemovesCacheEntry(t *testing.T) {
+	test := testUtils.TestCase{
+		SupportedClientTypes: immutable.Some([]state.ClientType{
+			state.HTTPClientType,
+			state.CLIClientType,
+		}),
+		HTTP: immutable.Some(options.NodeHTTPOptions{
+			TxnTTL:        500 * time.Millisecond,
+			TxnTTLTick:    50 * time.Millisecond,
+			TxnTTLBuckets: 20,
+		}),
+		SkipChangeDetector: true,
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `
+					type Users {
+						name: String
+						age: Int
+					}
+				`,
+			},
+			&action.AddDoc{
+				Doc: `{
+					"name": "John",
+					"age": 27
+				}`,
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `mutation {
+					update_Users(filter: {name: {_eq: "John"}}, input: {age: 28}) {
+						name
+						age
+					}
+				}`,
+				Results: map[string]any{
+					"update_Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(28),
+						},
+					},
+				},
+			},
+			&action.CommitTransaction{
+				TransactionID: 1,
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `
+					query {
+						Users {
+							name
+							age
+						}
+					}
+				`,
+				ExpectedError: "missing or expired transaction",
+			},
+			&action.Wait{
+				Duration: immutable.Some(600 * time.Millisecond),
+			},
+			&action.Request{
+				Request: `
+					query {
+						Users {
+							name
+							age
+						}
+					}
+				`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(28),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestTxnTTL_GivenDiscardedHTTPTransaction_RemovesCacheEntry(t *testing.T) {
+	test := testUtils.TestCase{
+		SupportedClientTypes: immutable.Some([]state.ClientType{
+			state.HTTPClientType,
+			state.CLIClientType,
+		}),
+		HTTP: immutable.Some(options.NodeHTTPOptions{
+			TxnTTL:        500 * time.Millisecond,
+			TxnTTLTick:    50 * time.Millisecond,
+			TxnTTLBuckets: 20,
+		}),
+		SkipChangeDetector: true,
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `
+					type Users {
+						name: String
+						age: Int
+					}
+				`,
+			},
+			&action.AddDoc{
+				Doc: `{
+					"name": "John",
+					"age": 27
+				}`,
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `mutation {
+					update_Users(filter: {name: {_eq: "John"}}, input: {age: 28}) {
+						name
+						age
+					}
+				}`,
+				Results: map[string]any{
+					"update_Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(28),
+						},
+					},
+				},
+			},
+			&action.DiscardTransaction{
+				TransactionID: 1,
+			},
+			&action.Request{
+				TransactionID: immutable.Some(1),
+				Request: `
+					query {
+						Users {
+							name
+							age
+						}
+					}
+				`,
+				ExpectedError: "missing or expired transaction",
+			},
+			&action.Wait{
+				Duration: immutable.Some(600 * time.Millisecond),
+			},
+			&action.Request{
+				Request: `
+					query {
+						Users {
+							name
+							age
+						}
+					}
+				`,
+				Results: map[string]any{
+					"Users": []map[string]any{
+						{
+							"name": "John",
+							"age":  int64(27),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestTxnTTL_GivenExpiredHTTPTransaction_OnGetCollections_ReturnsNonGraphQLError(t *testing.T) {
+	test := testUtils.TestCase{
+		SupportedClientTypes: immutable.Some([]state.ClientType{
+			state.HTTPClientType,
+			state.CLIClientType,
+		}),
+		HTTP: immutable.Some(options.NodeHTTPOptions{
+			TxnTTL:        500 * time.Millisecond,
+			TxnTTLTick:    50 * time.Millisecond,
+			TxnTTLBuckets: 20,
+		}),
+		SkipChangeDetector: true,
+		Actions: []any{
+			&action.AddCollection{
+				SDL: `
+					type Users {
+						name: String
+						age: Int
+					}
+				`,
+			},
+			&action.GetCollections{
+				TransactionID: immutable.Some(1),
+				FilterOptions: options.GetCollections(),
+				ExpectedResults: []client.CollectionVersion{
+					{
+						Name:           "Users",
+						VersionID:      "bafyreihsneodeja4lfer5puptim3lkwvketyckrmkhfpgxm67ch5wenjwq",
+						IsMaterialized: true,
+						IsActive:       true,
+					},
+				},
+			},
+			&action.Wait{
+				Duration: immutable.Some(600 * time.Millisecond),
+			},
+			&action.GetCollections{
+				TransactionID: immutable.Some(1),
+				FilterOptions: options.GetCollections(),
+				ExpectedError: "missing or expired transaction",
 			},
 		},
 	}
