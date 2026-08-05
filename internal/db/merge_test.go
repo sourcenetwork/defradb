a60282cee0cd9b60a023a65ceb98b713d5f21997
Menna Aboelnaga <mennatnaga@gmail.com>
2026-08-05 21:39:12 +0300

    refactor: move block-owner index reads into their own package

--- internal/db/merge_test.go
+++ internal/db/merge_test.go
@@ -32,6 +32,7 @@ import (
 	coreblock "github.com/sourcenetwork/defradb/internal/core/block"
 	"github.com/sourcenetwork/defradb/internal/core/crdt"
 	"github.com/sourcenetwork/defradb/internal/datastore"
+	"github.com/sourcenetwork/defradb/internal/db/blockowner"
 	"github.com/sourcenetwork/defradb/internal/db/id"
 )
 
@@ -233,7 +234,7 @@ func TestMerge_GenesisWithEmptyDocID_ResolvesDocIDAndFieldMappings(t *testing.T)
 	require.True(t, found)
 	require.NotEqual(t, sourceDoc.ID().String(), docShortID)
 
-	blockDocIDs, err := id.GetDocIDsForBlockFromStore(txnCtx, dbTxn.Systemstore(), fieldCID)
+	blockDocIDs, err := blockowner.DocIDs(txnCtx, dbTxn.Systemstore(), fieldCID)
 	require.NoError(t, err)
 	require.Equal(t, []string{sourceDoc.ID().String()}, blockDocIDs)
 }
